c88980a467a1f023bcf027d3532314c42a0fa592
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-05-05 15:45:43 +1000

    Fix swagger UI

--- api/server.py
+++ api/server.py
@@ -1,27 +1,41 @@
 from flask import Flask
+from flasgger import Swagger
+
+
 from api.routes import all_blueprints
-
 from api.utils.handleexception import handle_exception
-
-app = Flask('cpg_sample_metadata')
 
 
 API_PREFIX = '/api'
 
-for bp in all_blueprints:
-    app.register_blueprint(bp(API_PREFIX))
 
+def create_app():
+    """Create an return flask app"""
+    app = Flask('cpg_sample_metadata')
 
-@app.errorhandler(Exception)
-def exception_handler(e):
-    """
-    Source: https://flask.palletsprojects.com/en/1.1.x/errorhandling/#generic-exception-handlers
-    """
-    return handle_exception(e)
+    # pylint: disable=unused-variable
+    @app.errorhandler(Exception)
+    def exception_handler(e):
+        """
+        Source: https://flask.palletsprojects.com/en/1.1.x/errorhandling/#generic-exception-handlers
+        """
+        return handle_exception(e)
+
+    # add routes (before swagger)
+    for bp in all_blueprints:
+        app.register_blueprint(bp(API_PREFIX))
+
+        # add swagger
+        app.config['SWAGGER'] = {'title': 'Sample level metadata REST API'}
+
+    _ = Swagger(app)
+
+    return app
 
 
 def start_app():
     """Start web app"""
+    app = create_app()
     app.run(debug=True)
 
 
