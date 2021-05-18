096526e313b1ef82613e19d556990858a1b8eac8
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-05-18 16:48:38 +1000

    Cleanup sample_metadata python API

--- sample_metadata/api.py
+++ sample_metadata/api.py
@@ -1,55 +1,33 @@
 import json
-from models.models.analysis import Analysis
-from models.models.sample import Sample
 import requests
 
-from models.enums.samplestatus import SampleStatus
 
-class SampleMetadataApi:
+HOST = 'http://localhost:5000'
+URL_STRUCTURE = '{host}/api/v1/{project}/{module}/{name}'
 
-    def __init__(self, project: str) -> None:
-        pass
 
-    URL_STRUCTURE = "/api/v1/{project}/{module}/{name}"
+def _get_url(project, module, method_name, query_params=None):
+    q = ''
+    if query_params:
+        q = '?' + '&'.join(f'{k}={v}' for k, v in query_params.items())
 
-    def _get_url(self, module, name, query_params=None):
-        q = ""
-        if query_params:
-            q = "?" + "&".join(f'{k}={v}' for k,v in query_params.items())
-
-        return self.URL_STRUCTURE.format(
-            project=self.project,
-            module=module,
-            name=name
-        ) + q
-
-    def _post(self, module, name, data=None):
-        resp = requests.post(
-            self._get_url(module, name),
-            json=json.dumps(data)
+    return (
+        URL_STRUCTURE.format(
+            host=HOST, project=project, module=module, name=method_name
         )
-        resp.raise_for_status()
-        return resp.text
-
-    def _get(self, module, name, query_params):
-        resp = requests.get(
-            self._get_url(module, name, query_params)
-        )
-        resp.raise_for_status()
-        return resp.text
+        + q
+    )
 
 
-    def get_samples_by_status(self, status: SampleStatus) -> Sample:
-        return self._get("sample", "", {status: status})
+def post_sm(project, module, name, data=None):
+    """POST JSON and deserialise response from sample-metadata REST API"""
+    resp = requests.post(_get_url(project, module, name), json=json.dumps(data))
+    resp.raise_for_status()
+    return resp.json()
 
-    def add_analysis(self, analysis: Analysis, internal_sample_ids: List[str]) -> Analysis:
-        return self._post(
-            "analysis", "",
-            {
-                "analysis": {
-                    **analysis,
-                    "sample_ids": internal_sample_ids
-                }
-            }
-        )
 
+def get_sm(project, module, name, query_params):
+    """Get JSON response from sample-metadata REST API"""
+    resp = requests.get(_get_url(project, module, name, query_params))
+    resp.raise_for_status()
+    return resp.json()
