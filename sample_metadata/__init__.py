98c62e81b3335a076a8a0c6e677aa12ca6b5a612
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-05-19 11:45:23 +1000

    Add setup.py + version

---
+++ sample_metadata/__init__.py
@@ -0,0 +1,4 @@
+# pylint: disable=unused-variable
+
+import sample_metadata.sample as sample
+from ._version import __version__
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""
+Setup script for the analysis_runner python API
+- Used for development setup with `pip install --editable .`
+- Parsed by conda-build to extract version and metainfo
+"""
+
+from setuptools import setup, find_packages
+
+PKG = 'sample_metadata'
+
+all_packages = []
+for hl_package in 'sample_metadata', 'models':
+    all_packages.extend(
+        f'{hl_package}.' + p for p in sorted(find_packages(f'./{hl_package}'))
+    )
+
+setup(
+    name=PKG,
+    # This tag is automatically updated by bump2version
+    version='1.0.0',
+    description='Python API for interacting with the Sample API system',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url=f'https://github.com/populationgenomics/{PKG}',
+    license='MIT',
+    packages=all_packages,
+    requirements=['google-auth'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        # 'console_scripts': ['=analysis_runner.cli:main_from_args']
+    },
+    keywords='bioinformatics',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+)
