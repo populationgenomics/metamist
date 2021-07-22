258043b76fbc3e151140483b665731ea652e4dda
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-07-22 17:07:11 +1000

    Update create_project to include applying schema

--- db/scripts/create_project.py
+++ db/scripts/create_project.py
@@ -11,10 +11,13 @@ Requires:
 - google-cloud-secret-manager==2.2.0
     - Ensure you have the latest version of pip, as it installs significantly quicker
 """
+import os.path
 import logging
 import json
 import random
 import string
+import subprocess
+import urllib.request
 
 import pymysql
 from google.cloud import secretmanager
@@ -30,7 +33,12 @@ EXTERNAL_HOST = 'sm-db-vm-instance.australia-southeast1-b.c.sample-metadata.inte
 logging.basicConfig(level=logging.DEBUG)
 
 
-def create_database(project: str):
+def create_database(
+    project: str,
+    username: str = None,
+    password: str = None,
+    update_secret: bool = False,
+):
     """Driver function to create new database in sample-metadata"""
     if not project.isalpha():
         raise ValueError(
@@ -39,26 +47,42 @@ def create_database(project: str):
 
     project_name = project.lower()
 
-    databasename = project_name
-    username = f'sm_{project_name}'
-    password = _generate_password()
+    databasename = f'sm_{project_name}'
+    _username = username or f'sm_{project_name}'
+    _password = password or _generate_password()
+    force_recreate_user = True
+    if project_name == 'dev':
+        _username = 'root'
+        _password = None
+
+    if _username == 'root':
+        force_recreate_user = False
 
     connection = _get_connection()
     cursor: pymysql.cursors.Cursor = connection.cursor()
 
     _create_mariadb_database(cursor, databasename)
-    _create_user(cursor, databasename, username, password)
-    _grant_privileges_to_database(cursor, databasename, username)
-
-    secret_manager = secretmanager.SecretManagerServiceClient()
-
-    _update_databases_secret(
-        project=project_name,
-        databasename=databasename,
-        username=username,
-        password=password,
-        secret_manager=secret_manager,
+    _create_user(
+        cursor=cursor,
+        dbname=databasename,
+        username=_username,
+        password=_password,
+        force_recreate_user=force_recreate_user,
     )
+    _grant_privileges_to_database(cursor, databasename, _username)
+
+    _apply_schema(databasename)
+
+    if update_secret:
+        secret_manager = secretmanager.SecretManagerServiceClient()
+
+        _update_databases_secret(
+            project=project_name,
+            databasename=databasename,
+            username=_username,
+            password=password,
+            secret_manager=secret_manager,
+        )
 
 
 def _update_databases_secret(secret_manager, project, databasename, username, password):
@@ -127,7 +151,13 @@ def _get_connection() -> pymysql.Connection:
     return sqlcon
 
 
-def _create_user(cursor: pymysql.cursors.Cursor, dbname, username, password) -> bool:
+def _create_user(
+    cursor: pymysql.cursors.Cursor,
+    dbname,
+    username,
+    password,
+    force_recreate_user=False,
+) -> bool:
 
     # find if user exists
     _existing_query = (
@@ -136,6 +166,12 @@ def _create_user(cursor: pymysql.cursors.Cursor, dbname, username, password) -> 
     cursor.execute(_existing_query, (username,))
     row = cursor.fetchone()
     if row['count'] > 0:
+        if not force_recreate_user:
+            logging.warning(
+                'The user "{username}" already exists, skipping creation of user'
+            )
+            return False
+
         logging.critical(
             f'The user "{username}" already exists in the database, will remove and re-add'
         )
@@ -174,6 +210,58 @@ def _grant_privileges_to_database(
         raise
 
 
+def _apply_schema(databasename):
+
+    changelog_file = 'https://raw.githubusercontent.com/populationgenomics/sample-metadata/add-foundations/db/project.xml'
+    defaults_file = 'https://raw.githubusercontent.com/populationgenomics/sample-metadata/add-foundations/db/liquibase-project.properties'
+    mariadb_java_client_url = 'https://repo1.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/2.7.2/mariadb-java-client-2.7.2.jar'
+    mariadb_java_client_file = 'mariadb-java-client-2.7.2.jar'
+
+    if not os.path.exists(mariadb_java_client_file):
+        with urllib.request.urlopen(mariadb_java_client_url) as f, open(
+            mariadb_java_client_file, 'w+b'
+        ) as javafile:
+            javafile.write(f.read())
+
+    default_filename = 'defaults.properties'
+    changelog_filename = 'changelog.xml'
+
+    with open(default_filename, 'w+b') as tmp_defaults, open(
+        changelog_filename, 'w+b'
+    ) as tmp_changelog:
+        with urllib.request.urlopen(defaults_file) as f:
+            tmp_defaults.write(f.read())
+        with urllib.request.urlopen(changelog_file) as f:
+            tmp_changelog.write(f.read())
+
+    command = [
+        'liquibase',
+        '--logLevel=debug',
+        '--defaultsFile',
+        default_filename,
+        '--changeLogFile',
+        changelog_filename,
+        '--url',
+        f'jdbc:mariadb://localhost:3306/{databasename}',
+        'update',
+    ]
+    jcommand = ' '.join(command)
+    print(f'Running "{jcommand}"')
+    try:
+        output = subprocess.check_output(command, shell=True)
+        print(output.decode())
+    except subprocess.CalledProcessError as exp:
+        if exp.stderr:
+            print(exp.stderr.decode())
+        if exp.stdout:
+            print(exp.stdout.decode())
+
+    os.remove(default_filename)
+    os.remove(changelog_filename)
+
+    return True
+
+
 def from_args(args=None):
     """Run create_database(*)"""
 
@@ -181,10 +269,27 @@ def from_args(args=None):
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('project', help='Project name')
+    parser.add_argument('--update-gcp-secret', action='store_true')
+
+    parser.add_argument(
+        '--username', help='Override the user with format "sm_{project}"'
+    )
+
+    parser.add_argument(
+        '--password', help='(Default: generate password) override generated password'
+    )
+    parser.add_argument(
+        'project',
+        help='Project name, if the project is "dev", username will default to root, and password to empty',
+    )
 
     pargs = parser.parse_args(args)
-    create_database(pargs.project)
+    create_database(
+        pargs.project,
+        update_secret=pargs.update_gcp_secret,
+        password=pargs.password,
+        username=pargs.username,
+    )
 
 
 if __name__ == '__main__':
