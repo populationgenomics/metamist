2cfe120c3aeee99751340380633789803be996ca
vivbak <vivian.bakiris@populationgenomics.org.au>
2021-07-25 20:20:38 +1000

    Add upload processor

--- update_sample_status/main.py
+++ update_sample_status/main.py
@@ -15,28 +15,28 @@ secret_manager = secretmanager.SecretManagerServiceClient()
 def update_sample_status(request):
     """Main entry point for the Cloud Function."""
 
-    if request.method != 'PUT':
+    if request.method != "PUT":
         return abort(405)
 
     # Verify input parameters.
     request_json = request.get_json()
-    project = request_json.get('project')
-    sample = request_json.get('sample')
-    status = request_json.get('status')
+    project = request_json.get("project")
+    sample = request_json.get("sample")
+    status = request_json.get("status")
+
     if not project or not sample or not status:
         return abort(400)
 
-    logging.info(f'Processing request: {request_json}')
+    logging.info(f"Processing request: {request_json}")
 
     # Fetch the per-project configuration from the Secret Manager.
-    gcp_project = os.getenv('GCP_PROJECT')
+    gcp_project = os.getenv("GCP_PROJECT")
     secret_name = (
-        f'projects/{gcp_project}/secrets'
-        '/update-sample-status-config/versions/latest'
+        f"projects/{gcp_project}/secrets" "/update-sample-status-config/versions/latest"
     )
     config_str = secret_manager.access_secret_version(
-        request={'name': secret_name}
-    ).payload.data.decode('UTF-8')
+        request={"name": secret_name}
+    ).payload.data.decode("UTF-8")
     config = json.loads(config_str)
 
     project_config = config.get(project)
@@ -44,18 +44,16 @@ def update_sample_status(request):
         return abort(404)
 
     # Get the Airtable credentials.
-    base_key = project_config.get('baseKey')
-    table_name = project_config.get('tableName')
-    api_key = project_config.get('apiKey')
+    base_key = project_config.get("baseKey")
+    table_name = project_config.get("tableName")
+    api_key = project_config.get("apiKey")
     if not base_key or not table_name or not api_key:
         return abort(500)
 
     # Update the entry.
     airtable = Airtable(base_key, table_name, api_key)
     try:
-        response = airtable.update_by_field(
-            'Sample ID', sample, {'Status': status}
-        )
+        response = airtable.update_by_field("Sample ID", sample, {"Status": status})
     except HTTPError as err:  # Invalid status enum.
         logging.error(err)
         return abort(400)
@@ -63,4 +61,4 @@ def update_sample_status(request):
     if not response:  # Sample not found.
         return abort(404)
 
-    return ('', 204)
+    return ("", 204)
---
+++ upload_processor/upload_processor.py
@@ -0,0 +1,116 @@
+""" This function prepares gVCFs uploaded to GCS, based off sample status
+    logged in a database, for further QC and downstream analysis.
+    The upload processor will determine when samples should be added
+    to existing MatrixTables where appropriate and which MatrixTables
+    they should be combined with in this case. Following a successful
+    run, all uploaded files will be moved to archival storage.
+
+    Assumptions
+    ===========
+    - Specified files must exist in either the source or the destination
+      bucket
+    - Source and destination bucket must exist
+    - User must be authenticated with appropriate permissions """
+
+import os
+from typing import List, Optional, NamedTuple
+import hailtop.batch as hb
+
+# Import SampleAPI
+from sample_metadata.api.sample_api import SampleApi
+
+
+class SampleGroup(NamedTuple):
+    """ Defines a group of files associated with each sample"""
+
+    sample_id_external: str
+    data_file: str
+    index_file: str
+    md5: str
+
+
+def batch_move_files(
+    batch: hb.batch,
+    sample_group: SampleGroup,
+    source_prefix: str,
+    destination_prefix: str,
+    project: str,
+    docker_image: Optional[str] = None,
+    key: Optional[str] = None,
+) -> List:
+    """Creates a list of jobs to perform a batch move operation
+    to move files from a source location to a destination.
+
+    Parameters
+    ==========
+    batch: hb.Batch
+        An object representing the DAG of jobs to run.
+    files: SampleGroup
+        A NamedTuple containing 3 files to be moved.
+        For example ["TOB1543.g.vcf.gz","TOB1543.g.vcf.tbi","TOB1543.g.vcf.md5"]
+    source_prefix: str
+        The path to the sub-directory where the files are initially located.
+        For example "cpg-tob-wgs-upload" or "cpg-tob-wgs-upload/v1"
+    destination_prefix: str
+        The path to the sub-directory where the files should be moved.
+        For example "cpg-tob-wgs-main" or "cpg-tob-wgs-upload/batch1"
+    docker_image: str, optional
+        The address and tag of a previously built docker image, within the
+        artifact registry. Each batch job will run in this image.
+        For example;
+        australia-southeast1-docker.pkg.dev/project/images/driver:version'
+    key: str, optional
+        key-file for the service account used for authentication. In the case that this
+        is not provided as an input, it is assumed that this key will exist at
+        /gsa-key/key.json. This is the case when using the hail batch service backend.
+        For example:
+        "{
+            "type": "service_account",
+            "project_id": "",
+            "private_key_id": "" ...
+        }
+        " """
+
+    jobs = []
+
+    # Get internal sample ID
+    external_id = {"external_ids": [sample_group.sample_id_external]}
+    sapi = SampleApi()
+    internal_id_map = sapi.get_sample_id_map_by_external(project, external_id)
+    internal_id = str(list(internal_id_map.values())[0])
+
+    for tuple_key in sample_group._fields:
+        if tuple_key == "sample_id_external":
+            continue
+
+        file_name = getattr(sample_group, tuple_key)
+        previous_location = os.path.join("gs://", source_prefix, file_name)
+        file_extension = file_name[len(sample_group.sample_id_external) :]
+        new_file_name = internal_id + file_extension
+        new_location = os.path.join("gs://", destination_prefix, new_file_name)
+
+        j = batch.new_job(name=f"move {file_name} -> {new_file_name}")
+
+        if docker_image is not None:
+            j.image(docker_image)
+
+        # Authenticate to service account.
+        if key is not None:
+            j.command(f"echo '{key}' > /tmp/key.json")
+            j.command(
+                f"gcloud -q auth activate-service-account --key-file=/tmp/key.json"
+            )
+        # Handles service backend, or a key in the same default location.
+        else:
+            j.command(
+                "gcloud -q auth activate-service-account --key-file=/gsa-key/key.json"
+            )
+
+        # Checks file doesn't already exist at the destination, then performs move.
+        j.command(
+            f'gsutil -q stat "{new_location}" || '
+            f'gsutil mv "{previous_location}" "{new_location}"'
+        )
+        jobs.append(j)
+
+    return jobs
