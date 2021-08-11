5fe8ce74c8799afcba6f850d2cb04726a7a3622e
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-08-11 12:24:00 +1000

    Add kccg csv importer for tob-wgs

--- scripts/parse_tobwgs_csv.py
+++ scripts/parse_tobwgs_csv.py
@@ -1,9 +1,8 @@
 # pylint: disable=too-many-instance-attributes,too-many-locals
 import logging
 import os
-import re
 from io import StringIO
-from typing import Dict
+from typing import Dict, Optional
 
 import click
 
@@ -11,9 +10,8 @@ from sample_metadata.model.sample_type import SampleType
 from sample_metadata.model.sequence_status import SequenceStatus
 from sample_metadata.model.sequence_type import SequenceType
 
-from .parser import GenericParser, GROUPED_ROW
+from scripts.parser import GenericParser, GroupedRow
 
-rmatch = re.compile(r'_[Rr]\d')
 
 logger = logging.getLogger(__file__)
 logger.addHandler(logging.StreamHandler())
@@ -28,122 +26,134 @@ VCFGZ_EXTENSIONS = ('.vcf.gz',)
 class Columns:
     """Column keys for VCGS manifest"""
 
-    PHS_ACCESSION = 'phs_accession'
-    SAMPLE_NAME = 'sample_name'
-    LIBRARY_ID = 'library_ID'
-    TITLE = 'title'
-    LIBRARY_STRATEGY = 'library_strategy'
-    LIBRARY_SOURCE = 'library_source'
-    LIBRARY_SELECTION = 'library_selection'
-    LIBRARY_LAYOUT = 'library_layout'
-    PLATFORM = 'platform'
-    INSTRUMENT_MODEL = 'instrument_model'
-    DESIGN_DESCRIPTION = 'design_description'
-    REFERENCE_GENOME_ASSEMBLY = 'reference_genome_assembly'
-    ALIGNMENT_SOFTWARE = 'alignment_software'
-    FORWARD_READ_LENGTH = 'forward_read_length'
-    REVERSE_READ_LENGTH = 'reverse_read_length'
-    FILETYPE = 'filetype'
-    FILENAME = 'filename'
+    KCCG_ID = 'sample.id'
+    SAMPLE_NAME = 'sample.sample_name'
+    FLOWCELL_LANE = 'sample.flowcell_lane'
+    LIBRARY_ID = 'sample.library_id'
+    PLATFORM = 'sample.platform'
+    CENTRE = 'sample.centre'
+    REFERENCE_GENOME = 'sample.reference_genome'
+    FREEMIX = 'raw_data.FREEMIX'
+    PCT_CHIMERAS = 'raw_data.PCT_CHIMERAS'
+    PERCENT_DUPLICATION = 'raw_data.PERCENT_DUPLICATION'
+    MEDIAN_INSERT_SIZE = 'raw_data.MEDIAN_INSERT_SIZE'
+    MEDIAN_COVERAGE = 'raw_data.MEDIAN_COVERAGE'
+    BATCH_NAME = 'batch.batch_name'
+    SAMPLE_COUNT = 'batch.sample_count'
 
     @staticmethod
     def sequence_columns():
         """Columns that will be put into sequence.meta"""
         return [
+            Columns.FLOWCELL_LANE,
             Columns.LIBRARY_ID,
-            Columns.LIBRARY_STRATEGY,
-            Columns.LIBRARY_SOURCE,
-            Columns.LIBRARY_SELECTION,
-            Columns.LIBRARY_LAYOUT,
             Columns.PLATFORM,
-            Columns.INSTRUMENT_MODEL,
-            Columns.DESIGN_DESCRIPTION,
-            Columns.FORWARD_READ_LENGTH,
-            Columns.REVERSE_READ_LENGTH,
-        ]
-
-    @staticmethod
-    def sample_columns():
-        """Columns that will be put into sample.meta"""
-        return [
-            Columns.PHS_ACCESSION,
+            Columns.CENTRE,
+            Columns.REFERENCE_GENOME,
+            Columns.FREEMIX,
+            Columns.PCT_CHIMERAS,
+            Columns.PERCENT_DUPLICATION,
+            Columns.MEDIAN_INSERT_SIZE,
+            Columns.MEDIAN_COVERAGE,
         ]
 
 
 class TobWgsParser(GenericParser):
     """Parser for TOBWgs manifest"""
 
+    buckets = {}
+
+    def get_gvcf_path(self, sample_id: str, batch_number: int) -> Optional[str]:
+        """
+        GVCFs can be in a few places, so please find it from search locations
+        """
+        filename = f'{sample_id}.g.vcf.gz'
+        dirs = [
+            'gs://cpg-tob-wgs-main-upload/',
+            f'gs://cpg-tob-wgs-main/gvcf/batch{batch_number}/',
+        ]
+        for base in dirs:
+            fpath = os.path.join(base, filename)
+            if self.file_exists(fpath):
+                return fpath
+
+        return None
+
+    def get_cram_path(self, sample_id: str, batch_number: int) -> Optional[str]:
+        """
+        CRAMs can be in a few places, so please find it from search locations
+        """
+        filename = f'{sample_id}.cram'
+        dirs = [
+            'gs://cpg-tob-wgs-main-upload/',
+            f'gs://cpg-tob-wgs-archive/cram/batch{batch_number}/',
+        ]
+        for base in dirs:
+            fpath = os.path.join(base, filename)
+            if self.file_exists(fpath):
+                return fpath
+
+        return None
+
     def get_sample_id(self, row: Dict[str, any]):
+        """Get external sample ID from row"""
         return row[Columns.SAMPLE_NAME]
 
-    def get_sample_meta(self, sample_id: str, row: GROUPED_ROW):
-        if isinstance(row, list):
-            collapsed_sample_meta = {
-                col: ','.join(set(r[col] for r in row))
-                for col in Columns.sample_columns()
-            }
-            reads, reads_type = self.parse_reads([r[Columns.FILENAME] for r in row])
-        else:
-            collapsed_sample_meta = {col: row[col] for col in Columns.sample_columns()}
-            reads, reads_type = self.parse_reads([row[Columns.FILENAME]])
-
-        collapsed_sample_meta['reads'] = reads
-        collapsed_sample_meta['reads_type'] = reads_type
-        collapsed_sample_meta['project'] = self.project
+    def get_sample_meta(self, sample_id: str, row: GroupedRow):
+        """Get sample-metadata from row"""
+        collapsed_sample_meta = {}
 
         return collapsed_sample_meta
 
-    def get_sequence_meta(self, sample_id: str, row: GROUPED_ROW):
-        return {
-            col: ','.join(set(r[col] for r in row))
-            for col in Columns.sequence_columns()
-        }
+    def get_sequence_meta(self, sample_id: str, row: GroupedRow):
+        """Get sequence-metadata from row"""
 
-    def get_sequence_type(self, sample_id: str, row: GROUPED_ROW) -> SequenceType:
+        if isinstance(row, list):
+            collapsed_sequence_meta = {
+                col: ','.join(set(r[col] for r in row))
+                for col in Columns.sequence_columns()
+            }
+        else:
+            collapsed_sequence_meta = {
+                col: row[col] for col in Columns.sequence_columns()
+            }
+
+        batch_number = int(row[Columns.BATCH_NAME][-3:])
+
+        gvcf, variants_type = self.parse_file(
+            [self.get_gvcf_path(sample_id, batch_number)]
+        )
+        reads, reads_type = self.parse_file(
+            [self.get_cram_path(sample_id, batch_number)]
+        )
+
+        if len(gvcf) == 1:
+            gvcf = gvcf[0]
+        if len(reads) == 1:
+            reads = reads[0]
+
+        collapsed_sequence_meta['reads'] = reads
+        collapsed_sequence_meta['reads_type'] = reads_type
+        collapsed_sequence_meta['gvcf'] = gvcf
+        collapsed_sequence_meta['gvcf_type'] = variants_type
+
+        collapsed_sequence_meta['project'] = self.project
+
+        return collapsed_sequence_meta
+
+    def get_sequence_type(self, sample_id: str, row: GroupedRow) -> SequenceType:
         """
         Parse sequencing type (wgs / single-cell, etc)
         """
         # filter false-y values
-        if not isinstance(row, list):
-            return SequenceType(row[Columns.LIBRARY_STRATEGY])
-        else:
-            types = list(
-                set(
-                    r[Columns.LIBRARY_STRATEGY]
-                    for r in row
-                    if r[Columns.LIBRARY_STRATEGY]
-                )
-            )
-            if len(types) <= 0:
-                if (
-                    self.default_sequencing_type is None
-                    or self.default_sequencing_type.lower() == 'none'
-                ):
-                    raise ValueError(
-                        f"Couldn't detect sequence type for sample {sample_id}, and "
-                        'no default was available.'
-                    )
-                return self.default_sequencing_type
-            if len(types) > 1:
-                raise ValueError(
-                    f'Multiple library types for same sample {sample_id}, '
-                    f'maybe there are multiples types of sequencing in the same '
-                    f'manifest? If so, please raise an issue with mfranklin to '
-                    f'change the groupby to include {Columns.LIBRARY_STRATEGY}.'
-                )
+        return self.default_sequence_type
 
-            type_ = types[0].lower()
-            if type_ == 'wgs':
-                return SequenceType('wgs')
-            if type_ in ('single-cell', 'ss'):
-                return SequenceType('single-cell')
-
-            raise ValueError(f'Unrecognised sequencing type {type_}')
-
-    def get_sample_type(self, sample_id: str, row: GROUPED_ROW) -> SampleType:
+    def get_sample_type(self, sample_id: str, row: GroupedRow) -> SampleType:
+        """Get sample type"""
         return self.default_sample_type
 
-    def get_sequence_status(self, sample_id: str, row: GROUPED_ROW) -> SequenceStatus:
+    def get_sequence_status(self, sample_id: str, row: GroupedRow) -> SequenceStatus:
+        """Get sequence status from row"""
         return SequenceStatus('uploaded')
 
     @staticmethod
@@ -162,7 +172,7 @@ class TobWgsParser(GenericParser):
         else:
             manifest_filename = manifest
 
-        parser = VcgsManifestParser(
+        parser = TobWgsParser(
             path_prefix,
             project=project,
             sample_metadata_project=sample_metadata_project,
@@ -186,6 +196,7 @@ class TobWgsParser(GenericParser):
 )
 @click.option('--default-sample-type', default='blood')
 @click.option('--default-sequence-type', default='wgs')
+@click.option('--path-prefix', default=None)
 @click.argument('manifests', nargs=-1)
 def main(
     manifests,
@@ -193,17 +204,19 @@ def main(
     sample_metadata_project,
     default_sample_type='blood',
     default_sequence_type='wgs',
+    path_prefix=None,
 ):
     """Run script from CLI arguments"""
 
     for manifest in manifests:
         logger.info(f'Importing {manifest}')
-        resp = VcgsManifestParser.from_manifest_path(
+        resp = TobWgsParser.from_manifest_path(
             manifest=manifest,
             project=project,
             sample_metadata_project=sample_metadata_project,
             default_sample_type=default_sample_type,
             default_sequence_type=default_sequence_type,
+            path_prefix=path_prefix,
         )
         print(resp)
 
