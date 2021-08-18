726872995551f83f6447e9ac9915a670d0e0b063
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-08-18 21:22:47 +1000

    Introduce SampleLayer for managing permissions

--- api/routes/sample.py
+++ api/routes/sample.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 
 from models.enums import SampleType
 from models.models.sample import sample_id_transform_to_raw, sample_id_format
-from db.python.tables.sample import SampleTable, Sample
+from db.python.layers.sample import Sample, SampleLayer
 from db.python.tables.project import ProjectPermissionsTable
 
 from api.utils.db import (
@@ -41,7 +41,7 @@ async def create_new_sample(
     sample: NewSample, connection: Connection = get_project_db_connection
 ) -> str:
     """Creates a new sample, and returns the internal sample ID"""
-    st = SampleTable(connection)
+    st = SampleLayer(connection)
     async with connection.connection.transaction():
         internal_id = await st.insert_sample(
             external_id=sample.external_id,
@@ -49,6 +49,8 @@ async def create_new_sample(
             active=True,
             meta=sample.meta,
             participant_id=sample.participant_id,
+            # already checked on get_project_db_connection
+            check_project_id=False,
         )
         return sample_id_format(internal_id)
 
@@ -60,7 +62,7 @@ async def get_sample_id_map_by_external(
     connection: Connection = get_project_db_connection,
 ):
     """Get map of sample IDs, { [externalId]: internal_sample_id }"""
-    st = SampleTable(connection)
+    st = SampleLayer(connection)
     result = await st.get_sample_id_map_by_external_ids(
         external_ids, allow_missing=(allow_missing or False)
     )
@@ -76,11 +78,9 @@ async def get_sample_id_map_by_internal(
     Get map of sample IDs, { [internal_id]: external_sample_id }
     Without specifying a project, you might see duplicate external identifiers
     """
-    st = SampleTable(connection)
+    st = SampleLayer(connection)
     internal_ids_raw = sample_id_transform_to_raw(internal_ids)
-    result = await st.get_sample_id_map_by_internal_ids(
-        internal_ids_raw, check_project_ids=True
-    )
+    result = await st.get_sample_id_map_by_internal_ids(internal_ids_raw)
     return {sample_id_format(k): v for k, v in result.items()}
 
 
@@ -91,8 +91,8 @@ async def get_all_sample_id_map_by_internal(
     connection: Connection = get_project_db_connection,
 ):
     """Get map of ALL sample IDs, { [internal_id]: external_sample_id }"""
-    st = SampleTable(connection)
-    result = await st.get_all_sample_id_map_by_internal_ids()
+    st = SampleLayer(connection)
+    result = await st.get_all_sample_id_map_by_internal_ids(project=connection.project)
     return {sample_id_format(k): v for k, v in result.items()}
 
 
@@ -105,8 +105,8 @@ async def get_sample_by_external_id(
     external_id: str, connection: Connection = get_project_db_connection
 ):
     """Get sample by external ID"""
-    st = SampleTable(connection)
-    result = await st.get_single_by_external_id(external_id)
+    st = SampleLayer(connection)
+    result = await st.get_single_by_external_id(external_id, project=connection.project)
     return result
 
 
@@ -116,13 +116,13 @@ async def get_samples_by_criteria(
     meta: Dict = None,
     participant_ids: List[int] = None,
     project_ids: List[str] = None,
-    active: bool = Body(...),
+    active: bool = Body(default=True),
     connection: Connection = get_projectless_db_connection,
 ):
     """
     Get list of samples (dict) by some mixture of (AND'd) criteria
     """
-    st = SampleTable(connection)
+    st = SampleLayer(connection)
     if not project_ids:
         raise ValueError('You must specify at least one project id for this method')
 
@@ -156,7 +156,7 @@ async def update_sample(
     connection: Connection = get_projectless_db_connection,
 ):
     """Update sample with id"""
-    st = SampleTable(connection)
+    st = SampleLayer(connection)
     result = await st.update_sample(
         id_=sample_id_transform_to_raw(id_),
         meta=model.meta,
