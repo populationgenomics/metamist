39438f6bafd7bf77e1ae6ed3da337d33a049752a
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-08-18 21:21:29 +1000

    Add project permission and author to BaseLayer

--- db/python/layers/base.py
+++ db/python/layers/base.py
@@ -1,4 +1,4 @@
-from db.python.connect import Connection
+from db.python.connect import Connection, ProjectPermissionsTable
 
 
 class BaseLayer:
@@ -6,3 +6,9 @@ class BaseLayer:
 
     def __init__(self, connection: Connection):
         self.connection = connection
+        self.ptable = ProjectPermissionsTable(self.connection.connection)
+
+    @property
+    def author(self):
+        """Get author from connection"""
+        return self.connection.author
