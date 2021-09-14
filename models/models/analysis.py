7a1eeac3a7d8335ca643c31c82c55f4fc464f40c
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-09-14 10:13:11 +1000

    Add 'active' and 'meta' to analysis models

--- models/models/analysis.py
+++ models/models/analysis.py
@@ -1,4 +1,5 @@
-from typing import Optional, List, Union
+from typing import Optional, List, Union, Dict, Any
+import json
 
 from models.base import SMBase
 from models.enums import AnalysisType, AnalysisStatus
@@ -14,6 +15,8 @@ class Analysis(SMBase):
     sample_ids: List[Union[str, int]] = None
     timestamp_completed: Optional[str] = None
     project: int = None
+    active: bool = None
+    meta: Dict[str, Any] = None
 
     @staticmethod
     def from_db(**kwargs):
@@ -22,9 +25,12 @@ class Analysis(SMBase):
         """
         analysis_type = kwargs.pop('type', None)
         status = kwargs.pop('status', None)
-        sample_ids = [kwargs.pop('sample_id')]
-        output = kwargs.pop('output', [])
         timestamp_completed = kwargs.pop('timestamp_completed', None)
+        meta = kwargs.pop('meta', None)
+
+        if meta and isinstance(meta, str):
+            meta = json.loads(meta)
+
         if timestamp_completed:
             if not isinstance(timestamp_completed, str):
                 timestamp_completed = timestamp_completed.isoformat()
@@ -33,8 +39,10 @@ class Analysis(SMBase):
             id=kwargs.pop('id'),
             type=AnalysisType(analysis_type),
             status=AnalysisStatus(status),
-            sample_ids=sample_ids,
-            output=output,
+            sample_ids=[kwargs.pop('sample_id')],
+            output=kwargs.pop('output', []),
             timestamp_completed=timestamp_completed,
             project=kwargs.pop('project'),
+            meta=meta,
+            active=kwargs.pop('active'),
         )
