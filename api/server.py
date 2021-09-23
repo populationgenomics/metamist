fbf5c89a312dc7fa5520d53bd1ade06c5a554b19
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-09-23 10:21:05 +1000

    Bump version: 3.1.0 → 3.2.0

--- api/server.py
+++ api/server.py
@@ -21,7 +21,7 @@ from api.utils import get_openapi_schema_func
 from api.utils.exceptions import determine_code_from_error
 
 # This tag is automatically updated by bump2version
-_VERSION = '3.1.0'
+_VERSION = '3.2.0'
 
 logger = get_logger()
 
