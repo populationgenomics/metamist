fcda6bd812244e6cdd64c142af4df0925d68eabf
Michael Franklin <michael.franklin@populationgenomics.org.au>
2021-11-16 11:36:10 +1100

    Update Documentation.tsx

--- web/src/Documentation.tsx
+++ web/src/Documentation.tsx
@@ -14,11 +14,14 @@ interface IDocumentationArticleProps {
 }
 
 function RouterLink(props: any) {
+    // use an anchor for external links
     if (props.href.match(/^(https?:)?\/\//))
         return <a href={props.href}>{props.children}</a>
 
 
     let link: string = props.href
+    // rewrite the internal link here if it's a relative link
+    // because the openapi-generator template is incompatible with how we display markdown here.
     if (!link.startsWith('/')) { link = '/documentation/' + link; }
 
     return <Link to={link}>{props.children}</Link>;
