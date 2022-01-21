a0007bc081b22abbac15da18ec7e2d416cd9c70e
Michael Franklin <michael.franklin@populationgenomics.org.au>
2022-01-21 15:28:37 +1100

    Allow project selector to store project in URL

--- web/src/project/ProjectSelector.tsx
+++ web/src/project/ProjectSelector.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 
+import { useSearchParams } from 'react-router-dom'
+
 import { Input } from 'reactstrap'
 
 import { AppContext } from '../GlobalState'
@@ -11,11 +13,19 @@ interface ProjectSelectorProps {
 
 export const ProjectSelector: React.FunctionComponent<ProjectSelectorProps> = ({ onChange }) => {
 
+    // store project in global settings 
+    const globalContext = React.useContext(AppContext)
+
+    let [searchParams, setSearchParams] = useSearchParams()
+
+    const projectFromParams = searchParams.get('project')
+    if (!!projectFromParams && projectFromParams != globalContext.project) {
+        globalContext.setProject?.(projectFromParams)
+    }
+
     const [projects, setProjects] = React.useState<string[] | undefined>()
     const [error, setError] = React.useState<string | undefined>()
 
-    // store project in global settings 
-    const globalContext = React.useContext(AppContext)
 
     React.useEffect(() => {
         new ProjectApi().getMyProjects()
@@ -34,6 +44,8 @@ export const ProjectSelector: React.FunctionComponent<ProjectSelectorProps> = ({
     const projectChanged: React.ChangeEventHandler<HTMLInputElement> = (e) => {
         const project = e.currentTarget.value
         globalContext.setProject?.(project)
+        searchParams.set('project', project)
+        setSearchParams(searchParams)
         onChange?.(project)
 
     }
