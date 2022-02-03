7fe3c3519efda337a120fc70451f80de1e20ad59
Michael Franklin <michael.franklin@populationgenomics.org.au>
2022-02-04 02:19:19 +1100

    Fix participant / family IDs scripts

---
+++ scripts/fix_family_ids.py
@@ -0,0 +1,27 @@
+"""
+Small script to update external family IDs
+"""
+import json
+import click
+from sample_metadata.apis import FamilyApi
+from sample_metadata.model.family_update_model import FamilyUpdateModel
+
+
+@click.command()
+@click.option('--family-id-json', help='json map (string) of {old_external: new_external}')
+@click.option('--project')
+def main(family_id_json: str, project: str):
+    fid_map = json.loads(family_id_json)
+    fapi = FamilyApi()
+    families = fapi.get_families(project=project)
+    internal_fid_map = {f['external_id']: f['id'] for f in families}
+    update_map = {
+        internal_fid_map[prev_id]: new_id
+        for prev_id, new_id in fid_map.items()
+        if prev_id in internal_fid_map
+    }
+    print('Updating with', update_map)
+    new_map = list(update_map.items())
+    for fid, new_eid in new_map:
+        print(f'Updating {fid}: {new_eid}')
+        fapi.update_family(FamilyUpdateModel(id=fid, external_id=new_eid))
---
+++ scripts/fix_participant_ids.py
@@ -0,0 +1,28 @@
+"""
+Small script to update external participant IDs
+"""
+import json
+import click
+from sample_metadata.apis import ParticipantApi
+
+
+
+@click.command()
+@click.option('--participant-id-json', help='json map (string) of {old_external: new_external}')
+@click.option('--project')
+def main(participant_id_json: str, project: str):
+    """Update participant IDs by external ID with map {old_external: new_external}"""
+    pid_map = json.loads(participant_id_json)
+    papi = ParticipantApi()
+    internal_pid_map = papi.get_participant_id_map_by_external_ids(
+        project=project, request_body=list(pid_map.keys()), allow_missing=True
+    )
+    update_map = {
+        internal_pid_map[prev_id]: new_id
+        for prev_id, new_id in pid_map.items()
+        if prev_id in internal_pid_map
+    }
+
+    if click.confirm(f'Updating {len(update_map)} participants: {update_map}\n')
+        new_map = {str(k): str(v) for k, v in update_map.items()}
+        papi.update_many_participants(request_body=new_map)
