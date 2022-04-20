083f42c1c1bd29b0888731ddea51ef089d09d08d
vivbak <vivian.bakiris@populationgenomics.org.au>
2022-04-20 11:53:15 +1000

    Add participant ids to samples prior to upsert

--- db/python/layers/participant.py
+++ db/python/layers/participant.py
@@ -738,6 +738,25 @@ class ParticipantLayer(BaseLayer):
         # Create or update participants
         pids = [await self.upsert_participant(p) for p in all_participants]
 
+        # Pull a list of external pids
+        epids = [str(participant.external_id) for participant in all_participants]
+
+        # Get map of external pids to internal pids
+        epid_ipid_map = await self.get_id_map_by_external_ids(
+            epids,
+            project=self.connection.project,
+            allow_missing=True,
+        )
+
+        esid_ipid_map: Dict[str, str] = {}
+        for participant in all_participants:
+            pid = participant.external_id
+            ipid = epid_ipid_map[pid]
+
+            for sample in participant.samples:
+                sample.participant_id = ipid
+                esid_ipid_map[sample.external_id] = pid
+
         # Upsert all samples with sequences for each participant
         samples = [SampleBatchUpsertBody(samples=p.samples) for p in all_participants]
         results = [await sampt.batch_upsert_samples(s) for s in samples]
