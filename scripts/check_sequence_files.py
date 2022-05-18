197b8adbcd7780c3def31271b9d527245f4fff11
Sabrina Yan <sabrina.yan@populationgenomics.org.au>
2022-05-18 15:48:51 +1000

    STUFF

--- scripts/check_sequence_files.py
+++ scripts/check_sequence_files.py
@@ -1,16 +1,41 @@
+#!/usr/bin/env python3
+
 # import os
+import asyncio
+import logging
 import click
 
-# import sample_metadata
-
 # from typing import Set
+from sample_metadata.apis import ProjectApi, WebApi
+
+# Global vars
+logger = logging.getLogger(__file__)
+logging.basicConfig(level=logging.INFO)
+
+projapi = ProjectApi()
+webapi = WebApi()
+
+
+async def get_all_sequences():
+    """
+    Get all the sequences across all the projects from sample_metadata
+    """
+    projects = projapi.get_my_projects()
+    # Uncomment for get_all_projects
+    # projects = list(set([proj['name'] for proj in projects]))
+    projects.sort()
+
+    print(projects)
+
+    jobs = [webapi.get_project_summary_async(proj) for proj in projects]
+    summaries = await asyncio.gather(*jobs)
+    logging.info(summaries)
 
 
 @click.command()
-@click.argument('gs_dir')
 def main():
     """Main from CLI"""
-    # projects = get_all_projects()
+    asyncio.get_event_loop().run_until_complete(get_all_sequences())
 
 
 if __name__ == '__main__':
