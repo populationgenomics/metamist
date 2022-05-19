319a58e1254df0ff30142a55792d55ea93e96884
Michael Franklin <michael.franklin@populationgenomics.org.au>
2022-05-19 14:03:58 +1000

    Fix linting

--- sample_metadata/parser/generic_parser.py
+++ sample_metadata/parser/generic_parser.py
@@ -70,7 +70,7 @@ ALL_EXTENSIONS = (
 # construct rmatch string to capture all fastq patterns
 rmatch_str = (
     r'[_\.-][Rr]?[12]('
-    + '|'.join(s.replace(".", "\\.") for s in FASTQ_EXTENSIONS)
+    + '|'.join(s.replace('.', '\\.') for s in FASTQ_EXTENSIONS)
     + ')$'
 )
 rmatch = re.compile(rmatch_str)
