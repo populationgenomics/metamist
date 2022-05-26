95f2bad7a20c90c083f3f04f03338b0dacbe7563
Michael Franklin <michael.franklin@populationgenomics.org.au>
2022-05-27 00:58:32 +1000

    Add readsfile to ONT parser + add test

--- scripts/parse_ont_sheet.py
+++ scripts/parse_ont_sheet.py
@@ -5,15 +5,19 @@ import logging
 
 import click
 
-from sample_metadata.parser.generic_metadata_parser import run_as_sync, GenericMetadataParser
-from sample_metadata.parser.sample_file_map_parser import SampleFileMapParser
+from sample_metadata.parser.generic_metadata_parser import (
+    run_as_sync,
+    GenericMetadataParser,
+)
 
 
 logger = logging.getLogger(__file__)
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.INFO)
 
+
 class Columns:
+    """Columns for ONT data sheet from Garvan"""
     SEQUENCING_DATE = "Sequencing_date"
     EXPERIMENT_NAME = "Experiment name"
     SAMPLE_ID = "Sample ID"
@@ -24,40 +28,59 @@ class Columns:
     FLOWCELL_ID = "Flowcell ID"
     MUX_TOTAL = "MUX total"
     BASECALLING = "Basecalling"
+    FAIL_FASTQ_FILENAME = "Fail FASTQ filename"
+    PASS_FASTQ_FILENAME = "Pass FASTQ filename"
+
 
 class OntParser(GenericMetadataParser):
+    """Parser for ONT sheet data - provided by Garvan"""
     def __init__(
-            self,
-            search_locations: List[str],
-            sample_metadata_project: str,
-            default_sequence_type='genome',
-            default_sample_type='blood',
-            allow_extra_files_in_search_path=False,
+        self,
+        search_locations: List[str],
+        sample_metadata_project: str,
+        default_sequence_type='ont',
+        default_sample_type='blood',
+        allow_extra_files_in_search_path=False,
     ):
+        sequence_meta_map = {
+            Columns.SEQUENCING_DATE: Columns.SEQUENCING_DATE,
+            Columns.EXPERIMENT_NAME: Columns.EXPERIMENT_NAME,
+            Columns.PROTOCOL: Columns.PROTOCOL,
+            Columns.FLOW_CELL: Columns.FLOW_CELL,
+            Columns.BARCODING: Columns.BARCODING,
+            Columns.DEVICE: Columns.DEVICE,
+            Columns.FLOWCELL_ID: Columns.FLOWCELL_ID,
+            Columns.MUX_TOTAL: Columns.MUX_TOTAL,
+            Columns.BASECALLING: Columns.BASECALLING,
+            Columns.FAIL_FASTQ_FILENAME: 'failed_fastqs',
+        }
+
+        sequence_meta_map = {
+            k: v.lower().replace(' ', '_') for k, v in sequence_meta_map.items()
+        }
+
         super().__init__(
             search_locations=search_locations,
             sample_metadata_project=sample_metadata_project,
             participant_column=Columns.SAMPLE_ID,
             sample_name_column=Columns.SAMPLE_ID,
-            reads_column=None,
+            reads_column=Columns.PASS_FASTQ_FILENAME,
             default_sequence_type=default_sequence_type,
             default_sample_type=default_sample_type,
             participant_meta_map={},
             sample_meta_map={},
-            sequence_meta_map={
-                Columns.SEQUENCING_DATE: Columns.SEQUENCING_DATE,
-        Columns.EXPERIMENT_NAME: Columns.EXPERIMENT_NAME,
-        Columns.PROTOCOL: Columns.PROTOCOL,
-        Columns.FLOW_CELL: Columns.FLOW_CELL,
-        Columns.BARCODING: Columns.BARCODING,
-        Columns.DEVICE: Columns.DEVICE,
-        Columns.FLOWCELL_ID: Columns.FLOWCELL_ID,
-        Columns.MUX_TOTAL: Columns.MUX_TOTAL,
-        Columns.BASECALLING: Columns.BASECALLING,
-            },
+            sequence_meta_map=sequence_meta_map,
             qc_meta_map={},
             allow_extra_files_in_search_path=allow_extra_files_in_search_path,
         )
+
+    @staticmethod
+    def parse_fastqs_structure(fastqs) -> List[List[str]]:
+        """
+        In ONT, there is no pairs of fastqs as we don't
+        have to read forwards and backwards :D
+        """
+        return [fastqs]
 
 
 @click.command()
@@ -112,6 +135,7 @@ async def main(
         search_locations=search_path,
         allow_extra_files_in_search_path=allow_extra_files_in_search_path,
     )
+
     for manifest in manifests:
         logger.info(f'Importing {manifest}')
         resp = await parser.from_manifest_path(
---
+++ test/test_parse_ont_sheet.py
@@ -0,0 +1,62 @@
+
+
+import unittest
+from io import StringIO
+from unittest.mock import patch
+
+from scripts.parse_ont_sheet import OntParser
+from test.testbase import run_test_as_sync
+
+
+class TestOntSampleSheetParser(unittest.TestCase):
+    """Test the TestOntSampleSheetParser"""
+
+    @run_test_as_sync
+    @patch('sample_metadata.apis.SampleApi.get_sample_id_map_by_external')
+    @patch('sample_metadata.apis.SequenceApi.get_sequence_ids_from_sample_ids')
+    async def test_single_row_fastq(
+        self, mock_get_sequence_ids, mock_get_sample_id
+    ):
+        """
+        Test importing a single row, forms objects and checks response
+        - MOCKS: get_sample_id_map_by_external, get_sequence_ids_from_sample_ids
+        """
+        mock_get_sample_id.return_value = {'Sample01': 'CPG001'}
+        mock_get_sequence_ids.return_value = {}
+
+        rows = [
+            'Sequencing_date,Experiment name,Sample ID,Protocol,Flow cell,Barcoding,Device,Flowcell ID,MUX total,Basecalling,Fail FASTQ filename,Pass FASTQ filename',
+            '10/12/2034,PBXP_Awesome,Sample01,LSK1,PRO002,None,PromethION,XYZ1,7107,4.0.11+f1071ce,Sample01_fail.fastq.gz,Sample01_pass.fastq.gz',
+            '21/10/2021,PBXP_Awesome,Sample02,LSK1,PRO002,None,PromethION,XYZ1,8056,4.0.11+f1071ce,Sample02_fail.fastq.gz,Sample02_pass.fastq.gz',
+        ]
+        parser = OntParser(
+            search_locations=[],
+            # doesn't matter, we're going to mock the call anyway
+            sample_metadata_project='dev',
+        )
+
+        parser.skip_checking_gcs_objects = True
+        fs = ['Sample01_pass.fastq.gz','Sample02_pass.fastq.gz']
+        parser.filename_map = {k: 'gs://BUCKET/FAKE/' + k for k in fs}
+        parser.skip_checking_gcs_objects = True
+
+        file_contents = '\n'.join(rows)
+        resp = await parser.parse_manifest(
+            StringIO(file_contents), delimiter=',', dry_run=True
+        )
+
+        (
+            samples_to_add,
+            sequencing_to_add,
+            samples_to_update,
+            sequencing_to_update,
+            analyses_to_add,
+        ) = resp
+
+        self.assertEqual(1, len(samples_to_add))
+        self.assertEqual(2, len(sequencing_to_add))
+        self.assertEqual(1, len(samples_to_update))
+        self.assertEqual(0, len(sequencing_to_update))
+        self.assertEqual(0, len(analyses_to_add))
+
+
