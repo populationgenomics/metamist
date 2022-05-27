cd0327c912398f800e6664f3079fb9ce2c596a25
vivbak <vivian.bakiris@populationgenomics.org.au>
2022-05-27 15:53:06 +1000

    Clean up

--- scripts/parse_ped.py
+++ scripts/parse_ped.py
@@ -1,7 +1,8 @@
 """ A really simple script to import a pedigree file """
 
 import click
-from google.cloud import storage
+
+from cloudpathlib import CloudPath
 
 from sample_metadata.apis import (
     FamilyApi,
@@ -10,23 +11,13 @@ from sample_metadata.apis import (
 
 @click.command()
 @click.option('--ped-file-path', 'ped_file_path')
-@click.option('--bucket', 'bucket_name')
 @click.option('--project', 'project')
-def main(ped_file_path: str, bucket_name: str, project: str):
+def main(ped_file_path: str, project: str):
     """Pull ped file from GCS and import to SM"""
 
     fapi = FamilyApi()
-    client = storage.Client()
-    bucket = client.get_bucket(bucket_name)
-    blob = bucket.blob(ped_file_path)
-    ped_file = blob.download_as_bytes()
 
-    tmp_ped_tsv = 'tmp_ped.tsv'
-    # Work-around as import_pedigree takes a file.
-    with open(tmp_ped_tsv, 'wb') as tmp_ped:
-        tmp_ped.write(ped_file)
-
-    with open(tmp_ped_tsv) as ped_file:
+    with CloudPath(ped_file_path).open() as ped_file:
         fapi.import_pedigree(
             file=ped_file,
             has_header=True,
