fd45412921c84baec8f321e65973576c0ebf2ffa
Vivian Bakiris <79084890+vivbak@users.noreply.github.com>
2022-05-27 16:01:44 +1000

    Add parse ped script (#218)
    * Add parse ped script
    * Clean up

---
+++ scripts/parse_ped.py
@@ -0,0 +1,31 @@
+""" A really simple script to import a pedigree file """
+
+import click
+
+from cloudpathlib import CloudPath
+
+from sample_metadata.apis import (
+    FamilyApi,
+)
+
+
+@click.command()
+@click.option('--ped-file-path', 'ped_file_path')
+@click.option('--project', 'project')
+def main(ped_file_path: str, project: str):
+    """Pull ped file from GCS and import to SM"""
+
+    fapi = FamilyApi()
+
+    with CloudPath(ped_file_path).open() as ped_file:
+        fapi.import_pedigree(
+            file=ped_file,
+            has_header=True,
+            project=project,
+            create_missing_participants=True,
+        )
+
+
+if __name__ == '__main__':
+    # pylint: disable=no-value-for-parameter
+    main()
