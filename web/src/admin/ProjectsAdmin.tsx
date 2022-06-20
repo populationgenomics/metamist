ea33750e3596cb732f22c01c8ec856388e3ce874
Michael Franklin <michael.franklin@populationgenomics.org.au>
2022-06-20 22:29:43 +1000

    Add seqr guid modification to admin page

--- web/src/admin/ProjectsAdmin.tsx
+++ web/src/admin/ProjectsAdmin.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { ProjectApi, ProjectRow } from '../sm-api';
 
-import { Alert, Button, Input } from 'reactstrap';
+import { Alert, Button, Input, InputProps } from 'reactstrap';
 
 const ProjectsAdmin = (props: any) => {
 
     const [projects, setProjects] = React.useState<ProjectRow[]>([]);
     const [error, setError] = React.useState<string | undefined>();
+    const [projectStateValue, setProjectMetaState] = React.useState<any>({})
 
     const getProjects = () => {
         setError(undefined)
@@ -19,7 +20,7 @@ const ProjectsAdmin = (props: any) => {
         getProjects()
     }, [])
 
-    const headers = ['Id', 'Name', 'Dataset', 'Seqr']
+    const headers = ['Id', 'Name', 'Dataset', 'Seqr', 'Seqr GUID']
 
     if (!!error) return (<Alert color="danger">
         {error}<br />
@@ -27,6 +28,32 @@ const ProjectsAdmin = (props: any) => {
     </Alert>)
 
     if (!projects) return <div>Loading...</div>
+
+    const updateMetaValue = (projectName: string, metaKey: string, metaValue: any) => {
+        new ProjectApi().updateProject(projectName, { meta: { [metaKey]: metaValue } }).then(() => getProjects())
+    }
+
+    const ControlledInput: React.FunctionComponent<{ project: ProjectRow, metaKey: string } & InputProps> = ({ project, metaKey, ...props }) => {
+        // const projStateMeta: any = projectStateValue[project.name!]?.meta || {}
+        const projectMeta: any = project?.meta || {}
+        return (<Input
+            key={`input-${project.name}-${metaKey}`}
+            type="text"
+            label={metaKey}
+            defaultValue={projectMeta[metaKey]}
+            // onChange={(e) => setProjectMetaState({ [project.name!]: { meta: { ...projStateMeta, [metaKey]: e.target.value } } })}
+            onBlur={(e) => {
+                const newValue = e.currentTarget.value
+                if (newValue === projectMeta[metaKey]) {
+                    console.log(`Skip update to meta.${metaKey} as the value did not change`)
+                    return
+                }
+                console.log(`Updating ${project.name}: meta.${metaKey} to ${newValue}`)
+                updateMetaValue(project.name!, metaKey, newValue)
+            }}
+            {...props}
+        />)
+    }
 
     return <>
         <h1>Projects admin</h1>
@@ -37,17 +64,31 @@ const ProjectsAdmin = (props: any) => {
                 </tr>
             </thead>
             <tbody>
-                {projects.map(p => <tr key={p.id}>
-                    <td>{p.id}</td>
-                    <td>{p.name}</td>
-                    <td>{p.dataset}</td>
-                    <td>
-                        <Input type="checkbox" checked={p?.meta?.is_seqr} onChange={(e) => {
-                            new ProjectApi().updateProject(p.name!, { meta: { is_seqr: e.target.checked } }).then(() => getProjects())
-                        }
-                        } />
-                    </td>
-                </tr>)}
+                {projects.map(p => {
+                    // @ts-ignore
+                    const meta: { [key: string]: any } = p?.meta || {}
+                    return (<tr key={p.id}>
+                        <td>{p.id}</td>
+                        <td>{p.name}</td>
+                        <td>{p.dataset}</td>
+                        <td>
+                            <Input
+                                type="checkbox"
+                                checked={meta?.is_seqr}
+                                onChange={(e) => updateMetaValue(p.name!, 'is_seqr', e.target.checked)}
+                            />
+                        </td>
+                        <td>
+                            <ControlledInput
+                                key={`controlled-${p.name!}-seqr-guid}`}
+                                project={p}
+                                metaKey="seqr_guid"
+                                disabled={!p.meta?.is_seqr}
+                                placeholder='Seqr GUID'
+                            />
+                        </td>
+                    </tr>)
+                })}
             </tbody>
         </table>
     </>
