221db7cdd02b50b76e52ca8fa4db549e69e0e18f
Michael Franklin <michael.franklin@populationgenomics.org.au>
2022-09-11 18:37:03 +1000

    Add sql script to delete metamist project

---
+++ scripts/deleteproject.sql
@@ -0,0 +1,14 @@
+DECLARE @project VARCHAR(30);
+
+SELECT @project := id FROM project WHERE name = '<dataset>';
+
+DELETE FROM participant_phenotypes where participant_id IN (SELECT id FROM participant WHERE project = @project);
+DELETE FROM family_participant WHERE family_id IN (SELECT id FROM family where project = @project);
+DELETE FROM family WHERE project = @project;
+DELETE FROM sample_sequencing WHERE sample_id in (SELECT id FROM sample WHERE project = @project);
+DELETE FROM analysis_sample WHERE sample_id in (SELECT id FROM sample WHERE project = @project);
+DELETE FROM analysis_sample WHERE analysis_id in (SELECT id FROM analysis WHERE project = @project);
+DELETE FROM participant WHERE project = @project;
+DELETE FROM sample WHERE project = @project;
+DELETE FROM analysis WHERE project = @project;
+DELETE FROM project WHERE id = @project;
