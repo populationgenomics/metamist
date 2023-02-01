507eac7801547851feda1b28787c1af95fab88ee
Michael Franklin <michael.franklin@populationgenomics.org.au>
2023-02-02 06:42:12 +1100

    Fix group by import (#390)
    * Move group_by implementation into sample_metadata package
    
    * Bump version: 5.5.0 → 5.5.1
    
    * Fix type hint

--- api/server.py
+++ api/server.py
@@ -20,7 +20,7 @@ from api.settings import PROFILE_REQUESTS, SKIP_DATABASE_CONNECTION
 
 
 # This tag is automatically updated by bump2version
-_VERSION = '5.5.0'
+_VERSION = '5.5.1'
 
 logger = get_logger()
 
