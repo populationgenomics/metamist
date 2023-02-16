0d8cb666676df383b94e029de637746aeab09166
vivbak <vivian.bakiris@populationgenomics.org.au>
2023-02-16 16:04:02 +1100

    Add meta

--- scripts/create_test_subset.py
+++ scripts/create_test_subset.py
@@ -232,6 +232,7 @@ def main(
                 analysis_type=AnalysisType(a_type),
                 request_body=sample_ids,
             )
+            print(analyses)
         except exceptions.ApiException:
             traceback.print_exc()
         else:
@@ -311,6 +312,7 @@ def main(
                     ),
                     status=AnalysisStatus(analysis['status']),
                     sample_ids=[new_sample_map[s['id']]],
+                    meta=analysis['meta'],
                 )
                 logger.info(f'Creating {a_type} analysis entry in test')
                 aapi.create_new_analysis(project=target_project, analysis_model=am)
