787884537903dc80aeddfff35c31d90c7a38c7b8
EddieLF <eddieformaini@gmail.com>
2023-02-23 17:05:26 +1100

    Updated commands shell script for generic use

--- scripts/cmds.sh
+++ scripts/cmds.sh
@@ -1,12 +1,20 @@
-#!/opt/homebrew/Caskroom/miniforge/base/envs/gcp/bin/python
+PROJECT=""
+SEARCH_PATH="gs://cpg-${PROJECT}-main-upload/"
+SUBDIR=""
+SEARCH_PATH+=$SUBDIR
+ROOT_PATH=""
+DATA_DATE="20xx-xx-xx"
+FOLDER="${PROJECT}_${DATA_DATE}/"
+ROOT_PATH+="${FOLDER}"
+SAMPLE_MAPPING="${ROOT_PATH}sample_file_mapping.csv"
+MAPPING_FILE="${ROOT_PATH}sample_mapping.csv"
+PED_FILE="${ROOT_PATH}ped_file.csv"
+FAMILY_FILE="${ROOT_PATH}families_metadata.csv"
+INDIVIDUAL_METADATA="${ROOT_PATH}individuals_metadata.csv"
 
-PROJECT = 'rdp-kidney'
-SEARCH_PATH = 'gs://cpg-rdp-kidney-main-upload/'
-SAMPLE_MAPPING = '/Users/edwfor/Code/upload_scripts/sample-metadata/metadata_to_ingest/Feb2023_MGIKD_Sample file mapping template.csv'
-PED_FILE = '/Users/edwfor/Code/upload_scripts/sample-metadata/metadata_to_ingest/Feb2023_MGIKD_individuals_template.csv'
-INDIVIDUAL_METADATA = '/Users/edwfor/Code/upload_scripts/sample-metadata/metadata_to_ingest/Feb2023_MGIKD_individuals_metadata.csv'
 
+python generate_sample_file_map.py -i $SAMPLE_MAPPING -p $SEARCH_PATH -g > $MAPPING_FILE
 
-python parse_sample_file_map.py --project PROJECT --search-path SEARCH_PATH --dry-run SAMPLE_MAPPING
+python parse_sample_file_map.py --project $PROJECT --search-path $SEARCH_PATH --dry-run --default-sequence-type $MAPPING_FILE
 
-python parse_rd_metadata.py --project PROJECT --ped-file PED_FILE --individual-metadata INDIVIDUAL_METADATA
+python parse_rd_metadata.py --project $PROJECT --ped-file $PED_FILE --individual-metadata $INDIVIDUAL_METADATA
