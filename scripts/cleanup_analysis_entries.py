7d5c85cb76311d15213316b1dd71902a6d604f78
EddieLF <eddieformaini@gmail.com>
2023-03-06 10:51:45 +1100

    pandas/sql implementation for cram audit results

--- scripts/cleanup_analysis_entries.py
+++ scripts/cleanup_analysis_entries.py
@@ -4,7 +4,9 @@ import logging
 import json
 import re
 import click
+import pandas as pd
 
+from pandasql import sqldf
 from google.cloud import storage
 from sample_metadata.apis import ProjectApi, AnalysisApi
 from sample_metadata.model.analysis_query_model import AnalysisQueryModel
@@ -19,7 +21,11 @@ BUCKET_TYPES = [
     'archive',
     'release',
 ]
+# Samples Cas has been excluding from analysis runs
+EXCLUDED_SAMPLES = ['CPG11783', 'CPG253328', 'CPG13409', 'CPG243717', 'CPG246645', 'CPG246678', 'CPG246561', 'CPG261552','CPG261560','CPG261578','CPG261586','CPG261594','CPG261602','CPG261610','CPG261628','CPG261636','CPG261644','CPG261651','CPG261669','CPG261677','CPG261685','CPG261693','CPG261701','CPG261719','CPG261727','CPG261735','CPG261743',
+    'CPG261792', 'CPG259150','CPG258814','CPG258137','CPG258111','CPG258012', 'CPG221978', 'CPG222000', 'CPG261339','CPG261347', 'CPG265876', 'CPG261305', 'CPG261313', 'CPG261321']
 
+# Pattern to match the file paths (e.g. 'gs://cpg-dataset-type/subdir/file.cram')
 rmatch_str = (
     r'gs://(?P<bucket>cpg-(?P<dataset>[\w-]+)(?=-'
     + '|'.join(s for s in BUCKET_TYPES)
@@ -68,6 +74,8 @@ def get_path_components_from_path(path):
 
     bucket_name = path_components[0]
     dataset = path_components[1]
+    if dataset[-1]=='-':
+        dataset = dataset.removesuffix('-')
     subdir = path_components[2]
 
     return {'bucket_name': bucket_name, 'dataset': dataset, 'subdir': subdir}
@@ -89,18 +97,18 @@ def find_duplicate_analyses(
         all_analyses[analysis['output']].add(analysis['id'])
 
     duplicate_analyses = [vals for vals in all_analyses.values() if len(vals) > 1]
-    print(
-        'Path with most analysis entries pointing to it:',
-        sorted(all_analyses.items(), key=lambda x: len(x[1]), reverse=True)[0],
-    )
+    # print(
+    #     'Path with most analysis entries pointing to it:',
+    #     sorted(all_analyses.items(), key=lambda x: len(x[1]), reverse=True)[0],
+    # )
 
     # keep the first entry pointing to a path, the rest are duplicates
     # ids_to_delete = set(chain.from_iterable(duplicate_analyses))
-    ids_to_delete = set()
+    ids_with_duplicates = set()
     for duplicate_ids in duplicate_analyses:
-        ids_to_delete.update(sorted(duplicate_ids)[1:])
+        ids_with_duplicates.update(sorted(duplicate_ids))#[1:])
 
-    return analyses, ids_to_delete
+    return analyses, ids_with_duplicates
 
 
 def remove_dupes_from_analyses(
@@ -133,7 +141,7 @@ def remove_dupes_from_analyses(
     return remaining_analyses
 
 
-def check_all_analyses_belongs_to_project(analyses: list[dict], project_id_map: dict):
+def check_all_analyses_belongs_to_project(analyses: list[dict], project_id_map: dict) -> list[dict]:
     """
     Check the analysis['project'] matches the bucket name, `cpg-{dataset}-{main/test}`
     Uses the project id map to get the project name from the project id
@@ -142,16 +150,26 @@ def check_all_analyses_belongs_to_project(analyses: list[dict], project_id_map: 
     for analysis in analyses:
         project_id = analysis['project']
         project_name = project_id_map[project_id]
+        analysis['project_name'] = project_name
+
+        if analysis['valid_path'] == False:
+            analysis['cram_location_project_name'] = ''
+            continue
+
         try:
             dataset_name = get_path_components_from_path(analysis['output'])['dataset']
+            analysis['cram_location_project_name'] = dataset_name
         except TypeError:
             continue
+
         if dataset_name == project_name:
             continue
+
         logging.error(
             f'Analysis ID: {str(analysis["id"])} has project ID: {project_id} which maps to: {project_name}, but analysis dataset is: {dataset_name}'
         )
 
+    return analyses
 
 def replace_analysis_paths(analyses: list[dict], analysis_by_path: dict) -> list[dict]:
     """
@@ -301,44 +319,131 @@ def get_paths_for_subdir(bucket_name, subdirectory, analysis_type):
     return files_in_bucket_subdir
 
 
-def cleanup_analysis_output_paths(analyses: list[dict]) -> dict[int, str]:
+def cleanup_analysis_output_paths(analyses: list[dict]) -> tuple[dict[int, str], list[dict]]:
     """
-    Parses analysis output paths into simple path strings
+    Parses analysis output paths into simple path strings, storing the original output
+    in the 'original_output' value and the cleaned output in the 'output' value.
 
     Analysis output path is usually just a GS path: gs://some-bucket/file.cram, but
     sometimes its a key-value list of different output paths. In this case, the output
     paths are read into a dictionary then relevant path is extracted as a string.
 
-    Returns a dictionary of {analysis ID : analysis path}
+    Returns a dictionary of {analysis ID : analysis path} and the modified analyses list
     """
 
     analysis_paths = {}
     for analysis in analyses:
+        analysis['multiple_output_paths'] = 1 if analysis['output'].count(':')>1 else 0
+        analysis['original_output'] = analysis['output']
+
         # json.loads requires double quotes so we replace all single quotes
         # GSPath objects need to be converted to str, e.g. GSPath('gs://...') -> "gs:/..."
         analysis_output = analysis['output'].replace("'", "\"")  # noqa: Q000
         analysis_output = analysis_output.replace('GSPath("', '"')
         analysis_output = analysis_output.replace(')', '')
 
-        try:
+        if analysis['multiple_output_paths']:
             # For the case where the analysis output is a key-value list of paths
             output_dict = json.loads(analysis_output)
             analysis_path = str(output_dict['cram'])
 
-        except json.decoder.JSONDecodeError:
+        else:
             # For the case where the analysis output is just the path
             analysis_path = analysis['output']
 
         analysis_paths[analysis['id']] = analysis_path
 
-    return analysis_paths
+    # Replace all analyses output paths with raw paths
+    analyses = replace_analysis_paths(analyses, analysis_paths)
+
+    return analysis_paths, analyses
+
+
+def add_exclusions_duplicates_flags(analyses: list[dict], duplicate_ids: set[int]) -> list[dict]:
+    """
+    Takes the analyses list and flags sample IDs which are in the exclude list or the duplicate list.
+    Also transforms the sample ids array into the string value of the array for easier parsing.
+    """
+    for analysis in analyses:
+        analysis['sample_ids'] = analysis['sample_ids'][0]
+
+        analysis['excluded'] = 1 if analysis['sample_ids'] in EXCLUDED_SAMPLES else 0
+
+        analysis['duplicate_path'] = 1 if analysis['id'] in duplicate_ids else 0
+
+    return analyses
+
+
+def get_analysis_results_with_sql(analyses_df: pd.DataFrame) -> pd.DataFrame:
+    """"
+    Uses a pandasql implementation to transform the analyses dataframe into the analyses results dataframe.
+
+    First creates a flag for the latest completed sample by project, by filtering for completed analyses,
+    then selecting the analysis with the most recent timestamp_completed, grouping by sample and project.
+    Another flag is created in the same way, but this time grouping only by sample and not project.
+
+    The flags are set to 1 when the sample is not a duplicate since that must be the latest completion.
+    """
+
+    # TODO: implement this transform without invoking SQL code for consistency with codebase.
+    query = """ SELECT a.id, a.sample_ids, a.status, a.output, a.timestamp_completed, 
+                CASE WHEN a.timestamp_completed=a.last_completed_time_by_project and a.duplicate_path="1"
+                        then 1 
+                     WHEN a.duplicate_path<>"1"
+                        then 1
+                     else 0 
+                end as "latest_completed_sample_by_project",
+                CASE WHEN a.timestamp_completed=a.last_completed_time and a.duplicate_path="1"
+                        then 1 
+                     WHEN a.duplicate_path<>"1"
+                        then 1
+                     else 0 
+                end as "latest_completed_sample"
+             FROM (
+                    SELECT  a.id, a.status, a.timestamp_completed, a.output, a.project,
+                            a.sample_ids, a1.last_completed_time_by_project, a2.last_completed_time,
+                            a.duplicate_path
+                    FROM analyses_df a
+                        JOIN (  SELECT  sample_ids, project, 
+                                        max(timestamp_completed) as last_completed_time_by_project
+                                FROM analyses_df 
+                                group by sample_ids, project) as a1
+                        ON (a.sample_ids = a1.sample_ids and a.project=a1.project)
+                        JOIN (  SELECT  sample_ids, 
+                                        max(timestamp_completed) as last_completed_time
+                                FROM analyses_df 
+                                group by sample_ids) as a2
+                        ON (a.sample_ids = a2.sample_ids)
+                    WHERE status="completed"
+                ) a
+            ;"""
+    completed_samples_latest_flag = sqldf(query) # noqa: Q000
+
+    query = """ SELECT  a.*,
+                        case when c.id is not null 
+                             then c.latest_completed_sample
+                             else 0 
+                        end as 'latest_completed_sample',
+                        case when c.id is not null
+                             then c.latest_completed_sample_by_project
+                             else 0
+                        end as 'latest_completed_sample_by_project'
+                FROM analyses_df a
+                LEFT JOIN completed_samples_latest_flag c
+                    on (a.id = c.id)
+            ;"""
+
+    analyses_results = sqldf(query)
+
+    return analyses_results
 
 
 @click.command()
 @click.option('--analysis-type', default='cram')
 @click.option('--dry-run', is_flag=True)
 @click.option('--print-dupes', is_flag=True)
-def main(analysis_type, dry_run, print_dupes):
+@click.option('--output-path', default='~/')
+def main(analysis_type, dry_run, print_dupes, output_path):
     """
     Finds all analysis entries across the various seqr projects
     Checks each entry has a valid path to an analysis file
@@ -354,24 +459,24 @@ def main(analysis_type, dry_run, print_dupes):
     datasets = [p['name'] for p in projects]
     project_id_map = {p['id']: p['name'] for p in projects}
 
-    analyses = get_analyses_for_datasets(datasets, analysis_type)
+    analyses = get_analyses_for_datasets(datasets, analysis_type)        
 
     analyses_samples = {a['id']: a['sample_ids'][0] for a in analyses}
 
     # Get all {analysis id : raw analysis path string}
-    analysis_by_path = cleanup_analysis_output_paths(analyses)
-
-    # Replace all analyses output paths with raw paths
-    analyses = replace_analysis_paths(analyses, analysis_by_path)
+    analysis_by_path, analyses = cleanup_analysis_output_paths(analyses)
 
     analyses, duplicate_ids = find_duplicate_analyses(analyses)
 
-    # Remove analysis from analyses list if analysis id in duplicate_ids
+    # add flags for entries in the exlcusions list and duplicate list
+    analyses = add_exclusions_duplicates_flags(analyses, duplicate_ids)
+
+    # Create remaining analyses list by removing all duplicates from analyses list
     remaining_analyses = remove_dupes_from_analyses(
         analyses, duplicate_ids, print_dupes
     )
 
-    # Check which analysis paths lead to files that exist
+    # Check which analysis paths lead to files that exist and which dont
     (
         remaining_analyses,
         path_validity,
@@ -379,37 +484,31 @@ def main(analysis_type, dry_run, print_dupes):
         file_missing_analysis_path,
     ) = validate_paths(remaining_analyses, analysis_by_path, analysis_type)
 
+    for analysis in analyses:
+        analysis['valid_path'] = 1 if analysis['id'] in id_validity['valid_ids'] else 0
+          
     # log warnings if the analyses['project'] doesn't match the bucket name, `cpg-{dataset}-{main/test}`
     # lots of warnings here...
-    check_all_analyses_belongs_to_project(remaining_analyses, project_id_map)
+    analyses = check_all_analyses_belongs_to_project(analyses, project_id_map)
+
 
     print(
         f'{len(path_validity["valid_paths"])} valid paths, {len(path_validity["invalid_paths"])} invalid paths, '  # noqa: Q000
         + f'{len(file_missing_analysis_path)} files found with no analysis path'
     )
 
-    # Remove duplicate samples by inverting the dictionary twice
-    temp = {val: key for key, val in analyses_samples.items()}
-    analyses_samples = {val: key for key, val in temp.items()}
+    # Transform the analyses list into a dataframe with one row per analysis entry
+    analyses_df = pd.DataFrame(analyses)
 
-    # Now we can track all unique sample IDs and their corresponding analysis ID
-    samples_set = set(analyses_samples.values())
-    analysis_ids_for_samples = set(analyses_samples.keys())
+    # Remove the meta field for now since its a nested dictionary which doesn't play nice 
+    analyses_df = analyses_df.drop(labels='meta', axis=1)
 
-    print(len(samples_set), 'unique sample IDs in all analyses')
-    print(
-        len(analysis_ids_for_samples.intersection(id_validity['invalid_ids'])),
-        'samples with invalid paths',
-    )
+    # Make sure the timestamp field is a datetime type, not string
+    analyses_df['timestamp_completed'] = pd.to_datetime(analyses_df['timestamp_completed'])
 
-    # Print the invalid sample IDs and their paths
-    samples_invalid_path = analysis_ids_for_samples.intersection(
-        id_validity['invalid_ids']
-    )
-    for invalid_id in samples_invalid_path:
-        logging.error(
-            f'Sample missing file: {analyses_samples[invalid_id]} {analysis_by_path[invalid_id]}'
-        )
+    analyses_results = get_analysis_results_with_sql(analyses_df)
+
+    analyses_results.to_csv(f'{output_path}analyses_results.csv', index=False)
 
     # Combine the duplicates ids with the invalid ids
     ids_to_delete = set(duplicate_ids).union(id_validity['invalid_ids'])
