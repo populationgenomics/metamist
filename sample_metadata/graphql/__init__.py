09a4e3fa04f0b07ed30e1ee926170f8b582d6a94
Michael Franklin <michael.franklin@populationgenomics.org.au>
2023-04-06 12:12:22 +1000

    Add rough implementation that allows graphql query

---
+++ sample_metadata/graphql/__init__.py
@@ -0,0 +1,30 @@
+import os
+import requests
+from cpg_utils.cloud import get_google_identity_token
+
+from sample_metadata.configuration import sm_url
+
+
+def query(_query, variables=None, auth_token: str = None):
+    """Query the Metamist GraphQL API"""
+    if variables is None:
+        variables = {}
+
+    token = auth_token or get_google_identity_token(target_audience=sm_url)
+
+    request = requests.post(
+        os.path.join(sm_url, 'graphql'),
+        json={'query': _query, 'variables': variables},
+        headers={'Authorization': f'Bearer {token}'},
+        timeout=60,
+    )
+
+    if not request.ok:
+        raise ValueError(
+            f'Metamist GraphQL query failed with code {request.status_code}: {request.text}'
+        )
+
+    data = request.json()
+    if 'errors' in data:
+        raise ValueError(f'Metamist GraphQL query failed: {data["errors"]}')
+    return data['data']
