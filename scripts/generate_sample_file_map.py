508c362d722aba52d35a99f82c19d7f15a61654c
EddieLF <eddieformaini@gmail.com>
2023-05-09 13:11:03 +1000

    added excel parser - just resave as csv and parse that instead

--- scripts/generate_sample_file_map.py
+++ scripts/generate_sample_file_map.py
@@ -4,11 +4,11 @@
 
  """
 
-from collections import defaultdict
-import click
 import csv
 import sys
+import click
 
+import pandas as pd
 from cloudpathlib import CloudPath
 
 from redcap_parsing_utils import Facility, find_fastq_pairs, FILEMAP_HEADERS
@@ -18,22 +18,22 @@ from redcap_parsing_utils import Facility, find_fastq_pairs, FILEMAP_HEADERS
 @click.option('-i', '--id-map-path', required=True)
 @click.option('-p', '--search-path', required=True)
 @click.option('-s', '--sample_id_column', default='Sample ID')
-@click.option('-g', '--garvan',  is_flag=True)
+@click.option('-g', '--garvan', is_flag=True)
 def main(search_path: str, id_map_path: str, garvan: bool, sample_id_column):
     """
-        Helper script to preapare a sample file map for ingest.
+    Helper script to preapare a sample file map for ingest.
 
-        Given a map of extIDs -> Facility Seq IDs, finds all fastq-pairs for
-        each sample id in the nominated bucket. Writes a sample map file ready
-        metamist for ingest.
+    Given a map of extIDs -> Facility Seq IDs, finds all fastq-pairs for
+    each sample id in the nominated bucket. Writes a sample map file ready
+    metamist for ingest.
 
-        Currently only works for the following seq facilities:
-        - VCGS [default]:
-            - Modern VCGS file name format only (>= ~2018)
-            - Detects genome, exome and RNA types
+    Currently only works for the following seq facilities:
+    - VCGS [default]:
+        - Modern VCGS file name format only (>= ~2018)
+        - Detects genome, exome and RNA types
 
-        - Garvan/KCCG
-            - Assumes WGS type
+    - Garvan/KCCG
+        - Assumes WGS type
 
     """
     if garvan:
@@ -47,20 +47,29 @@ def main(search_path: str, id_map_path: str, garvan: bool, sample_id_column):
 
     read_pairs_by_sample_id = find_fastq_pairs(search_path, facility)
 
-    # Read id map
+    # Read id map - first convert xlsx to csv
+    if id_map_path.endswith('xlsx'):
+        # Open an xlsx with pandas
+        read_file = pd.read_excel(id_map_path)
+        # Write the excel to an identical csv
+        id_map_path = id_map_path[: len(id_map_path) - 4] + 'csv'
+        read_file.to_csv(id_map_path, index=None, header=True, encoding='utf-8-sig')
+
     with open(id_map_path, encoding='utf-8-sig') as id_map:
         reader = csv.DictReader(id_map)
-        writer = csv.DictWriter(sys.stdout, fieldnames=FILEMAP_HEADERS,  delimiter=',')
+        writer = csv.DictWriter(sys.stdout, fieldnames=FILEMAP_HEADERS, delimiter=',')
         writer.writeheader()
 
         for line in reader:
             for pair in read_pairs_by_sample_id[line[sample_id_column]]:
-                writer.writerow({
-                    'Individual ID': line['Individual ID'],
-                    'Sample ID': line[sample_id_column],
-                    'Filenames': ','.join(sorted([x.path.name for x in pair])),
-                    'Type': pair[0].seq_type.value
-                })
+                writer.writerow(
+                    {
+                        'Individual ID': line['Individual ID'],
+                        'Sample ID': line[sample_id_column],
+                        'Filenames': ','.join(sorted([x.path.name for x in pair])),
+                        'Type': pair[0].seq_type.value,
+                    }
+                )
 
 
 if __name__ == '__main__':
--- scripts/parse_rd_metadata.py
+++ scripts/parse_rd_metadata.py
@@ -1,10 +1,23 @@
 """ A simple script to import metadata files for RD projects """
 
-import sys
-import click
 from pathlib import Path
+import sys
 
+import click
+import pandas as pd
 from sample_metadata.apis import FamilyApi, ImportApi
+
+
+def save_xlsx_as_csv(filepath: str):
+    """Uses pandas to resave a .xlsx file as a csv with identical path"""
+    # Open the xlsx with pandas
+    read_file = pd.read_excel(filepath)
+
+    # Write the excel to an identical csv
+    filepath = filepath[: len(filepath) - 4] + 'csv'
+    read_file.to_csv(filepath, index=None, header=True, encoding='utf-8-sig')
+
+    return filepath
 
 
 @click.command()
@@ -14,21 +27,26 @@ from sample_metadata.apis import FamilyApi, ImportApi
 @click.option('--family-metadata', 'family_metadata')
 def main(
     project: str,
-    ped_file_path: str = "",
-    individual_metadata: str = "",
-    family_metadata: str = "",
+    ped_file_path: str = '',
+    individual_metadata: str = '',
+    family_metadata: str = '',
 ):
     """
     A simple metamist API wrapper for local imports of pedigree, individual and
     family metadata files.
+    Converts .xlsx files to csv and saves them to the original directory.
     """
 
     if not any([ped_file_path, individual_metadata, family_metadata]):
-        print("Please provide at least one metadata file to import", file=sys.stderr)
+        print('Please provide at least one metadata file to import', file=sys.stderr)
         sys.exit(1)
 
     if ped_file_path:
-        print(f"processing pedigree file: {ped_file_path}")
+        if ped_file_path.endswith('xlsx'):
+            ped_file_path = save_xlsx_as_csv(ped_file_path)
+            print(f'Converted pedigree excel to csv.')
+
+        print(f'processing pedigree file: {ped_file_path}')
         with Path(ped_file_path).open() as ped_file:
             response = FamilyApi().import_pedigree(
                 file=ped_file,
@@ -39,7 +57,11 @@ def main(
             print('API response:', response)
 
     if individual_metadata:
-        print(f"\nProcessing individual_metadata file: {individual_metadata}")
+        if individual_metadata.endswith('xlsx'):
+            individual_metadata = save_xlsx_as_csv(individual_metadata)
+            print(f'Converted individual_metadata excel to csv.')
+
+        print(f'\nProcessing individual_metadata file: {individual_metadata}')
         with Path(individual_metadata).open() as individual_m_file:
             response = ImportApi().import_individual_metadata_manifest(
                 file=individual_m_file,
@@ -48,7 +70,11 @@ def main(
             print('API response:', response)
 
     if family_metadata:
-        print(f"\nProcessing family_metadata file: {family_metadata}")
+        if family_metadata.endswith('xlsx'):
+            family_metadata = save_xlsx_as_csv(family_metadata)
+            print(f'Converted family_metadata excel to csv.')
+
+        print(f'\nProcessing family_metadata file: {family_metadata}')
         with Path(family_metadata).open() as fam_m_file:
             response = FamilyApi().import_families(
                 file=fam_m_file, project=project, has_header=True
