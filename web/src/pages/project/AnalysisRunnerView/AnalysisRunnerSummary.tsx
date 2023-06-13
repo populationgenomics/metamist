d3f7622bfc6e064de1428c294fc6ba26e8b3290b
danielreti <47542969+danielreti@users.noreply.github.com>
2023-06-13 14:08:22 +1000

    Add mode as a column in Analysis Grid (#496)
    * handle blank batch field + make cromwell mode more prominent
    
    * remove console log

--- web/src/pages/project/AnalysisRunnerView/AnalysisRunnerSummary.tsx
+++ web/src/pages/project/AnalysisRunnerView/AnalysisRunnerSummary.tsx
@@ -86,7 +86,9 @@ const AnalysisRunnerSummary: React.FunctionComponent = () => {
         output,
         ...meta,
         position: i,
-        'Hail Batch': meta?.batch_url.replace('https://batch.hail.populationgenomics.org.au/', ''),
+        'Hail Batch': meta?.batch_url
+            ? meta?.batch_url.replace('https://batch.hail.populationgenomics.org.au/', '')
+            : '',
         GitHub: `${meta?.repo}@${meta?.commit.substring(0, 7)}`,
         Date: `${parseDate(sanitiseValue(meta?.timestamp))}`,
         Author: `${parseEmail(sanitiseValue(author))}`,
