d30e0e2c812427b8c9a6240dfb6ebb5266d67df4
Michael Franklin <michael.franklin@populationgenomics.org.au>
2023-06-16 08:26:20 +1000

    Bump version: 6.0.3 → 6.0.4

--- api/server.py
+++ api/server.py
@@ -20,7 +20,7 @@ from api.graphql.schema import MetamistGraphQLRouter  # type: ignore
 from api.settings import PROFILE_REQUESTS, SKIP_DATABASE_CONNECTION
 
 # This tag is automatically updated by bump2version
-_VERSION = '6.0.3'
+_VERSION = '6.0.4'
 
 logger = get_logger()
 
