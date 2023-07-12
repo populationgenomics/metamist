2252b2786a985c39506850b66c67788f757357dd
vivbak <vivian.bakiris@populationgenomics.org.au>
2023-07-12 14:15:22 +1000

    add family sg function + silence the linter

--- scripts/create_test_subset.py
+++ scripts/create_test_subset.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# pylint: disable=too-many-instance-attributes,too-many-locals,unused-argument,wrong-import-order,unused-argument,too-many-arguments
+# pylint: disable=too-many-instance-attributes,too-many-locals,too-many-arguments
 
 """ Example Invocation
 
@@ -11,13 +11,12 @@ scripts/create_test_subset.py --project acute-care --families 4
 This example will populate acute-care-test with the metamist data for 4 families.
 """
 
-from typing import Optional
+from typing import Optional, Counter as CounterType
 import logging
 import os
 import random
 import subprocess
 import traceback
-import typing
 from collections import Counter
 import csv
 
@@ -39,6 +38,8 @@ from metamist.models import (
     Analysis,
     AnalysisStatus,
 )
+
+from metamist.graphql import gql, query
 
 logger = logging.getLogger(__file__)
 logging.basicConfig(format='%(levelname)s (%(name)s %(lineno)s): %(message)s')
@@ -99,6 +100,13 @@ DEFAULT_SAMPLES_N = 10
     This is in addition to the number of families specified in
     --families and the number of samples specified in -n""",
 )
+@click.option(
+    '--noninteractive',
+    'noninteractive',
+    is_flag=True,
+    default=False,
+    help='Skip interactive confirmation',
+)
 def main(
     project: str,
     samples_n: Optional[int],
@@ -106,13 +114,14 @@ def main(
     skip_ped: Optional[bool] = True,
     additional_families: Optional[tuple[str]] = None,
     additional_samples: Optional[tuple[str]] = None,
+    noninteractive: Optional[bool] = False,
 ):
     """
     Script creates a test subset for a given project.
     A new project with a prefix -test is created, and for any files in sample/meta,
     sequence/meta, or analysis/output a copy in the -test namespace is created.
     """
-    samples_n, families_n = _validate_opts(samples_n, families_n)
+    samples_n, families_n = _validate_opts(samples_n, families_n, noninteractive)
     _additional_families: list[str] = list(additional_families)
     _additional_samples: list[str] = list(additional_samples)
 
@@ -123,7 +132,7 @@ def main(
         }
     )
     logger.info(f'Found {len(all_samples)} samples')
-    if samples_n and samples_n >= len(all_samples):
+    if (samples_n and samples_n >= len(all_samples)) and not noninteractive:
         resp = str(
             input(
                 f'Requesting {samples_n} samples which is >= '
@@ -225,13 +234,14 @@ def main(
     for a_type, analysis_by_sid in analysis_by_sid_by_type.items():
         try:
             # TODO: fix this
-            analyses: list[
-                dict[str, str]
-            ] = aapi.get_latest_analysis_for_samples_and_type(
-                project=project,
-                analysis_type=str(a_type),
-                request_body=sample_ids,
-            )
+            # analyses: list[
+            #     dict[str, str]
+            # ] = aapi.get_latest_analysis_for_samples_and_type(
+            #     project=project,
+            #     analysis_type=str(a_type),
+            #     request_body=sample_ids,
+            # )
+            analyses: list[dict[str, str]] = []
         except exceptions.ApiException:
             traceback.print_exc()
         else:
@@ -442,8 +452,7 @@ def get_map_ipid_esid(
 
     ip_es_map = []
     for ip_is_pair in ip_is_map:
-        samples_per_participant = []
-        samples_per_participant.append(ip_is_pair[0])
+        samples_per_participant = [ip_is_pair[0]]
         for isid in ip_is_pair[1:]:
             if isid in is_es_map:
                 samples_per_participant.append(is_es_map[isid])
@@ -455,10 +464,9 @@ def get_map_ipid_esid(
     return external_sample_internal_participant_map
 
 
-def get_samples_for_families(project: str, additional_families: list[str]):
+def get_samples_for_families(project: str, additional_families: list[str]) -> list[str]:
     """Returns the samples that belong to a list of families"""
 
-    samples: list[str] = []
     full_pedigree = fapi.get_pedigree(
         project=project,
         replace_with_participant_external_ids=False,
@@ -479,7 +487,7 @@ def get_samples_for_families(project: str, additional_families: list[str]):
         }
     )
 
-    samples = [sample['id'] for sample in sample_objects]
+    samples: list[str] = [sample['id'] for sample in sample_objects]
 
     return samples
 
@@ -487,9 +495,8 @@ def get_samples_for_families(project: str, additional_families: list[str]):
 def get_fams_for_samples(
     project: str,
     additional_samples: Optional[list[str]] = None,
-):
+) -> list[str]:
     """Returns the families that a list of samples belong to"""
-    fams: set[str] = set()
     sample_objects = sapi.get_samples(
         body_get_samples={
             'project_ids': [project],
@@ -505,7 +512,7 @@ def get_fams_for_samples(
         replace_with_family_external_ids=True,
     )
 
-    fams = {
+    fams: set[str] = {
         fam['family_id'] for fam in full_pedigree if str(fam['individual_id']) in pids
     }
 
@@ -526,7 +533,7 @@ def _normalise_map(unformatted_map: list[list[str]]) -> dict[str, str]:
 
 
 def _validate_opts(
-    samples_n: int, families_n: int
+    samples_n: int, families_n: int, noninteractive: bool
 ) -> tuple[Optional[int], Optional[int]]:
     if samples_n is not None and families_n is not None:
         raise click.BadParameter('Please specify only one of --samples or --families')
@@ -541,7 +548,7 @@ def _validate_opts(
     if families_n is not None and families_n < 1:
         raise click.BadParameter('Please specify --families higher than 0')
 
-    if families_n is not None and families_n >= 30:
+    if (families_n is not None and families_n >= 30) and not noninteractive:
         resp = str(
             input(
                 f'You requested a subset of {families_n} families. '
@@ -551,7 +558,7 @@ def _validate_opts(
         if resp.lower() != 'y':
             raise SystemExit()
 
-    if samples_n is not None and samples_n >= 100:
+    if (samples_n is not None and samples_n >= 100) and not noninteractive:
         resp = str(
             input(
                 f'You requested a subset of {samples_n} samples. '
@@ -565,7 +572,7 @@ def _validate_opts(
 
 def _print_fam_stats(families: list[dict[str, str]]):
     family_sizes = Counter([fam['family_id'] for fam in families])
-    fam_by_size: typing.Counter[int] = Counter()
+    fam_by_size: CounterType[int] = Counter()
     # determine number of singles, duos, trios, etc
     for fam in family_sizes:
         fam_by_size[family_sizes[fam]] += 1
@@ -725,6 +732,80 @@ def file_exists(path: str) -> bool:
         gs = storage.Client()
         return gs.get_bucket(bucket).get_blob(path)
     return os.path.exists(path)
+
+
+def _get_sgs_for_families(
+    project: str,
+    families_n: int,
+    additional_families,
+) -> list[str]:
+    """Returns specific sequencing groups to be included in the test project."""
+
+    included_sgids: list = []
+    _num_families_to_subset: int = None
+    _randomly_selected_families: list = []
+
+    # Case 1: If neither families_n nor _additional_families
+    if not families_n and not additional_families:
+        return included_sgids
+
+    # Case 2: If families_n but not _additional_families
+    if families_n and not additional_families:
+        _num_families_to_subset = families_n
+
+    # Case 3: If both families_n and _additional_families
+    if families_n and additional_families:
+        _num_families_to_subset = families_n - len(additional_families)
+
+    QUERY_FAMILY_SGID = gql(
+        """
+        query FamilyQuery($project: String!) {
+            project(name: $project) {
+                families {
+                    id
+                    externalId
+                    participants {
+                        samples {
+                            sequencingGroups {
+                                id
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+    """
+    )
+
+    family_sgid_output = query(QUERY_FAMILY_SGID, {'project': project})
+
+    # 1. Remove the families in _families_to_subset
+    all_family_sgids = family_sgid_output.get('project').get('families')
+    _filtered_family_sgids = [
+        fam for fam in all_family_sgids if fam['externalId'] not in additional_families
+    ]
+    _user_input_families = [
+        fam for fam in all_family_sgids if fam['externalId'] in additional_families
+    ]
+
+    # 2. Randomly select _num_families_to_subset from the remaining families
+    if _num_families_to_subset:
+        _randomly_selected_families = random.sample(
+            _filtered_family_sgids, _num_families_to_subset
+        )
+
+    # 3. Combine the families in _families_to_subset with the randomly selected families & return sequencing group ids
+
+    _all_families_to_subset = _randomly_selected_families + _user_input_families
+
+    for fam in _all_families_to_subset:
+        for participant in fam['participants']:
+            for sample in participant['samples']:
+                for sg in sample['sequencingGroups']:
+                    included_sgids.append(sg['id'])
+
+    return included_sgids
 
 
 if __name__ == '__main__':
