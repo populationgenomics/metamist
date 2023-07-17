6ad453415b85917f5d6815aea5d68524a5d049f6
Daniel <iamdanielreti@gmail.com>
2023-07-17 11:06:00 +1000

    Allows uses to filter on sample ID within a project

--- api/graphql/schema.py
+++ api/graphql/schema.py
@@ -144,14 +144,18 @@ class GraphQLProject:
         root: 'GraphQLProject',
         type: GraphQLFilter[str] | None = None,
         external_id: GraphQLFilter[str] | None = None,
+        id: GraphQLFilter[str] | None = None,
         meta: GraphQLMetaFilter | None = None,
     ) -> list['GraphQLSample']:
         loader = info.context[LoaderKeys.SAMPLES_FOR_PROJECTS]
+        print(id)
         filter_ = SampleFilter(
             type=type.to_internal_filter() if type else None,
             external_id=external_id.to_internal_filter() if external_id else None,
+            id=id.to_internal_filter(sample_id_transform_to_raw) if id else None,
             meta=meta,
         )
+        print(filter_)
         samples = await loader.load({'id': root.id, 'filter': filter_})
         return [GraphQLSample.from_internal(p) for p in samples]
 
@@ -366,6 +370,7 @@ class GraphQLSample:
 
     @staticmethod
     def from_internal(sample: SampleInternal):
+        print(sample.id)
         return GraphQLSample(
             id=sample_id_format(sample.id),
             external_id=sample.external_id,
