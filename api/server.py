71473693222dffe3edd47961d463fd3183d567b9
Michael Franklin <illusional@users.noreply.github.com>
2023-10-19 08:05:06 +1100

    Bump version: 6.3.2 → 6.4.0

--- api/server.py
+++ api/server.py
@@ -20,7 +20,7 @@ from api.graphql.schema import MetamistGraphQLRouter  # type: ignore
 from api.settings import PROFILE_REQUESTS, SKIP_DATABASE_CONNECTION
 
 # This tag is automatically updated by bump2version
-_VERSION = '6.3.2'
+_VERSION = '6.4.0'
 
 logger = get_logger()
 
