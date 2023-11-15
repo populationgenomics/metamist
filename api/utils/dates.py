d69ae5e0fea4c96fe7e1003446ecb685a7324f7b
Sabrina Yan <9669990+violetbrina@users.noreply.github.com>
2023-11-15 13:29:34 +1100

    FIX: additional get_invoice_month_range comment detail

--- api/utils/dates.py
+++ api/utils/dates.py
@@ -15,7 +15,16 @@ def parse_date_only_string(d: str | None) -> date | None:
 
 
 def get_invoice_month_range(convert_month: date) -> tuple[date, date]:
-    """Get the start and end date of the invoice month for a given date"""
+    """
+    Get the start and end date of the invoice month for a given date
+
+    Specifically it will return the start and end date of the invoice month passed in.
+    This enables us to optimise queries where the main date filtering is by
+    invoice_month. Of course the partiontioning is not by invoice_month, it's by day.
+
+    So given an invoice_month, return an start and end day to filter on the partition
+    to reduce query costs and time.
+    """
     first_day = convert_month.replace(day=1)
 
     # Grab the first day of invoice month then subtract INVOICE_DAY_DIFF days
@@ -27,10 +36,6 @@ def get_invoice_month_range(convert_month: date) -> tuple[date, date]:
         next_month = first_day.replace(month=convert_month.month + 1)
 
     # Grab the last day of invoice month then add INVOICE_DAY_DIFF days
-    last_day = (
-        next_month
-        + timedelta(days=-1)
-        + timedelta(days=INVOICE_DAY_DIFF)
-    )
+    last_day = next_month + timedelta(days=-1) + timedelta(days=INVOICE_DAY_DIFF)
 
     return start_day, last_day
