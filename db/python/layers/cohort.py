57afc9fa5ef1f57f04fd5a1f489c0c3f8b089938
Daniel Esposito <7043686+daniaki@users.noreply.github.com>
2023-11-27 11:41:34 +1100

    cohort layer update

--- db/python/layers/cohort.py
+++ db/python/layers/cohort.py
@@ -8,6 +8,7 @@ from db.python.tables.project import ProjectId
 from db.python.tables.sample import SampleTable
 from db.python.utils import get_logger
 from models.models.cohort import Cohort
+from models.models.sequencing_group import SequencingGroupInternal
 
 logger = get_logger()
 
@@ -25,18 +26,38 @@ class CohortLayer(BaseLayer):
     # GETS
 
     async def query(self, filter_: CohortFilter) -> list[Cohort]:
-        """ Query Cohorts"""
+        """Query Cohorts"""
         cohorts = await self.ct.query(filter_)
         return cohorts
+
+    async def get_cohort_sequencing_group_ids(self, cohort_id: int) -> list[int]:
+        """
+        Get the sequencing group IDs for the given cohort.
+        """
+        return await self.ct.get_cohort_sequencing_group_ids(cohort_id)
+
+    # PUTS
 
     async def create_cohort(
         self,
         project: ProjectId,
         cohort_name: str,
         sequencing_group_ids: list[str],
+        author: str,
         description: str,
-        author: str = None,
-    ) -> dict[str, Any]:
-        """Create a new cohort"""
-        output = await self.ct.create_cohort(project=project, cohort_name=cohort_name, sequencing_group_ids=sequencing_group_ids, description=description, author=author)
-        return {'cohort_id': cohort_name, 'sg': sequencing_group_ids, 'output': output}
+        derived_from: int | None = None,
+    ) -> int:
+        """
+        Create a new cohort from the given parameters. Returns the newly created cohort_id.
+        """
+
+        cohort_id = await self.ct.create_cohort(
+            project=project,
+            cohort_name=cohort_name,
+            sequencing_group_ids=sequencing_group_ids,
+            description=description,
+            author=author,
+            derived_from=derived_from,
+        )
+
+        return cohort_id
