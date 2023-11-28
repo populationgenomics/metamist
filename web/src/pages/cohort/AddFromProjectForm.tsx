adce34b3d7707f9be540597add2db3ef886b7230
Daniel Esposito <7043686+daniaki@users.noreply.github.com>
2023-11-28 11:27:26 +1100

    Add GQL query to project form

--- web/src/pages/cohort/AddFromProjectForm.tsx
+++ web/src/pages/cohort/AddFromProjectForm.tsx
@@ -1,141 +1,207 @@
 import React, { useState, useContext } from 'react'
 import { Container, Form } from 'semantic-ui-react'
-import { useQuery } from '@apollo/client'
+import { useLazyQuery } from '@apollo/client'
 
+import { uniqBy } from 'lodash'
 import { gql } from '../../__generated__/gql'
 import { ThemeContext } from '../../shared/components/ThemeProvider'
 import MuckError from '../../shared/components/MuckError'
 
 import SequencingGroupTable from './SequencingGroupTable'
-import { SequencingGroup } from './types'
-import { setSearchParams } from '../../sm-api/common'
+import { Project, SequencingGroup } from './types'
+import { FetchSequencingGroupsQueryVariables } from '../../__generated__/graphql'
 
-const GET_PROJECTS_QUERY = gql(`
-query VisibleProjects($activeOnly: Boolean!) {
-    myProjects {
-        id
-        name
-        sequencingGroups(activeOnly: {eq: $activeOnly}) {
-            id
-            type
-            technology
-            platform
+const GET_SEQUENCING_GROUPS_QUERY = gql(`
+query FetchSequencingGroups(
+    $project: String!, 
+    $platform: String, 
+    $technology: String, 
+    $seqType: String,
+    $assayMeta: JSON
+  ) {
+    project(name: $project) {
+      participants {
+        samples {
+          assays(meta: $assayMeta) {
+            sample {
+              sequencingGroups(
+                platform: {icontains: $platform}
+                technology: {icontains: $technology}
+                type: {contains: $seqType}
+              ) {
+                id
+                type
+                technology
+                platform
+              }
+            }
+          }
         }
+      }
     }
-}`)
+  }
+`)
+
+// NOTE: Put additional objects here to add more search fields for assay metadata
+const assayMetaSearchFields = [
+    { label: 'Batch', id: 'batch', searchVariable: 'batch' },
+    { label: 'Emoji', id: 'emoji', searchVariable: 'emoji' },
+]
 
 interface IAddFromProjectForm {
+    projects: Project[]
     onAdd: (sequencingGroups: SequencingGroup[]) => void
 }
 
-const AddFromProjectForm: React.FC<IAddFromProjectForm> = ({ onAdd }) => {
-    const [sequencingGroups, setSequencingGroups] = useState<SequencingGroup[]>([])
+const AddFromProjectForm: React.FC<IAddFromProjectForm> = ({ projects, onAdd }) => {
+    const [selectedProject, setSelectedProject] = useState<Project>()
     const [searchHits, setSearchHits] = useState<SequencingGroup[]>([])
 
     const { theme } = useContext(ThemeContext)
     const inverted = theme === 'dark-mode'
 
     // Load all available projects and associated data for this user
-    const { loading, error, data } = useQuery(GET_PROJECTS_QUERY, {
-        variables: { activeOnly: true },
-    })
+    const [searchSquencingGroups, { loading, error }] = useLazyQuery(GET_SEQUENCING_GROUPS_QUERY)
 
-    if (loading) {
-        return <div>Loading project form...</div>
+    const search = () => {
+        const seqTypeInput = document.getElementById('seq_type') as HTMLInputElement
+        const technologyInput = document.getElementById('technology') as HTMLInputElement
+        const platformInput = document.getElementById('platform') as HTMLInputElement
+
+        if (!selectedProject?.name) {
+            return
+        }
+
+        const searchParams: FetchSequencingGroupsQueryVariables = {
+            project: selectedProject.name,
+            seqType: null,
+            technology: null,
+            platform: null,
+            assayMeta: {},
+        }
+
+        if (seqTypeInput?.value) {
+            searchParams.seqType = seqTypeInput.value
+        }
+
+        if (technologyInput?.value) {
+            searchParams.technology = technologyInput.value
+        }
+
+        if (platformInput?.value) {
+            searchParams.platform = platformInput.value
+        }
+
+        assayMetaSearchFields.forEach((field) => {
+            const input = document.getElementById(field.id) as HTMLInputElement
+            if (input?.value) {
+                searchParams.assayMeta[field.searchVariable] = input.value
+            }
+        })
+
+        searchSquencingGroups({
+            variables: {
+                project: selectedProject.name,
+                seqType: searchParams.seqType,
+                technology: searchParams.technology,
+                platform: searchParams.platform,
+                assayMeta: searchParams.assayMeta,
+            },
+
+            onCompleted: (hits) => {
+                const samples = hits.project.participants.flatMap((p) => p.samples)
+                const assays = samples.flatMap((s) => s.assays)
+
+                const sgs = assays.flatMap((a) =>
+                    a.sample.sequencingGroups.map((sg) => ({
+                        id: sg.id,
+                        type: sg.type,
+                        technology: sg.technology,
+                        platform: sg.platform,
+                        project: { id: selectedProject.id, name: selectedProject.name },
+                    }))
+                )
+
+                setSearchHits(uniqBy(sgs, 'id'))
+            },
+        })
     }
+
+    const projectOptions = projects.map((project) => ({
+        key: project.id,
+        value: project.id,
+        text: project.name,
+    }))
 
     if (error) {
         return <MuckError message={error.message} />
     }
 
-    const projectOptions: { key: any; value: any; text: any }[] | undefined = []
-    if (!loading && data?.myProjects) {
-        data.myProjects.forEach((project) => {
-            projectOptions.push({ key: project.id, value: project.id, text: project.name })
-        })
-    }
-
-    const search = () => {
-        const seqType = (document.getElementById('seq_type') as HTMLInputElement)?.value?.trim()
-        const technology = (
-            document.getElementById('technology') as HTMLInputElement
-        )?.value?.trim()
-        const platform = (document.getElementById('platform') as HTMLInputElement)?.value?.trim()
-        // const batch = (document.getElementById('batch') as HTMLInputElement)?.value?.trim()
-
-        const hits = sequencingGroups
-        setSearchHits(
-            hits.filter(
-                (sg: SequencingGroup) =>
-                    (!seqType || sg.type.toLowerCase().includes(seqType.toLowerCase())) &&
-                    (!technology ||
-                        sg.technology.toLowerCase().includes(technology.toLowerCase())) &&
-                    (!platform || sg.platform.toLowerCase().includes(platform.toLowerCase()))
-            )
-        )
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const updateSequencingGroupSelection = (_: any, d: any) => {
-        if (!data?.myProjects) {
-            setSequencingGroups([])
-            return
-        }
-
-        const newSequencingGroups: SequencingGroup[] = []
-        data.myProjects.forEach((project) => {
-            if (d.value.includes(project.id)) {
-                project.sequencingGroups.forEach((sg: SequencingGroup) => {
-                    newSequencingGroups.push({
-                        id: sg.id,
-                        type: sg.type,
-                        technology: sg.technology,
-                        platform: sg.platform,
-                        project: { id: project.id, name: project.name },
-                    })
-                })
-            }
-        })
-
-        setSequencingGroups(newSequencingGroups)
-    }
-
     return (
         <Form inverted={inverted}>
             <h3>Project</h3>
-            <p>Include Sequencing Groups from the following projects</p>
+            <p>Include Sequencing Groups from the following project</p>
             <Form.Dropdown
                 placeholder="Select Projects"
                 fluid
-                multiple
                 selection
-                onChange={updateSequencingGroupSelection}
+                onChange={(_, d) => {
+                    const project = projects.find((p) => p.id === d.value)
+                    if (!project) return
+                    setSelectedProject({ id: project.id, name: project.name })
+                }}
                 options={projectOptions}
             />
 
             <p>
-                Including the Sequencing Groups which match the following criteria (leave blank to
-                include all Sequencing Groups)
+                Matching the following search criteria (leave blank to include all Sequencing
+                Groups)
             </p>
             <Container>
-                <Form.Input label="Sequencing Type" name="seq_type" id="seq_type" />
-                <Form.Input label="Technology" name="technology" id="technology" />
-                <Form.Input label="Platform" name="platform" id="platform" />
-                <Form.Input label="Assay Batch" name="batch" id="batch" />
+                <Form.Input label="Sequencing Type" id="seq_type" />
+                <Form.Input label="Technology" id="technology" />
+                <Form.Input label="Platform" id="platform" />
+                {assayMetaSearchFields.map((field) => (
+                    <Form.Input label={field.label} id={field.id} key={field.id} />
+                ))}
             </Container>
             <br />
             <Form.Group>
-                <Form.Button type="button" content="Search" onClick={search} />
                 <Form.Button
                     type="button"
+                    disabled={selectedProject == null}
+                    content="Search"
+                    onClick={search}
+                />
+                <Form.Button
+                    type="button"
+                    disabled={
+                        loading ||
+                        error != null ||
+                        selectedProject == null ||
+                        searchHits.length === 0
+                    }
                     content="Add"
                     onClick={() => {
-                        onAdd(searchHits)
+                        // eslint-disable-next-line no-alert
+                        const proceed = window.confirm(
+                            'This will add all Sequencing Groups in the table to your Cohort. ' +
+                                'Sequencing Groups hidden by the interactive table search will ' +
+                                'also be added. Do you wish to continue?'
+                        )
+                        if (proceed) {
+                            onAdd(searchHits)
+                            setSearchHits([])
+                        }
                     }}
                 />
             </Form.Group>
             <br />
-            <SequencingGroupTable editable={false} sequencingGroups={searchHits} />
+            {loading ? (
+                <div>Finding Sequencing Groups...</div>
+            ) : (
+                <SequencingGroupTable editable={false} sequencingGroups={searchHits} />
+            )}
         </Form>
     )
 }
