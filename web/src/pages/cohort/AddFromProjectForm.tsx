d6017cb19e7476230792c8501b95d1cc9a06e1ab
Daniel Esposito <7043686+daniaki@users.noreply.github.com>
2023-11-28 13:06:35 +1100

    merge dupe sgs from search results

--- web/src/pages/cohort/AddFromProjectForm.tsx
+++ web/src/pages/cohort/AddFromProjectForm.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useContext } from 'react'
 import { Container, Form } from 'semantic-ui-react'
 import { useLazyQuery } from '@apollo/client'
+import { uniq } from 'lodash'
 
-import { uniqBy } from 'lodash'
 import { gql } from '../../__generated__/gql'
 import { ThemeContext } from '../../shared/components/ThemeProvider'
 import MuckError from '../../shared/components/MuckError'
@@ -45,7 +45,7 @@ query FetchSequencingGroups(
 // NOTE: Put additional objects here to add more search fields for assay metadata
 const assayMetaSearchFields = [
     { label: 'Batch', id: 'batch', searchVariable: 'batch' },
-    { label: 'Emoji', id: 'emoji', searchVariable: 'emoji' },
+    // { label: 'Emoji', id: 'emoji', searchVariable: 'emoji' },
 ]
 
 interface IAddFromProjectForm {
@@ -122,7 +122,31 @@ const AddFromProjectForm: React.FC<IAddFromProjectForm> = ({ projects, onAdd }) 
                     }))
                 )
 
-                setSearchHits(uniqBy(sgs, 'id'))
+                // Remove duplicates by merging string fields with same id
+                const merged: SequencingGroup[] = []
+                const seen = new Set()
+                sgs.forEach((sg) => {
+                    if (!seen.has(sg.id)) {
+                        merged.push(sg)
+                        seen.add(sg.id)
+                    } else {
+                        const existing = merged.find((e) => e.id === sg.id)
+                        if (existing) {
+                            existing.type = `${existing.type}|${sg.type}`
+                            existing.technology = `${existing.technology}|${sg.technology}`
+                            existing.platform = `${existing.platform}|${sg.platform}`
+                        }
+                    }
+                })
+
+                setSearchHits(
+                    merged.map((sg) => ({
+                        ...sg,
+                        type: uniq(sg.type.split('|')).sort().join(' | '),
+                        technology: uniq(sg.technology.split('|')).sort().join(' | '),
+                        platform: uniq(sg.platform.split('|')).sort().join(' | '),
+                    }))
+                )
             },
         })
     }
@@ -169,6 +193,7 @@ const AddFromProjectForm: React.FC<IAddFromProjectForm> = ({ projects, onAdd }) 
             <Form.Group>
                 <Form.Button
                     type="button"
+                    loading={loading}
                     disabled={selectedProject == null}
                     content="Search"
                     onClick={search}
