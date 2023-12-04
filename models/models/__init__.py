0172dffbd3151d8387961ef9409e1a876ad801c6
Daniel Esposito <7043686+daniaki@users.noreply.github.com>
2023-12-04 11:03:04 +1100

    formatting

--- models/models/__init__.py
+++ models/models/__init__.py
@@ -1,34 +1,66 @@
-from models.models.analysis import (Analysis, AnalysisInternal, DateSizeModel,
-                                    ProjectSizeModel, ProportionalDateModel,
-                                    ProportionalDateProjectModel,
-                                    ProportionalDateTemporalMethod,
-                                    SequencingGroupSizeModel)
-from models.models.assay import (Assay, AssayInternal, AssayUpsert,
-                                 AssayUpsertInternal)
-from models.models.billing import (BillingColumn, BillingRowRecord,
-                                   BillingTotalCostQueryModel,
-                                   BillingTotalCostRecord)
+from models.models.analysis import (
+    Analysis,
+    AnalysisInternal,
+    DateSizeModel,
+    ProjectSizeModel,
+    ProportionalDateModel,
+    ProportionalDateProjectModel,
+    ProportionalDateTemporalMethod,
+    SequencingGroupSizeModel,
+)
+from models.models.assay import Assay, AssayInternal, AssayUpsert, AssayUpsertInternal
+from models.models.billing import (
+    BillingColumn,
+    BillingRowRecord,
+    BillingTotalCostQueryModel,
+    BillingTotalCostRecord,
+)
 from models.models.cohort import Cohort
-from models.models.family import (Family, FamilyInternal, FamilySimple,
-                                  FamilySimpleInternal, PedRowInternal)
-from models.models.participant import (NestedParticipant,
-                                       NestedParticipantInternal, Participant,
-                                       ParticipantInternal, ParticipantUpsert,
-                                       ParticipantUpsertInternal)
+from models.models.family import (
+    Family,
+    FamilyInternal,
+    FamilySimple,
+    FamilySimpleInternal,
+    PedRowInternal,
+)
+from models.models.participant import (
+    NestedParticipant,
+    NestedParticipantInternal,
+    Participant,
+    ParticipantInternal,
+    ParticipantUpsert,
+    ParticipantUpsertInternal,
+)
 from models.models.project import Project
-from models.models.sample import (NestedSample, NestedSampleInternal, Sample,
-                                  SampleInternal, SampleUpsert,
-                                  SampleUpsertInternal)
-from models.models.search import (ErrorResponse, FamilySearchResponseData,
-                                  ParticipantSearchResponseData,
-                                  SampleSearchResponseData, SearchItem,
-                                  SearchResponse, SearchResponseData,
-                                  SequencingGroupSearchResponseData)
-from models.models.sequencing_group import (NestedSequencingGroup,
-                                            NestedSequencingGroupInternal,
-                                            SequencingGroup,
-                                            SequencingGroupInternal,
-                                            SequencingGroupUpsert,
-                                            SequencingGroupUpsertInternal)
-from models.models.web import (PagingLinks, ProjectSummary,
-                               ProjectSummaryInternal, WebProject)
+from models.models.sample import (
+    NestedSample,
+    NestedSampleInternal,
+    Sample,
+    SampleInternal,
+    SampleUpsert,
+    SampleUpsertInternal,
+)
+from models.models.search import (
+    ErrorResponse,
+    FamilySearchResponseData,
+    ParticipantSearchResponseData,
+    SampleSearchResponseData,
+    SearchItem,
+    SearchResponse,
+    SearchResponseData,
+    SequencingGroupSearchResponseData,
+)
+from models.models.sequencing_group import (
+    NestedSequencingGroup,
+    NestedSequencingGroupInternal,
+    SequencingGroup,
+    SequencingGroupInternal,
+    SequencingGroupUpsert,
+    SequencingGroupUpsertInternal,
+)
+from models.models.web import (
+    PagingLinks,
+    ProjectSummary,
+    ProjectSummaryInternal,
+    WebProject,
+)
