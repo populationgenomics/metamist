7a3611cf6e0f97ad8f2a2e3b2f0f381e01e7f501
Daniel Esposito <7043686+daniaki@users.noreply.github.com>
2023-12-04 12:41:33 +1100

    Tmp fix for billing query inside NavBar

--- web/src/shared/components/Header/NavBar.tsx
+++ web/src/shared/components/Header/NavBar.tsx
@@ -2,14 +2,10 @@ import * as React from 'react'
 import { Link } from 'react-router-dom'
 import { Menu, Dropdown, Popup } from 'semantic-ui-react'
 
-import { BillingApi } from '../../../sm-api'
-
 // this wasn't working, so added import to HTML
 // import 'bootstrap/dist/css/bootstrap.min.css'
-import Searchbar from './Search'
-import MuckTheDuck from '../MuckTheDuck'
-import SwaggerIcon from '../SwaggerIcon'
-import ConstructionIcon from '@mui/icons-material/Construction';
+
+import ConstructionIcon from '@mui/icons-material/Construction'
 import HomeIcon from '@mui/icons-material/Home'
 import ExploreIcon from '@mui/icons-material/Explore'
 import InsightsIcon from '@mui/icons-material/Insights'
@@ -17,64 +13,88 @@ import TableRowsIcon from '@mui/icons-material/TableRows'
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney'
 import DescriptionIcon from '@mui/icons-material/Description'
 import TroubleshootIcon from '@mui/icons-material/Troubleshoot'
+import CodeIcon from '@mui/icons-material/Code'
 import DarkModeTriButton from './DarkModeTriButton/DarkModeTriButton'
 
+// import { BillingApi } from '../../../sm-api'
+
+import MuckTheDuck from '../MuckTheDuck'
+import SwaggerIcon from '../SwaggerIcon'
 import { ThemeContext } from '../ThemeProvider'
+
+import Searchbar from './Search'
 
 import './NavBar.css'
 
-const billingPages = {
-    title: 'Billing',
-    url: '/billing',
-    icon: <AttachMoneyIcon />,
-    submenu: [
-        {
-            title: 'Home',
-            url: '/billing',
-            icon: <HomeIcon />,
-        },
-        {
-            title: 'Invoice Month Cost',
-            url: '/billing/invoiceMonthCost',
-            icon: <TableRowsIcon />,
-        },
-        {
-            title: 'Cost By Time',
-            url: '/billing/costByTime',
-            icon: <TableRowsIcon />,
-        },
-        {
-            title: 'Seqr Prop Map',
-            url: '/billing/seqrPropMap',
-            icon: <TableRowsIcon />,
-        },
-    ],
-}
+// FIXME: Billing pages API query takes a long time on load and locks up the entire application
+// so I'm commenting this query out for now until it becomes faster. See below for the useEffect
+// where this query is performed. In general, I think we should avoid making API calls in the
+// NavBar unless they are absolutely necessary.
 
-interface MenuItem {
+// const billingPages = {
+//     title: 'Billing',
+//     url: '/billing',
+//     icon: <AttachMoneyIcon />,
+//     submenu: [
+//         {
+//             title: 'Home',
+//             url: '/billing',
+//             icon: <HomeIcon />,
+//         },
+//         {
+//             title: 'Invoice Month Cost',
+//             url: '/billing/invoiceMonthCost',
+//             icon: <TableRowsIcon />,
+//         },
+//         {
+//             title: 'Cost By Time',
+//             url: '/billing/costByTime',
+//             icon: <TableRowsIcon />,
+//         },
+//         {
+//             title: 'Seqr Prop Map',
+//             url: '/billing/seqrPropMap',
+//             icon: <TableRowsIcon />,
+//         },
+//     ],
+// }
+
+interface MenuItemDetails {
     title: string
     url: string
     icon: JSX.Element
-    submenu?: MenuItem[]
+    external?: boolean
+    submenu?: MenuItemDetails[]
 }
+
 interface MenuItemProps {
     index: number
-    item: MenuItem
+    item: MenuItemDetails
 }
 
 const MenuItem: React.FC<MenuItemProps> = ({ index, item }) => {
     const theme = React.useContext(ThemeContext)
     const isDarkMode = theme.theme === 'dark-mode'
 
-    const dropdown = (item: MenuItem) => (
-        <Dropdown text={item.title} key={index}>
+    const dropdown = (i: MenuItemDetails) => (
+        <Dropdown text={i.title} key={index} simple>
             <Dropdown.Menu id="navDrop">
-                {item.submenu &&
-                    item.submenu.map((subitem, subindex) => (
-                        <Dropdown.Item as={Link} id="navItem" to={subitem.url} key={subindex}>
-                            {subitem.title}
-                        </Dropdown.Item>
-                    ))}
+                {i.submenu &&
+                    i.submenu.map((subitem, subindex) => {
+                        if (subitem.external) {
+                            return (
+                                <Dropdown.Item id="navItem" key={subindex}>
+                                    <a href={subitem.url}>{subitem.title}</a>
+                                </Dropdown.Item>
+                            )
+                        }
+
+                        return (
+                            <Dropdown.Item as={Link} id="navItem" to={subitem.url} key={subindex}>
+                                {subitem.title}
+                            </Dropdown.Item>
+                        )
+                    })}
             </Dropdown.Menu>
         </Dropdown>
     )
@@ -110,7 +130,7 @@ interface NavBarProps {
 }
 
 const NavBar: React.FC<NavBarProps> = ({ fixed }) => {
-    const [menuItems, setMenuItems] = React.useState<MenuItem[]>([
+    const menuItems: MenuItemDetails[] = [
         {
             title: 'Explore',
             url: '/project',
@@ -122,29 +142,128 @@ const NavBar: React.FC<NavBarProps> = ({ fixed }) => {
             icon: <InsightsIcon />,
         },
         {
-            title: 'Swagger',
-            url: '/swagger',
-            icon: <SwaggerIcon height={22} style={{ marginTop: '2px' }} />,
+            title: 'Cohort Builder',
+            url: '/cohort-builder',
+            icon: <ConstructionIcon />,
+        },
+        {
+            title: 'Billing',
+            url: '/billing',
+            icon: <AttachMoneyIcon />,
+            submenu: [
+                {
+                    title: 'Home',
+                    url: '/billing',
+                    icon: <HomeIcon />,
+                },
+                {
+                    title: 'Invoice Month Cost',
+                    url: '/billing/invoiceMonthCost',
+                    icon: <TableRowsIcon />,
+                },
+                {
+                    title: 'Cost By Time',
+                    url: '/billing/costByTime',
+                    icon: <TableRowsIcon />,
+                },
+                {
+                    title: 'Seqr Prop Map',
+                    url: '/billing/seqrPropMap',
+                    icon: <TableRowsIcon />,
+                },
+            ],
+        },
+        {
+            title: 'API',
+            url: '/api',
+            icon: <CodeIcon />,
+            submenu: [
+                {
+                    title: 'Swagger',
+                    url: '/swagger',
+                    icon: <SwaggerIcon height={22} style={{ marginTop: '2px' }} />,
+                },
+                {
+                    title: 'GraphQL',
+                    url: '/graphql',
+                    icon: <TroubleshootIcon />,
+                    external: true,
+                },
+            ],
         },
         {
             title: 'Docs',
             url: '/documentation',
             icon: <DescriptionIcon />,
         },
-        {
-            title: 'GraphQL',
-            url: '/graphql',
-            icon: <TroubleshootIcon />,
-        },
-    ])
+    ]
+    // const [menuItems, setMenuItems] = React.useState<MenuItemDetails[]>([
+    //     {
+    //         title: 'Explore',
+    //         url: '/project',
+    //         icon: <ExploreIcon />,
+    //     },
+    //     {
+    //         title: 'Analysis Runner',
+    //         url: '/analysis-runner',
+    //         icon: <InsightsIcon />,
+    //     },
+    //     {
+    //         title: 'Cohort Builder',
+    //         url: '/cohort-builder',
+    //         icon: <ConstructionIcon />,
+    //     },
+    //     {
+    //         title: 'Billing',
+    //         url: '/billing',
+    //         icon: <AttachMoneyIcon />,
+    //         submenu: [
+    //             {
+    //                 title: 'Home',
+    //                 url: '/billing',
+    //                 icon: <HomeIcon />,
+    //             },
+    //             {
+    //                 title: 'Invoice Month Cost',
+    //                 url: '/billing/invoiceMonthCost',
+    //                 icon: <TableRowsIcon />,
+    //             },
+    //             {
+    //                 title: 'Cost By Time',
+    //                 url: '/billing/costByTime',
+    //                 icon: <TableRowsIcon />,
+    //             },
+    //             {
+    //                 title: 'Seqr Prop Map',
+    //                 url: '/billing/seqrPropMap',
+    //                 icon: <TableRowsIcon />,
+    //             },
+    //         ],
+    //     },
+    //     {
+    //         title: 'Swagger',
+    //         url: '/swagger',
+    //         icon: <SwaggerIcon height={22} style={{ marginTop: '2px' }} />,
+    //     },
+    //     {
+    //         title: 'Docs',
+    //         url: '/documentation',
+    //         icon: <DescriptionIcon />,
+    //     },
+    //     {
+    //         title: 'GraphQL',
+    //         url: '/graphql',
+    //         icon: <TroubleshootIcon />,
+    //     },
+    // ])
 
-    React.useEffect(() => {
-        new BillingApi().getTopics().then((response) => {
-            if (response.status === 200) {
-                setMenuItems([...menuItems.slice(0, 2), billingPages, ...menuItems.slice(2)])
-            }
-        })
-    }, [])
+    // React.useEffect(() => {
+    //     new BillingApi().getTopics().then((response) => {
+    //         if (response.status === 200) {
+    //             setMenuItems([...menuItems.slice(0, 2), billingPages, ...menuItems.slice(2)])
+    //         }
+    //     })
+    // }, [])
 
     return (
         <header className="App-header">
