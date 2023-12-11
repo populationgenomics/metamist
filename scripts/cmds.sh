641c23726d4eacffc7468c08e746cb53b690fa8c
EddieLF <eddieformaini@gmail.com>
2023-12-11 12:10:32 +1100

    Update cmd script for ingestion with env variables

--- scripts/cmds.sh
+++ scripts/cmds.sh
@@ -1,9 +1,9 @@
-PROJECT="mito-mdt"
-SEARCH_PATH="gs://cpg-${PROJECT}-main-upload/"
-SUBDIR="2023-10-23"
+PROJECT="tob-wgs-test"
+SEARCH_PATH="gs://cpg-${PROJECT}/"
+SUBDIR="cram/nagim/"
 SEARCH_PATH+=$SUBDIR
 ROOT_PATH="/Users/edwfor/Code/metamist/ingestion_metadata/"
-DATA_DATE="2023-10-23"
+DATA_DATE="2023-12-08"
 FOLDER="${PROJECT}_${DATA_DATE}/"
 ROOT_PATH+="${FOLDER}"
 SAMPLE_MAPPING="${ROOT_PATH}sample_file_mapping.csv"
@@ -16,6 +16,12 @@ CRAM_REF="gs://cpg-common-main/references/hg38/v0/dragen_reference/Homo_sapiens_
 
 python generate_sample_file_map.py -i $SAMPLE_MAPPING -p $SEARCH_PATH -g > $MAPPING_FILE
 
-python parse_sample_file_map.py --project $PROJECT --search-path $SEARCH_PATH --ref $CRAM_REF --dry-run $MAPPING_FILE
+
+PROJECT="tob-wgs-test"
+SEARCH_PATH="gs://cpg-tob-wgs-test/cram/nagim/"
+MAPPING_FILE="sample_mapping.csv"
+CRAM_REF="gs://cpg-common-main/references/hg38/v0/dragen_reference/Homo_sapiens_assembly38_masked.fasta" # Might need to be in the test bucket?
+
+python parse_sample_file_map.py --project $PROJECT --search-path $SEARCH_PATH --ref $CRAM_REF --dry-run --allow-extra-files-in-search_path $MAPPING_FILE
 
 python parse_rd_metadata.py --project $PROJECT --ped-file $PED_FILE
