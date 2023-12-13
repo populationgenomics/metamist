fc014b64e80ac47bfbaa650fb64880705f0d0c7c
EddieLF <eddieformaini@gmail.com>
2023-12-14 10:08:06 +1100

    Fix cmds.sh file

--- scripts/cmds.sh
+++ scripts/cmds.sh
@@ -1,9 +1,10 @@
-PROJECT="tob-wgs-test"
-SEARCH_PATH="gs://cpg-${PROJECT}/"
-SUBDIR="cram/nagim/"
+SM_ENVIRONMENT=
+PROJECT="ibmdx"
+SEARCH_PATH="gs://cpg-${PROJECT}-main-upload/"
+SUBDIR="2023-12-08_transfer1/ibmdx_20231208_103034/"
 SEARCH_PATH+=$SUBDIR
 ROOT_PATH="/Users/edwfor/Code/metamist/ingestion_metadata/"
-DATA_DATE="2023-12-08"
+DATA_DATE="2023-12-12"
 FOLDER="${PROJECT}_${DATA_DATE}/"
 ROOT_PATH+="${FOLDER}"
 SAMPLE_MAPPING="${ROOT_PATH}sample_file_mapping.csv"
@@ -14,14 +15,8 @@ INDIVIDUAL_METADATA="${ROOT_PATH}individuals_metadata.csv"
 CRAM_REF="gs://cpg-common-main/references/hg38/v0/dragen_reference/Homo_sapiens_assembly38_masked.fasta"
 
 
-python generate_sample_file_map.py -i $SAMPLE_MAPPING -p $SEARCH_PATH -g > $MAPPING_FILE
+python generate_sample_file_map.py -i $SAMPLE_MAPPING -p $SEARCH_PATH > $MAPPING_FILE
 
+python parse_sample_file_map.py --project $PROJECT --search-path $SEARCH_PATH --allow-extra-files-in-search_path $MAPPING_FILE
 
-PROJECT="tob-wgs-test"
-SEARCH_PATH="gs://cpg-tob-wgs-test/cram/nagim/"
-MAPPING_FILE="sample_mapping.csv"
-CRAM_REF="gs://cpg-common-main/references/hg38/v0/dragen_reference/Homo_sapiens_assembly38_masked.fasta" # Might need to be in the test bucket?
-
-python parse_sample_file_map.py --project $PROJECT --search-path $SEARCH_PATH --ref $CRAM_REF --dry-run --allow-extra-files-in-search_path $MAPPING_FILE
-
-python parse_rd_metadata.py --project $PROJECT --ped-file $PED_FILE
+python parse_rd_metadata.py --project $PROJECT --ped-file $PED_FILE --individual-metadata $INDIVIDUAL_METADATA --family-metadata $FAMILY_FILE
