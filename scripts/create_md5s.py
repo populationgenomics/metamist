cd7a1a468ec5fe0cd27374f6c96a28c0c49bc13e
EddieLF <eddieformaini@gmail.com>
2023-12-14 16:36:27 +1100

    Add force_recreate option, get driver image from config

--- scripts/create_md5s.py
+++ scripts/create_md5s.py
@@ -1,14 +1,11 @@
 import os
-from typing import Set
 
 import click
 from cpg_utils.hail_batch import get_batch, get_config, copy_common_env
 from google.cloud import storage
 
-DRIVER_IMAGE = 'australia-southeast1-docker.pkg.dev/analysis-runner/images/driver:latest'
 
-
-def create_md5s_for_files_in_directory(skip_filetypes: tuple[str, str], gs_dir):
+def create_md5s_for_files_in_directory(skip_filetypes: tuple[str, str], force_recreate: bool, gs_dir):
     """Validate files with MD5s in the provided gs directory"""
     b = get_batch(f'Create md5 checksums for files in {gs_dir}')
 
@@ -16,36 +13,38 @@ def create_md5s_for_files_in_directory(skip_filetypes: tuple[str, str], gs_dir):
         raise ValueError(f'Expected GS directory, got: {gs_dir}')
 
     billing_project = get_config()['hail']['billing_project']
+    driver_image = get_config()['workflow']['driver_image']
 
     bucket_name, *components = gs_dir[5:].split('/')
 
     client = storage.Client()
     blobs = client.list_blobs(bucket_name, prefix='/'.join(components))
-    files: Set[str] = {f'gs://{bucket_name}/{blob.name}' for blob in blobs}
+    files: set[str] = {f'gs://{bucket_name}/{blob.name}' for blob in blobs}
     for obj in files:
         if obj.endswith('.md5') or obj.endswith(skip_filetypes):
             continue
-        if f'{obj}.md5' not in files:
-            print('Creating md5 for', obj)
+        if f'{obj}.md5' in files and not force_recreate:
+            print(f'{obj}.md5 already exists, skipping')
+            continue
 
+        print('Creating md5 for', obj)
         job = b.new_job(f'Create {os.path.basename(obj)}.md5')
-        copy_common_env(job)
-        job.image(DRIVER_IMAGE)
-        create_md5(job, obj, billing_project)
+        create_md5(job, obj, billing_project, driver_image)
 
     b.run(wait=False)
 
 
-def create_md5(job, file, billing_project):
+def create_md5(job, file, billing_project, driver_image):
     """
     Streams the file with gsutil and calculates the md5 checksum,
     then uploads the checksum to the same path as filename.md5.
     """
+    copy_common_env(job)
+    job.image(driver_image)
     md5 = f'{file}.md5'
-    job.command('set -euxo pipefail')
-    job.env('GOOGLE_APPLICATION_CREDENTIALS', '/gsa-key/key.json')
     job.command(
         f"""\
+    set -euxo pipefail
     gcloud -q auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
     gsutil cat {file} | md5sum | cut -d " " -f1  > /tmp/uploaded.md5
     gsutil -u {billing_project} cp /tmp/uploaded.md5 {md5}
@@ -57,10 +56,11 @@ def create_md5(job, file, billing_project):
 
 @click.command()
 @click.option('--skip-filetypes', '-s', default=('.crai', '.tbi'), multiple=True)
+@click.option('--force-recreate', '-f', is_flag=True, default=False)
 @click.argument('gs_dir')
-def main(skip_filetypes: tuple[str, str], gs_dir: str):
+def main(skip_filetypes: tuple[str, str], force_recreate: bool, gs_dir: str):
     """Scans the directory for files and creates md5 checksums for them."""
-    create_md5s_for_files_in_directory(skip_filetypes, gs_dir=gs_dir)
+    create_md5s_for_files_in_directory(skip_filetypes, force_recreate, gs_dir=gs_dir)
 
 
 if __name__ == '__main__':
