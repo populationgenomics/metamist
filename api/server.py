c9b4b6e0f8654494c0f57662e490c048b792e24d
Yash <yash.pankhania@outlook.com>
2024-01-10 13:40:13 +1100

    Bump version: 6.6.0 → 6.7.0

--- api/server.py
+++ api/server.py
@@ -20,7 +20,7 @@ from api.graphql.schema import MetamistGraphQLRouter  # type: ignore
 from api.settings import PROFILE_REQUESTS, SKIP_DATABASE_CONNECTION
 
 # This tag is automatically updated by bump2version
-_VERSION = '6.6.0'
+_VERSION = '6.7.0'
 
 logger = get_logger()
 
