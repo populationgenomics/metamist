6f619d0cd036cb58fc80709b0947f2bf7cb43988
Michael Harper <michael.harper@populationgenomics.org.au>
2024-01-15 16:08:10 +1100

    script to create samples into tob-wgs metamist

---
+++ scripts/add_tob_samples_to_metamist.py
@@ -0,0 +1,88 @@
+from metamist.apis import SampleApi
+from metamist.models import SampleUpsert, SequencingGroupUpsert, AssayUpsert
+import csv
+
+import click
+
+sapi = SampleApi()
+
+
+def add_sample_to_metamist(extID: str, project: str):
+    """
+    Adds samples to metamist
+
+    Args:
+        sample_ids (list[str]): List of sample IDs to add to metamist
+        project (str): The name of the project to add samples to
+    """
+    sample_upsert = SampleUpsert(
+        id=None,
+        external_id=extID,
+        meta={},
+        project=project,
+        type='blood',
+        participant_id=None,
+        active=None,
+        sequencing_groups=[
+            SequencingGroupUpsert(
+                id=None,
+                type='genome',
+                technology='short-read',
+                platform='illumina',
+                meta=None,
+                sample_id=None,
+                external_ids=None,
+                assays=None,
+            ),
+        ],
+        non_sequencing_assays=[
+            AssayUpsert(
+                id=None,
+                type='sequencing',
+                external_ids=None,
+                sample_id=None,
+                meta=None,
+            ),
+        ],
+    )
+
+    return sample_upsert
+
+
+@click.command()
+@click.option(
+    '--project',
+    required=True,
+    help='The name of the project to add samples to.',
+)
+@click.option(
+    '--files_path',
+    required=True,
+    help='Path to the CSV file with sample data.',
+)
+def main(project: str, files_path: str):
+    """
+    Adds samples to metamist
+
+    Args:
+        project (str): The name of the project to add samples to
+        files_path (str): Path to the CSV file with sample data
+    """
+    # Get list of sample IDs from bucket directory
+    upsert_list = []
+    with open(files_path, 'r') as f:
+        reader = csv.reader(f)
+        next(reader)  # skip the header
+        for row in reader:
+            sgid, extID, gvcf, gvcf_idx = row[0], row[1], row[2], row[3]
+            upsert_list.append(add_sample_to_metamist(extID, project))
+
+    for upsert in upsert_list:
+        api_response = sapi.create_sample(
+            project, upsert
+        )  # returns the internal sample ID
+        print(api_response)
+
+
+if __name__ == '__main__':
+    main()
