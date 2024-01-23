e860302f866bc49120f20c6beddc9ba9b35cc975
Yash Pankhania <yash.pankhania@populationgenomics.org.au>
2024-01-23 19:12:49 +1100

    chore(fixed linting and added TODO):

--- models/models/file.py
+++ models/models/file.py
@@ -1,5 +1,5 @@
-from typing import Optional, Union, List
-from pydantic import  BaseModel
+from typing import Optional
+from pydantic import BaseModel
 
 from models.base import SMBase
 from cloudpathlib.anypath import AnyPath
@@ -10,6 +10,7 @@ class File(BaseModel):
     """File model for external use"""
 
     id: int
+    analysis_id: int
     path: str
     basename: str
     dirname: str
@@ -21,6 +22,8 @@ class File(BaseModel):
     def to_internal(self):
         """Convert to internal model"""
         return FileInternal(
+            id=self.id,
+            analysis_id=self.analysis_id,
             path=self.path,
             basename=self.basename,
             dirname=self.dirname,
@@ -29,12 +32,13 @@ class File(BaseModel):
             checksum=self.checksum,
             size=self.size,
         )
-    
+
 
 class FileInternal(SMBase):
     """File model for internal use"""
 
     id: int
+    analysis_id: int
     path: str
     basename: str
     dirname: AnyPath
@@ -42,43 +46,44 @@ class FileInternal(SMBase):
     nameext: str | None
     checksum: str | None
     size: int
-    
+
     @staticmethod
     def get_basename(path: str) -> str:
         """Get file basename"""
         return AnyPath(path).name
-    
+
     @staticmethod
     def get_dirname(path: str) -> str:
         """Get file dirname"""
         return AnyPath(path).parent
-    
+
     @staticmethod
     def get_nameroot(path: str) -> str:
         """Get file nameroot"""
         return AnyPath(path).stem
-    
+
     @staticmethod
     def get_extension(path: str) -> str:
         """Get file extension"""
         return AnyPath(path).suffix
-    
+
     @staticmethod
     def get_checksum(path: str) -> str:
         return hashlib.sha256(AnyPath(path).read_bytes()).hexdigest()
-    
+
     @staticmethod
     def get_size(path: str) -> int:
         """Get file size"""
         return AnyPath(path).stat().st_size
-    
+
     @staticmethod
-    def from_path(path: str) -> "FileInternal":
+    def from_path(path: str) -> 'FileInternal':
         if not AnyPath(path).exists():
-            raise Exception(f"File {path} does not exist")
+            raise Exception(f'File {path} does not exist')
         return FileInternal(
             # TODO: get id from db
             id=0,
+            analysis_id=0,
             path=path,
             basename=FileInternal.get_basename(path),
             dirname=FileInternal.get_dirname(path),
@@ -87,12 +92,12 @@ class FileInternal(SMBase):
             checksum=FileInternal.get_checksum(path),
             size=FileInternal.get_size(path),
         )
-    
 
     def to_external(self):
         """Convert to external model"""
         return File(
             id=self.id,
+            analysis_id=self.analysis_id,
             path=self.path,
             basename=self.basename,
             dirname=self.dirname,
--- scripts/20240124_migrate_output_to_file.py
+++ scripts/20240124_migrate_output_to_file.py
@@ -1,45 +1,49 @@
-import click
 import asyncio
-from databases import Database
 from textwrap import dedent
-
+import click
+from databases import Database
 from models.models.file import FileInternal
+
 
 def _get_connection_string():
     from db.python.connect import CredentialedDatabaseConfiguration
-    
-    config = CredentialedDatabaseConfiguration.dev_config()
-    
-    #config = CredentialedDatabaseConfiguration(dbname='sm_dev', username='root')
 
-    
+    config = CredentialedDatabaseConfiguration.dev_config()
+
+    # config = CredentialedDatabaseConfiguration(dbname='sm_dev', username='root')
+
     return config.get_connection_string()
 
 
 async def get_analyses_without_fileid(connection):
+    """Get analyses without fileid"""
     query = dedent(
         """SELECT id, output FROM analysis WHERE output_file_id IS NULL"""
     )
-    print("Fetching...")
+    print('Fetching...')
     rows = await connection.fetch_all(query=query)
-    print(f"Found {len(rows)} analyses without fileid")
-    
+    print(f'Found {len(rows)} analyses without fileid')
+
     return rows
 
 
-async def execute_many(connection, query, inserts, dry_run):
-    if dry_run:
-        print(f'Inserting {len(inserts)} with query: {query}')
-    else:
-        await connection.execute_many(query, inserts)
+async def execute_many(connection, query, inserts):
+    """Executes many inserts"""
+    print(f'Inserting {len(inserts)} with query: {query}')
+
+    await connection.execute_many(query, inserts)
 
 
 async def prepare_files(analyses):
+    """Serialize files for insertion"""
     files = []
     for analysis in analyses:
         output = analysis['output']
         if output is None:
             continue
+
+        # TODO support JSON output that has multiple file outputs instead of single file outputs
+
         file_to_insert = FileInternal.from_path(output)
         files.append(
             {
@@ -55,35 +59,34 @@ async def prepare_files(analyses):
     return files
 
 
-async def insert_files(connection, files, dry_run):
+async def insert_files(connection, files):
+    """Insert files"""
     query = dedent(
         """INSERT INTO file (path, basename, dirname, nameroot, nameext, checksum, size, secondary_files)
         VALUES (:path, :basename, :dirname, :nameroot, :nameext, :checksum, :size, :secondary_files)
         RETURNING id"""
     )
-    print("Inserting...")
-    if not dry_run:
-        await execute_many(
-            connection=connection,
-            query=query,
-            values=files,
-            dry_run=dry_run,
-        )
-    print(f"Inserted {len(files)} files")
-    
-    
+    print('Inserting...')
+    await execute_many(
+        connection=connection,
+        query=query,
+        inserts=files,
+    )
+    print(f'Inserted {len(files)} files')
+
+
 @click.command()
 @click.option('--dry-run/--no-dry-run', default=True)
 @click.option('--connection-string', default=None)
 @click.argument('author', default='sequencing-group-migration')
-def main_sync(author, dry_run: bool = True, connection_string: str = None):
+def main_sync(connection_string: str = None):
     """Run synchronisation"""
     asyncio.get_event_loop().run_until_complete(
-        main(author, dry_run=dry_run, connection_string=connection_string)
+        main(connection_string=connection_string)
     )
-    
-    
-async def main(author, dry_run: bool = True, connection_string: str = None):
+
+
+async def main(connection_string: str = None):
     """Run synchronisation"""
     connection = Database(connection_string or _get_connection_string(), echo=True)
     await connection.connect()
@@ -91,7 +94,7 @@ async def main(author, dry_run: bool = True, connection_string: str = None):
         analyses = await get_analyses_without_fileid(connection=connection)
         print(await prepare_files(analyses))
     await connection.disconnect()
-    
-    
+
+
 if __name__ == '__main__':
     main_sync()  # pylint: disable=no-value-for-parameter
