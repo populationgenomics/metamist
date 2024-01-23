292e39ff4e495a1c00dde75d653e07fb4d8b9eac
Yash Pankhania <yash.pankhania@populationgenomics.org.au>
2024-01-23 18:05:46 +1100

    feat(analysis): Added initial File model definition

--- models/models/file.py
+++ models/models/file.py
@@ -1,53 +1,15 @@
 from typing import Optional, Union, List
+from pydantic import  BaseModel
+
 from models.base import SMBase
+from cloudpathlib.anypath import AnyPath
+import hashlib
 
 
-
-class Directory(SMBase):
-    """Directory model for external use"""
-
-    location: Optional[str]
-    path: str
-    basename: str
-    listing: Optional[List[Union['File', 'Directory']]]
-    writable: Optional[bool]
-
-    def to_internal(self):
-        """Convert to internal model"""
-        return DirectoryInternal(
-            location=self.location,
-            path=self.path,
-            basename=self.basename,
-            listing=[item.to_internal() for item in self.listing or []],
-            writable=self.writable,
-        )
-
-
-class DirectoryInternal(SMBase):
-    """Directory model for internal use"""
-
-    id: int
-    location: Optional[str]
-    path: str
-    basename: str
-    listing: Optional[List[Union['FileInternal', 'DirectoryInternal']]]
-    writable: Optional[bool]
-
-    def to_external(self):
-        """Convert to external model"""
-        return Directory(
-            id=self.id,
-            location=self.location,
-            path=self.path,
-            basename=self.basename,
-            listing=[item.to_external() for item in self.listing or []],
-            writable=self.writable,
-        )    
-
-
-class File(SMBase):
+class File(BaseModel):
     """File model for external use"""
 
+    id: int
     path: str
     basename: str
     dirname: str
@@ -55,7 +17,6 @@ class File(SMBase):
     nameext: Optional[str]
     checksum: Optional[str]
     size: int
-    secondaryFiles: Optional[List[Union['File', Directory]]]
 
     def to_internal(self):
         """Convert to internal model"""
@@ -67,7 +28,6 @@ class File(SMBase):
             nameext=self.nameext,
             checksum=self.checksum,
             size=self.size,
-            secondaryFiles=[f.to_internal() for f in self.secondaryFiles or []],
         )
     
 
@@ -77,12 +37,57 @@ class FileInternal(SMBase):
     id: int
     path: str
     basename: str
-    dirname: str
+    dirname: AnyPath
     nameroot: str
     nameext: str | None
     checksum: str | None
     size: int
-    secondaryFiles: List[Union['FileInternal', DirectoryInternal]] | None
+    
+    @staticmethod
+    def get_basename(path: str) -> str:
+        """Get file basename"""
+        return AnyPath(path).name
+    
+    @staticmethod
+    def get_dirname(path: str) -> str:
+        """Get file dirname"""
+        return AnyPath(path).parent
+    
+    @staticmethod
+    def get_nameroot(path: str) -> str:
+        """Get file nameroot"""
+        return AnyPath(path).stem
+    
+    @staticmethod
+    def get_extension(path: str) -> str:
+        """Get file extension"""
+        return AnyPath(path).suffix
+    
+    @staticmethod
+    def get_checksum(path: str) -> str:
+        return hashlib.sha256(AnyPath(path).read_bytes()).hexdigest()
+    
+    @staticmethod
+    def get_size(path: str) -> int:
+        """Get file size"""
+        return AnyPath(path).stat().st_size
+    
+    @staticmethod
+    def from_path(path: str) -> "FileInternal":
+        if not AnyPath(path).exists():
+            raise Exception(f"File {path} does not exist")
+        return FileInternal(
+            # TODO: get id from db
+            id=0,
+            path=path,
+            basename=FileInternal.get_basename(path),
+            dirname=FileInternal.get_dirname(path),
+            nameroot=FileInternal.get_nameroot(path),
+            nameext=FileInternal.get_extension(path),
+            checksum=FileInternal.get_checksum(path),
+            size=FileInternal.get_size(path),
+        )
+    
 
     def to_external(self):
         """Convert to external model"""
@@ -95,5 +100,4 @@ class FileInternal(SMBase):
             nameext=self.nameext,
             checksum=self.checksum,
             size=self.size,
-            secondaryFiles=[f.to_external() for f in self.secondaryFiles or []],
         )
