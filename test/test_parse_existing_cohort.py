eb25aeff40e9f69c356870223cbfb9ffe5f93476
Michael Harper <michael.harper@populationgenomics.org.au>
2024-01-29 09:08:09 +1100

    Add unit test for validating sequencing type in assay metadata

--- test/test_parse_existing_cohort.py
+++ test/test_parse_existing_cohort.py
@@ -311,3 +311,87 @@ class TestExistingCohortParser(DbIsolatedTest):
             sequencing_type='exome',
         )
         self.assertEqual(parser.default_sequencing_type, 'exome')
+
+    @run_as_sync
+    @patch('metamist.parser.generic_parser.query_async')
+    @patch('metamist.parser.cloudhelper.CloudHelper.datetime_added')
+    @patch('metamist.parser.cloudhelper.CloudHelper.file_exists')
+    @patch('metamist.parser.cloudhelper.CloudHelper.file_size')
+    async def test_sequencing_type_in_assay_meta(
+        self,
+        mock_filesize,
+        mock_fileexists,
+        mock_datetime_added,
+        mock_graphql_query,
+    ):
+        """Test that the sequencing type is set correctly when the --sequencing-type flag is set to 'genome' or 'exome'"""
+
+        mock_graphql_query.side_effect = self.run_graphql_query_async
+
+        mock_filesize.return_value = 111
+        mock_fileexists.return_value = False
+        mock_datetime_added.return_value = datetime.fromisoformat('2022-02-02T22:22:22')
+
+        rows = [
+            'HEADER',
+            '""',
+            'Application\tExternal ID\tSample Concentration (ng/ul)\tVolume (uL)\tSex\tSample/Name\tReference Genome\tParticipant ID\t',
+            'App\tEXTID1234\t100\t100\tFemale\t220405_FLUIDX1234\thg38\tPID123',
+        ]
+
+        for sequencing_type in ['genome', 'exome']:
+            with self.subTest(sequencing_type=sequencing_type):
+                parser = ExistingCohortParser(
+                    include_participant_column=False,
+                    batch_number='M01',
+                    search_locations=[],
+                    project=self.project_name,
+                    allow_missing_files=False,
+                    sequencing_type=sequencing_type,
+                )
+                parser.filename_map = {
+                    'HG3F_2_220405_FLUIDX1234_Homo-sapiens_AAC-TAT_R_220208_VB_BLAH_M002_R1.fastq': '/path/to/HG3F_2_220405_FLUIDX1234_Homo-sapiens_AAC-TAT_R_220208_VB_BLAH_M002_R1.fastq',
+                    'HG3F_2_220405_FLUIDX1234_Homo-sapiens_AAC-TAT_R_220208_VB_BLAH_M002_R2.fastq': '/path/to/HG3F_2_220405_FLUIDX1234_Homo-sapiens_AAC-TAT_R_220208_VB_BLAH_M002_R2.fastq',
+                }
+
+                file_contents = '\n'.join(rows)
+                participants: list[ParsedParticipant]
+                _, participants = await parser.parse_manifest(
+                    StringIO(file_contents), delimiter='\t', dry_run=True
+                )
+
+                sample_to_add = participants[0].samples[0]
+                expected_sequence_dict = {
+                    'reference_genome': 'hg38',
+                    'platform': 'App',
+                    'concentration': '100',
+                    'volume': '100',
+                    'fluid_x_tube_id': '220405_FLUIDX1234',
+                    'reads_type': 'fastq',
+                    'reads': [
+                        {
+                            'location': '/path/to/HG3F_2_220405_FLUIDX1234_Homo-sapiens_AAC-TAT_R_220208_VB_BLAH_M002_R1.fastq',
+                            'basename': 'HG3F_2_220405_FLUIDX1234_Homo-sapiens_AAC-TAT_R_220208_VB_BLAH_M002_R1.fastq',
+                            'class': 'File',
+                            'checksum': None,
+                            'size': 111,
+                            'datetime_added': '2022-02-02T22:22:22',
+                        },
+                        {
+                            'location': '/path/to/HG3F_2_220405_FLUIDX1234_Homo-sapiens_AAC-TAT_R_220208_VB_BLAH_M002_R2.fastq',
+                            'basename': 'HG3F_2_220405_FLUIDX1234_Homo-sapiens_AAC-TAT_R_220208_VB_BLAH_M002_R2.fastq',
+                            'class': 'File',
+                            'checksum': None,
+                            'size': 111,
+                            'datetime_added': '2022-02-02T22:22:22',
+                        },
+                    ],
+                    'sequencing_platform': 'illumina',
+                    'sequencing_technology': 'short-read',
+                    'sequencing_type': f'{sequencing_type}',
+                    'batch': 'M01',
+                }
+                assay = sample_to_add.sequencing_groups[0].assays[0]
+                self.maxDiff = None
+                self.assertDictEqual(expected_sequence_dict, assay.meta)
+        return
