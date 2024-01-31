c26fad64f5aabff3f49cd29467d715191bc740ac
Michael Harper <michael.harper@populationgenomics.org.au>
2024-01-31 12:19:43 +1100

    linting

--- scripts/clear_project_test_metadata.py
+++ scripts/clear_project_test_metadata.py
@@ -1,10 +1,22 @@
-from metamist.apis import ProjectApi
 import click
+
+from metamist.apis import ProjectApi
 
 
 @click.command()
 @click.option('--project', required=True, help='Project to delete data from')
 def main(project: str):
+    """
+    Deletes all data from a specified project in Metamist.
+
+    This function uses the Metamist API to delete all data associated with a given project.
+
+    Args:
+        project (str): The name of the project from which to delete data.
+
+    Returns:
+        None
+    """
     papi = ProjectApi()
 
     # Delete Project Data
@@ -14,4 +26,5 @@ def main(project: str):
 
 
 if __name__ == '__main__':
+    # pylint: disable=no-value-for-parameter
     main()
