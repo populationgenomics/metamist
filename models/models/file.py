afa67783bde8bec8cceee0b60f82094e87f635e6
Yash Pankhania <yash.pankhania@populationgenomics.org.au>
2024-02-26 22:18:36 +1100

    fixed fileinternal from_db

--- models/models/file.py
+++ models/models/file.py
@@ -27,27 +27,17 @@ class FileInternal(SMBase):
         """
         Convert from db keys, mainly converting id to id_
         """
-        path = kwargs.get('path')
-        basename = kwargs.get('basename')
-        dirname = kwargs.get('dirname')
-        nameroot = kwargs.get('nameroot')
-        nameext = kwargs.get('nameext')
-        file_checksum = kwargs.get('file_checksum')
-        size = kwargs.get('size')
-        meta = kwargs.get('meta')
-        valid = kwargs.get('valid')
-
-        return File(
+        return FileInternal(
             id=kwargs.pop('id'),
-            path=path,
-            basename=basename,
-            dirname=dirname,
-            nameroot=nameroot,
-            nameext=nameext,
-            file_checksum=file_checksum,
-            size=size,
-            meta=meta,
-            valid=valid,
+            path=kwargs.get('path'),
+            basename=kwargs.get('basename'),
+            dirname=kwargs.get('dirname'),
+            nameroot=kwargs.get('nameroot'),
+            nameext=kwargs.get('nameext'),
+            file_checksum=kwargs.get('file_checksum'),
+            size=kwargs.get('size'),
+            meta=kwargs.get('meta'),
+            valid=kwargs.get('valid', False),
         )
 
     def to_external(self):
