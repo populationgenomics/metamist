9c72d65f87570b9a692c818a6cacf0912dd9d0bc
Michael Franklin <illusional@users.noreply.github.com>
2024-03-27 14:51:12 +1100

    Get carried away with typing

--- web/src/shared/utilities/formatMoney.ts
+++ web/src/shared/utilities/formatMoney.ts
@@ -1,3 +1,3 @@
-const formatMoney = (val: number, dp: number = 2): string => `$${val.toFixed(dp).replace(/\d(?=(\d{3})+\.)/g, '$&,')}`
+const formatMoney = (val: number | undefined, dp: number = 2): string => val ? `$${val.toFixed(dp).replace(/\d(?=(\d{3})+\.)/g, '$&,')}` : ''
 
 export default formatMoney
