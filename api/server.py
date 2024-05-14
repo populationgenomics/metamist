c9313f90d2e6f1aa22bba4e8f0775e0443ab9b35
Michael Franklin <illusional@users.noreply.github.com>
2024-05-14 20:44:28 +1000

    Bump version: 6.10.2 → 7.0.0

--- api/server.py
+++ api/server.py
@@ -20,7 +20,7 @@ from db.python.tables.project import is_all_access
 from db.python.utils import get_logger
 
 # This tag is automatically updated by bump2version
-_VERSION = '6.10.2'
+_VERSION = '7.0.0'
 
 
 logger = get_logger()
