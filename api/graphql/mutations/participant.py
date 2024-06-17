c305c2670c2668786dff5719a44d7207dfbe8979
Yash Pankhania <yash.pankhania@populationgenomics.org.au>
2024-06-17 17:01:01 +1000

    removed gets

--- api/graphql/mutations/participant.py
+++ api/graphql/mutations/participant.py
@@ -1,21 +1,14 @@
-from typing import TYPE_CHECKING
-
 import strawberry
 from strawberry.types import Info
-from typing_extensions import Annotated
 
 from api.graphql.types import (
     CustomJSON,
     ParticipantUpsertInput,
     ParticipantUpsertType,
-    QueryParticipantCriteria,
     UpdateParticipantFamilyType,
 )
 from db.python.layers.participant import ParticipantLayer
 from models.models.participant import ParticipantUpsertInternal
-
-if TYPE_CHECKING:
-    from ..schema import GraphQLParticipant
 
 
 @strawberry.type
@@ -37,25 +30,6 @@ class ParticipantMutations:
 
         return CustomJSON(
             {'success': await participant_layer.fill_in_missing_participants()}
-        )
-
-    @strawberry.mutation
-    async def get_id_map_by_external_ids(
-        self,
-        info: Info,
-        external_participant_ids: list[str],
-        allow_missing: bool = False,
-    ) -> CustomJSON:
-        """Get ID map of participants, by external_id"""
-        # TODO: Reconfigure connection permissions as per `routes``
-        connection = info.context['connection']
-        player = ParticipantLayer(connection)
-        return CustomJSON(
-            await player.get_id_map_by_external_ids(
-                external_participant_ids,
-                allow_missing=allow_missing,
-                project=connection.project,
-            )
         )
 
     @strawberry.mutation
@@ -112,28 +86,6 @@ class ParticipantMutations:
             ]
         )
         return [ParticipantUpsertType.from_upsert_internal(p) for p in results]
-
-    @strawberry.mutation
-    async def get_participants(
-        self,
-        criteria: QueryParticipantCriteria,
-        info: Info,
-    ) -> list[Annotated['GraphQLParticipant', strawberry.lazy('..schema')]]:
-        """Get participants, default ALL participants in project"""
-        # TODO: Reconfigure connection permissions as per `routes`
-        connection = info.context['connection']
-        player = ParticipantLayer(connection)
-        participants = await player.get_participants(
-            project=connection.project,
-            external_participant_ids=(
-                criteria.external_participant_ids if criteria else None
-            ),
-            internal_participant_ids=(
-                criteria.internal_participant_ids if criteria else None
-            ),
-        )
-        #  pylint: disable=no-member
-        return [Annotated['GraphQLParticipant', strawberry.lazy('..schema')].from_internal(p) for p in participants]  # type: ignore[attr-defined]
 
     @strawberry.mutation
     async def update_participant_family(
