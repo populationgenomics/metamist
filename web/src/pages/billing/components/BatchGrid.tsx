d0cb19b32e76a0113eba8cac902c7c4b6b3b0073
Milo Hyben <milo.hyben@gmail.com>
2024-07-01 15:57:48 +1000

    Added count go stages to BillingAnalysisCost page.

--- web/src/pages/billing/components/BatchGrid.tsx
+++ web/src/pages/billing/components/BatchGrid.tsx
@@ -200,7 +200,8 @@ const AnalysisRunnerRecordCard: React.FC<{ data: AnalysisCostRecord }> = ({ data
                         >
                             Cost by Sequencing Groups -{' '}
                             {data.seq_groups?.filter((s) => !!s.sequencing_group)?.length || 0}{' '}
-                            sequencing group(s) across {data.seq_groups?.filter((s) => !!s.stage)?.length || 0} stages
+                            sequencing group(s) across{' '}
+                            {data.seq_groups?.filter((s) => !!s.stage)?.length || 0} stages
                         </CheckboxRow>
                         <DisplayRow label="" isVisible={isOpen && isSeqGroupOpen}>
                             <SeqGrpDisplay seq_groups={data.seq_groups || []} />
@@ -279,7 +280,8 @@ const BatchCard: React.FC<{ item: AnalysisCostRecordBatch }> = ({ item }) => {
                     >
                         Cost by Sequencing Groups -{' '}
                         {item.seq_groups?.filter((s) => !!s.sequencing_group)?.length || 0}{' '}
-                        sequencing group(s) across {item.seq_groups?.filter((s) => !!s.stage)?.length || 0} stages
+                        sequencing group(s) across{' '}
+                        {item.seq_groups?.filter((s) => !!s.stage)?.length || 0} stages
                     </CheckboxRow>
                     <DisplayRow
                         label="Cost By Sequencing Group"
