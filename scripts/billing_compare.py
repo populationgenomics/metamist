a36122d037cf5c3d299e2abfc1b5bc9b86ca8e74
Milo Hyben <milo.hyben@gmail.com>
2024-07-11 15:09:26 +1000

    Added Service ID to the output of billing compare script.

--- scripts/billing_compare.py
+++ scripts/billing_compare.py
@@ -4,6 +4,19 @@ check for changes to budgets.yaml files for each of the projects.
 Once collected it gets checked agains budget BQ table and
 it inserts any missing records.
 At this stage it does not delete any records from BQ table.
+
+example of usage:
+# generate the billing data for 2023:
+
+# aggregate and compare by topic/service, include all charges
+python scripts/billing_compare.py -yr 2023 -t 1 -s 1 -o billing-2023-topic-service.csv
+
+# aggregate and compare by service only
+python scripts/billing_compare.py -yr 2023 -t 0 -s 1 -o billing-2023-service.csv
+
+# aggregate and compare by tax-only service charges
+python scripts/billing_compare.py -yr 2023 -t 1 -s 1 -tx 1 -o billing-2023-tax-only.csv
+
 """
 
 import argparse
@@ -26,11 +39,14 @@ logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
 
+TAX_ONLY_FILTER = "AND service.description = 'Invoice' AND sku.description = 'Tax'"
+
+
 def generate_invoice_months(yr: str) -> str:
     """
     Generate the invoice months for the year
     """
-    return "','".join([f'{yr}{str(m).zfill(2)}' for m in range(1, 13)])
+    return "','".join([f'{yr}{str(m).zfill(2)}' for m in range(1, 7)])
 
 
 def generate_group_by(columns: list) -> str:
@@ -41,7 +57,9 @@ def generate_group_by(columns: list) -> str:
 
 
 def set_index(
-    df: pd.DataFrame, include_topic: bool, include_service: bool
+    df: pd.DataFrame,
+    include_topic: bool,
+    include_service: bool,
 ) -> pd.DataFrame:
     """
     Set the index for the dataframe based on selected columns
@@ -53,11 +71,20 @@ def set_index(
             + df['topic'].astype(str)
             + '|'
             + df['service'].astype(str)
+            + '|'
+            + df['service_id'].astype(str)
         )
     elif include_topic:
         df['idx'] = df['month'].astype(str) + '|' + df['topic'].astype(str)
     elif include_service:
-        df['idx'] = df['month'].astype(str) + '|' + df['service'].astype(str)
+        df['idx'] = (
+            # set service first so we can easy sort in the final sprradsheet
+            df['service'].astype(str)
+            + '|'
+            + df['month'].astype(str)
+            + '|'
+            + df['service_id'].astype(str)
+        )
     else:
         df['idx'] = df['month'].astype(str)
     return df.set_index('idx')
@@ -67,14 +94,17 @@ def get_metamist_billing(
     yr: str,
     include_topic: bool = True,
     include_service: bool = False,
+    in_tax_only: bool = False,
 ) -> pd.DataFrame:
     """
     Get the billing data from metamist BQ table
+    If tax only is set then only include the tax records
     """
     columns = ['invoice.month as month']
     if include_topic:
         columns.append('topic')
     if include_service:
+        columns.append('service.id as service_id')
         columns.append('service.description as service')
 
     query = f"""
@@ -85,6 +115,7 @@ def get_metamist_billing(
         DATE_TRUNC(usage_end_time, DAY) >= TIMESTAMP("{int(yr) - 1}-12-28")
         AND DATE_TRUNC(usage_end_time, DAY) <= TIMESTAMP("{int(yr) + 1}-01-05")
         AND invoice.month in ('{generate_invoice_months(yr)}')
+        {TAX_ONLY_FILTER if in_tax_only else ''}
         GROUP BY {generate_group_by(columns)};
     """
     # print(query)
@@ -97,6 +128,7 @@ def get_gcp_billing(
     yr: str,
     include_topic: bool = True,
     include_service: bool = False,
+    in_tax_only: bool = False,
 ) -> pd.DataFrame:
     """
     Get the billing data from GCP BQ table
@@ -105,6 +137,7 @@ def get_gcp_billing(
     if include_topic:
         columns.append('project.name as gcp_project_name')
     if include_service:
+        columns.append('service.id as service_id')
         columns.append('service.description as service')
 
     query = f"""
@@ -115,6 +148,7 @@ def get_gcp_billing(
         TIMESTAMP_TRUNC(_PARTITIONTIME, DAY) >= TIMESTAMP("{int(yr) - 1}-12-28")
         AND TIMESTAMP_TRUNC(_PARTITIONTIME, DAY) <= TIMESTAMP("{int(yr) + 1}-01-05")
         AND invoice.month in ('{generate_invoice_months(yr)}')
+        {TAX_ONLY_FILTER if in_tax_only else ''}
         GROUP BY {generate_group_by(columns)};
     """
     # print(query)
@@ -134,7 +168,9 @@ def get_gcp_billing(
         return_cols.append('gcp_project_name')
     if include_service:
         # include project service in the final df
+        df['gcp_service_id'] = df['service_id']
         df['gcp_service'] = df['service']
+        return_cols.append('gcp_service_id')
         return_cols.append('gcp_service')
 
     # total cost as last gcp column
@@ -182,6 +218,13 @@ def main():
         type=int,
         default=0,
     )
+    parser.add_argument(
+        '-tx',
+        '--tax_only',
+        help='Include Only TAX records',
+        type=int,
+        default=0,
+    )
     args = parser.parse_args()
     if args.year is None:
         print('Missing year argument')
@@ -190,10 +233,11 @@ def main():
     in_year: str = args.year
     in_topic: bool = args.topic == 1
     in_service: bool = args.service == 1
+    in_tax_only: bool = args.tax_only == 1
 
     # get
-    m_df = get_metamist_billing(in_year, in_topic, in_service)
-    gcp_df = get_gcp_billing(in_year, in_topic, in_service)
+    m_df = get_metamist_billing(in_year, in_topic, in_service, in_tax_only)
+    gcp_df = get_gcp_billing(in_year, in_topic, in_service, in_tax_only)
 
     total_metamist = np.sum(m_df.metamist_total_cost)
     total_gcp = np.sum(gcp_df.gcp_total_cost)
