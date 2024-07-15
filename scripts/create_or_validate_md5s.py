5635abcf7898e3d563c13c5bc0fd3879abadea96
EddieLF <eddieformaini@gmail.com>
2024-07-15 12:08:07 +1000

    Refactor script a bit

--- scripts/create_or_validate_md5s.py
+++ scripts/create_or_validate_md5s.py
@@ -5,21 +5,20 @@ from google.cloud import storage
 
 from cpg_utils.hail_batch import config_retrieve, copy_common_env, get_batch
 
-def validate_all_objects_in_directory(gs_dir, skip_filetypes: tuple[str, ...], billing_project: str = None):
-    """Validate files with MD5s in the provided gs directory"""
-    b = get_batch(f'Validate md5 checksums for files in {gs_dir}')
-    client = storage.Client()
-    
-    if not billing_project:
-        billing_project = config_retrieve(['workflow', 'gcp_billing_project'])
-    driver_image = config_retrieve(['workflow', 'driver_image'])
 
+def get_blobs_in_directory(gs_dir: str, billing_project: str, storage_client: storage.Client):
+    """Get all blobs in a directory as a set of full gs:// URIs"""
     bucket_name, *components = gs_dir[5:].split('/')
-    
-    client = storage.Client()
-    bucket = client.bucket(bucket_name, user_project=billing_project)
+    bucket = storage_client.bucket(bucket_name, user_project=billing_project)
     blobs = bucket.list_blobs(prefix='/'.join(components))
-    files: set[str] = {f'gs://{bucket_name}/{blob.name}' for blob in blobs}
+    return {f'gs://{bucket_name}/{blob.name}' for blob in blobs}
+
+
+def validate_md5s_in_directory(gs_dir: str, skip_filetypes: tuple[str, ...], billing_project: str, driver_image: str, storage_client: storage.Client):
+    """Validate files with MD5s in the provided gs directory, skipping files with certain extensions"""
+    b = get_batch(f'Validate md5 checksums for files in {gs_dir}')
+
+    files = get_blobs_in_directory(gs_dir, billing_project, storage_client)
     for obj in files:
         if obj.endswith('.md5') or obj.endswith(skip_filetypes):
             continue
@@ -32,35 +31,8 @@ def validate_all_objects_in_directory(gs_dir, skip_filetypes: tuple[str, ...], b
 
     b.run(wait=False)
 
-def create_md5s_for_files_in_directory(gs_dir, skip_filetypes: tuple[str, ...], force_recreate: bool, billing_project: str = None):
-    """Validate files with MD5s in the provided gs directory"""
-    b = get_batch(f'Create md5 checksums for files in {gs_dir}')
 
-    if not billing_project:
-        billing_project = config_retrieve(['workflow', 'gcp_billing_project'])
-    driver_image = config_retrieve(['workflow', 'driver_image'])
-
-    bucket_name, *components = gs_dir[5:].split('/')
-
-    client = storage.Client()
-    bucket = client.bucket(bucket_name, user_project=billing_project)
-    blobs = bucket.list_blobs(prefix='/'.join(components))
-    files: set[str] = {f'gs://{bucket_name}/{blob.name}' for blob in blobs}
-    for filepath in files:
-        if filepath.endswith('.md5') or filepath.endswith(skip_filetypes):
-            continue
-        if f'{filepath}.md5' in files and not force_recreate:
-            print(f'{filepath}.md5 already exists, skipping')
-            continue
-
-        print('Creating md5 for', filepath)
-        job = b.new_job(f'Create {os.path.basename(filepath)}.md5')
-        create_md5(job, filepath, billing_project, driver_image)
-
-    b.run(wait=False)
-
-
-def validate_md5(job, file, billing_project, driver_image):
+def validate_md5(job, file: str, billing_project: str, driver_image: str):
     """
     This quickly validates a file and it's md5
     """
@@ -89,7 +61,26 @@ def validate_md5(job, file, billing_project, driver_image):
     return job
 
 
-def create_md5(job, file, billing_project, driver_image):
+def create_md5s_for_files_in_directory(gs_dir: str, skip_filetypes: tuple[str, ...], force_recreate: bool, billing_project: str, driver_image: str, storage_client: storage.Client):
+    """Validate files with MD5s in the provided gs directory, skipping files with certain extensions"""
+    b = get_batch(f'Create md5 checksums for files in {gs_dir}')
+
+    files = get_blobs_in_directory(gs_dir, billing_project, storage_client)
+    for filepath in files:
+        if filepath.endswith('.md5') or filepath.endswith(skip_filetypes):
+            continue
+        if f'{filepath}.md5' in files and not force_recreate:
+            print(f'{filepath}.md5 already exists, skipping')
+            continue
+
+        print('Creating md5 for', filepath)
+        job = b.new_job(f'Create {os.path.basename(filepath)}.md5')
+        create_md5(job, filepath, billing_project, driver_image)
+
+    b.run(wait=False)
+
+
+def create_md5(job, file: str, billing_project: str, driver_image: str):
     """
     Streams the file with gsutil and calculates the md5 checksum,
     then uploads the checksum to the same path as filename.md5.
@@ -113,17 +104,23 @@ def create_md5(job, file, billing_project, driver_image):
 @click.option('--billing-project', '-b', default=None)
 @click.option('--skip-filetypes', '-s', default=('.crai', '.tbi'), multiple=True)
 @click.option('--validate-only', '-v', is_flag=True, default=False, help='Validate existing md5s, do not create new ones.')
-@click.option('--force-recreate', '-f', is_flag=True, default=False)
+@click.option('--force-recreate', '-f', is_flag=True, default=False, help='Recreate md5s even if they already exist.')
 @click.argument('gs_dir')
 def main(billing_project: str | None, skip_filetypes: tuple[str, str], validate_only: bool, force_recreate: bool, gs_dir: str):
     """Scans the directory for files and creates md5 checksums for them."""
     if not gs_dir.startswith('gs://'):
         raise ValueError(f'Expected GS directory, got: {gs_dir}')
     
+    if not billing_project:
+        billing_project = config_retrieve(['workflow', 'dataset_gcp_project'])
+    driver_image = config_retrieve(['workflow', 'driver_image'])
+    
+    storage_client = storage.Client()
+    
     if validate_only:
-        validate_all_objects_in_directory(gs_dir, skip_filetypes, billing_project)
+        validate_md5s_in_directory(gs_dir, skip_filetypes, billing_project, driver_image, storage_client)
     else:
-        create_md5s_for_files_in_directory(gs_dir, skip_filetypes, force_recreate, billing_project)
+        create_md5s_for_files_in_directory(gs_dir, skip_filetypes, force_recreate, billing_project, driver_image, storage_client)
 
 
 if __name__ == '__main__':
