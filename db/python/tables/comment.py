12bb8d9f0509521e8a9e7a4c96e720ac34693fb3
Dan Coates <dan.coates@populationgenomics.org.au>
2024-07-29 17:50:28 +1000

    update to handle comment versions

--- db/python/tables/comment.py
+++ db/python/tables/comment.py
@@ -1,3 +1,5 @@
+from itertools import groupby
+
 from db.python.tables.base import DbBase
 from models.models.comment import CommentInternal
 
@@ -7,38 +9,43 @@ class CommentTable(DbBase):
     Capture Comment table operations and queries
     """
 
-    table_name = 'comment'
-
     async def query(self) -> list[CommentInternal]:
         """Query comments"""
 
-        _query = f"""
-            
+        _query = """
             with top_level_comment_list as (
-                select comment_id, 'sample' as entity, sample_id as entity_id
+                select
+                    comment_id,
+                    'sample' as entity_type,
+                    sample_id as entity_id
                 from sample_comment sc
                 join sample s
                 on s.id = sc.sample_id
-                where s.project = 1 
+                where s.project = 1
             ) select
                 c.id as comment_id,
                 c.parent_id,
-                c.root_id,
                 c.content,
-                tc.entity,
+                tc.entity_type,
                 tc.entity_id,
                 al.timestamp,
-                al.author
-
-            from top_level_comment_list tc
-            left join {self.table_name} c
-            on c.id = tc.comment_id or c.root_id = tc.comment_id
+                al.author,
+                cc.id is null as is_deleted
+			from comment FOR SYSTEM_TIME ALL as c
+            -- Join with the comments table with just the current state to see
+            -- if this row is a deleted comment
+            left join comment cc
+            on cc.id = c.id
+            join top_level_comment_list tc
+            on c.id = tc.comment_id or c.parent_id = tc.comment_id
             left join audit_log al
             on al.id = c.audit_log_id
-            order by comment_id
-
-
+            order by c.id, al.timestamp
         """
 
-        comments = await self.connection.fetch_all(_query)
-        return [CommentInternal.from_db(dict(comment)) for comment in comments]
+        comment_versions = await self.connection.fetch_all(_query)
+
+        return [
+            CommentInternal.from_db_versions(list(dict(v) for v in g))
+            for _, g in groupby(comment_versions, key=lambda k: k['comment_id'])
+        ]
--- models/models/comment.py
+++ models/models/comment.py
@@ -1,6 +1,13 @@
 import datetime
+from typing import Any
 
 from models.base import SMBase
+
+
+class CommentVersionInternal(SMBase):
+    content: str
+    author: str
+    timestamp: datetime.datetime
 
 
 class CommentInternal(SMBase):
@@ -10,18 +17,41 @@ class CommentInternal(SMBase):
     parent_id: int | None
     content: str
     author: str
-    timestamp: datetime.datetime
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
+    entity_type: str
+    entity_id: int
+    versions: list[CommentVersionInternal]
+    is_deleted: bool
 
     @staticmethod
-    def from_db(d: dict):
+    def from_db_versions(versions: list[dict[str, Any]]):
         """
-        Convert from db keys, mainly converting id to id_
+        Convert from a list of ordered comment versions to a
+        single comment instance with version
         """
 
+        first_version = versions[0]
+        last_version = versions[-1]
+
+        history = [
+            CommentVersionInternal(
+                author=v.get('author'),
+                timestamp=v.get('timestamp'),
+                content=v.get('content'),
+            )
+            for v in versions[1:]
+        ]
+
         return CommentInternal(
-            id=d.get('comment_id'),
-            parent_id=d.get('parent_id'),
-            content=d.get('content'),
-            author=d.get('author'),
-            timestamp=d.get('timestamp'),
+            id=first_version.get('comment_id'),
+            parent_id=last_version.get('parent_id'),
+            entity_type=first_version.get('entity_type'),
+            entity_id=first_version.get('entity_id'),
+            content=last_version.get('content'),
+            author=first_version.get('author'),
+            created_at=first_version.get('timestamp'),
+            updated_at=last_version.get('timestamp'),
+            versions=history,
+            is_deleted=last_version.get('is_deleted'),
         )
