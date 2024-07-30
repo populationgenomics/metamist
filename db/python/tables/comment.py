55866e25769138220bd780a5916bff61e73e89af
Dan Coates <dan.coates@populationgenomics.org.au>
2024-07-30 10:55:15 +1000

    add rough adding of comments to samples

--- db/python/tables/comment.py
+++ db/python/tables/comment.py
@@ -3,6 +3,8 @@ from itertools import groupby
 from db.python.tables.base import DbBase
 from models.models.comment import CommentInternal
 
+entity_table_map = {'sample': ('sample_comment', 'sample_id')}
+
 
 class CommentTable(DbBase):
     """
@@ -49,3 +51,42 @@ class CommentTable(DbBase):
             CommentInternal.from_db_versions(list(dict(v) for v in g))
             for _, g in groupby(comment_versions, key=lambda k: k['comment_id'])
         ]
+
+    async def add_comment(self, entity: str, entity_id: int, content: str):
+
+        if entity not in entity_table_map:
+            raise ValueError(f'Unknown entity {entity}')
+
+        join_table, join_column = entity_table_map[entity]
+
+        comment_insert = """
+            INSERT INTO comment (content, audit_log_id)
+            VALUES (:content, :audit_log_id) RETURNING id;
+        """
+
+        join_insert = f"""
+            INSERT INTO {join_table} (comment_id, {join_column}, audit_log_id)
+            VALUES (:comment_id, :entity_id, :audit_log_id);
+        """
+
+        async with self.connection.transaction():
+            audit_log_id = await self._connection.audit_log_id()
+
+            comment_id = await self.connection.fetch_val(
+                comment_insert,
+                {
+                    'content': content,
+                    'audit_log_id': audit_log_id,
+                },
+            )
+
+            await self.connection.execute(
+                join_insert,
+                {
+                    'comment_id': comment_id,
+                    'entity_id': entity_id,
+                    'audit_log_id': audit_log_id,
+                },
+            )
+
+            return 1
