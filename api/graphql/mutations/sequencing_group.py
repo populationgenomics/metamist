b2f3bd131911736c2a4fd670f877d0e7dcc42bc9
Dan Coates <dan.coates@populationgenomics.org.au>
2024-08-09 10:12:15 +1000

    return created comment from add comment mutations

--- api/graphql/mutations/sequencing_group.py
+++ api/graphql/mutations/sequencing_group.py
@@ -1,9 +1,14 @@
+from typing import TYPE_CHECKING, Annotated
+
 import strawberry
 from strawberry.types import Info
 
 from api.graphql.loaders import GraphQLContext
 from db.python.layers.sequencing_group import SequencingGroupLayer
 from models.utils.sequencing_group_id_format import sequencing_group_id_transform_to_raw
+
+if TYPE_CHECKING:
+    from api.graphql.schema import GraphQLComment
 
 
 @strawberry.type
@@ -16,12 +21,14 @@ class SequencingGroupMutations:
         content: str,
         id: str,
         info: Info[GraphQLContext, 'SequencingGroupMutations'],
-    ) -> int:
+    ) -> Annotated['GraphQLComment', strawberry.lazy('api.graphql.schema')]:
         """Add a comment to a sequencing group"""
+        from api.graphql.schema import GraphQLComment
+
         connection = info.context['connection']
         sequencing_group_layer = SequencingGroupLayer(connection)
         result = await sequencing_group_layer.add_comment_to_sequencing_group(
             content=content,
             sequencing_group_id=sequencing_group_id_transform_to_raw(id),
         )
-        return result
+        return GraphQLComment.from_internal(result)
