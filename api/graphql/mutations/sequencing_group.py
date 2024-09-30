9c3bfa11802f5914873e2d157cbfa7efa88b0ccd
Yash Pankhania <yash.pankhania@populationgenomics.org.au>
2024-10-01 08:18:07 +1000

    added sequencing group mutations

--- api/graphql/mutations/sequencing_group.py
+++ api/graphql/mutations/sequencing_group.py
@@ -6,8 +6,12 @@ from strawberry.types import Info
 
 from api.graphql.loaders import GraphQLContext
 from api.graphql.mutations.assay import AssayUpsertInput
+from db.python.connect import Connection
 from db.python.layers.comment import CommentLayer
+from db.python.layers.sequencing_group import SequencingGroupLayer
 from models.models.comment import CommentEntityType
+from models.models.project import FullWriteAccessRoles
+from models.models.sequencing_group import SequencingGroupUpsertInternal
 from models.utils.sequencing_group_id_format import sequencing_group_id_transform_to_raw
 
 if TYPE_CHECKING:
@@ -52,3 +56,25 @@ class SequencingGroupMutations:
             content=content,
         )
         return GraphQLComment.from_internal(result)
+
+    @strawberry.mutation
+    async def update_sequencing_group(
+        self,
+        sequencing_group_id: str,
+        sequencing_group: strawberry.scalars.JSON,
+        info: Info,
+    ) -> bool:
+        """Update the meta fields of a sequencing group"""
+        connection: Connection = info.context['connection']
+        connection.check_access(FullWriteAccessRoles)
+
+        st = SequencingGroupLayer(connection)
+        await st.upsert_sequencing_groups(
+            [
+                SequencingGroupUpsertInternal(
+                    id=sequencing_group_id_transform_to_raw(sequencing_group_id),
+                    meta=sequencing_group,  # type: ignore [arg-type]
+                )
+            ]
+        )
+        return True
