db26cab443ad17085c9aff62f6ebcfdcf4601e28
Alex Stuckey <alex.stuckey@populationgenomics.org.au>
2024-11-04 13:30:03 +1300

    register missing combiner analysis script

---
+++ scripts/register_migging_combiner_analysis.py
@@ -0,0 +1,31 @@
+from argparse import ArgumentParser
+
+from hail.vds import read_vds
+
+from cpg_utils import to_path
+from metamist.apis import AnalysisApi
+from metamist.models import Analysis, AnalysisStatus
+
+
+def get_sg_ids(vds: str) -> list[str]:
+    return read_vds(to_path(vds)).variant_data.s.collect()
+
+
+def main(vds: str, dataset: str):
+    aapi = AnalysisApi()
+    am = Analysis(
+        type='combiner',
+        output=vds,
+        status=AnalysisStatus('completed'),
+        sequencing_group_ids=get_sg_ids(vds),
+        meta=None,
+    )
+    aapi.create_analysis(project=dataset, analysis=am)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--vds', help='VDS to register analysis for.')
+    parser.add_argument('--dataset', help='Dataset to register analysis in.')
+    args = parser.parse_args()
+    main(args.vds, args.dataset)
