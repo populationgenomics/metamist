1717daad5b5e538346bea241d91a4defaab6740d
Milo Hyben <milo.hyben@gmail.com>
2024-11-22 10:43:55 +1100

    external-org-name set to ''.

--- scripts/upsert_participants.py
+++ scripts/upsert_participants.py
@@ -110,17 +110,13 @@ def main(
     participant_meta: str,
     sample_meta: str,
     sample_external_id_column: str,
-    external_org_name: str | None = None,
+    external_org_name: str = '',
 ):  # pylint: disable=too-many-locals
     """Upsert participants to metamist"""
     # Query metamist
     project = bucket.split('-')[1]  # project name derived from bucket name
     data_response = get_sample_data(project)
     project_id = data_response.get('project')['id']
-
-    # allow empty external_org_name for backwards compatibility
-    if external_org_name is None:
-        external_org_name = ''
 
     sample_eid_mapping = map_sample_eid_to_sample_data(data_response)
 
