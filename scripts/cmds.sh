7e7690bc44153b349d13cbb708c31b14873225b0
EddieLF <eddieformaini@gmail.com>
2024-12-20 15:23:15 +1100

    add batch option

--- scripts/cmds.sh
+++ scripts/cmds.sh
@@ -1,12 +1,15 @@
 SM_ENVIRONMENT=
-PROJECT="rdnow"
+PROJECT="rdp-autoimmune"
 SEARCH_PATH="gs://cpg-${PROJECT}-main-upload/"
-SUBDIR="DNA/transfer_20230322145214"
+SUBDIR="2024-12-20"
+# BATCH="R_241021_ALEBUT_DNA_P002"
 SEARCH_PATH+=$SUBDIR
 ROOT_PATH="/Users/edwfor/Code/metamist/ingestion_metadata/"
-DATA_DATE="2023-12-13"
+DATA_DATE="2024-11-21"
 FOLDER="${PROJECT}_${DATA_DATE}/"
 ROOT_PATH+="${FOLDER}"
+# SAMPLE_MAPPING="${ROOT_PATH}sample_file_mapping_${BATCH}.csv"
+# MAPPING_FILE="${ROOT_PATH}mapping_file_${BATCH}.csv"
 SAMPLE_MAPPING="${ROOT_PATH}sample_file_mapping.csv"
 MAPPING_FILE="${ROOT_PATH}mapping_file.csv"
 PED_FILE="${ROOT_PATH}ped_file.csv"
@@ -17,6 +20,6 @@ CRAM_REF="gs://cpg-common-main/references/hg38/v0/dragen_reference/Homo_sapiens_
 
 python generate_sample_file_map.py -i $SAMPLE_MAPPING -p $SEARCH_PATH > $MAPPING_FILE
 
-python parse_sample_file_map.py --project $PROJECT --search-path $SEARCH_PATH --dry-run --allow-extra-files-in-search_path $MAPPING_FILE
-
+python parse_sample_file_map.py --project $PROJECT --search-path $SEARCH_PATH --allow-extra-files-in-search_path  --confirm $MAPPING_FILE  
+ 
 python parse_rd_metadata.py --project $PROJECT --ped-file $PED_FILE --individual-metadata $INDIVIDUAL_METADATA --family-metadata $FAMILY_FILE
