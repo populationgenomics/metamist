c53efecd0d91e2e5c315d7bd89b430b7c74307f3
Dan Coates <dan.coates@populationgenomics.org.au>
2025-01-08 15:58:05 +1100

    switch query ui to store queries as a list, rather than an object
    this has a bunch of advantages, mostly makes it a ton easier to update
    and rename queries, and don't need to worry about queries having the
    same name

--- web/src/pages/report/SqlQueryUI.tsx
+++ web/src/pages/report/SqlQueryUI.tsx
@@ -1,6 +1,4 @@
 import Editor from '@monaco-editor/react'
-import { editor, KeyCode, KeyMod } from 'monaco-editor'
-
 import DeleteIcon from '@mui/icons-material/Delete'
 import EditIcon from '@mui/icons-material/Edit'
 import ErrorIcon from '@mui/icons-material/Error'
@@ -24,49 +22,67 @@ import {
     ListSubheader,
     Typography,
 } from '@mui/material'
-import { Fragment, memo, useContext, useEffect, useRef, useState } from 'react'
+import { debounce } from 'lodash'
+import { editor, KeyCode, KeyMod } from 'monaco-editor'
+import { Fragment, memo, useCallback, useContext, useEffect, useRef, useState } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import { ThemeContext } from '../../shared/components/ThemeProvider'
 import { TableFromQuery } from './chart/TableFromQuery'
 import { useProjectDbSetup } from './data/projectDatabase'
 
-// Memoized version of the result table to avoid rerenders
+// Memoized version of the result table to avoid rerenders, this is very important for performance
+// without this each keystroke takes forever to render
 const QueryResultTable = memo(function QueryResultTable(props: { project: string; query: string }) {
     return <TableFromQuery project={props.project} query={props.query} showToolbar />
 })
 
-function persistQueryToUrl(query: string) {
+// This fn is debounced to try and limit the amount of history entries.
+const persistQueryToUrl = debounce(function persistQueryToUrl(query: string) {
     const url = new URL(window.location.href)
     url.searchParams.set('query', query)
     window.history.replaceState({}, '', url.toString())
-}
+}, 1000)
 
 function getQueryFromUrl() {
     const url = new URL(window.location.href)
     return url.searchParams.get('query')
 }
 
-export default function ProjectDatabaseQuery() {
+function getQueryId() {
+    return crypto.randomUUID()
+}
+
+type QueryEntry = {
+    id: string
+    query: string
+    name: string
+}
+
+export default function SqlQueryUi() {
     const { projectName, tableName: selectedTableName } = useParams()
     const navigate = useNavigate()
-    const localStorageQueriesKey = `project-db-queries-${projectName}`
+    // This is the key where the user's queries will be stored in local storage.
+    // It includes the project name so that queries can be different for each project.
+    const localStorageQueriesKey = `project-sql:${projectName}`
     const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null)
 
     const theme = useContext(ThemeContext)
-    const [queryStringFromUrl, setqueryStringFromUrl] = useState<string | null>(null)
-    const [activeQuery, setActiveQuery] = useState<string | null>(null)
+    const [queryStringFromUrl, setQueryStringFromUrl] = useState<string | null>(null)
+    const [activeQueryId, setActiveQueryId] = useState<string | null>(null)
     const [tableQueryValue, setTableQueryValue] = useState<string>('')
-    const [queries, setQueries] = useState<Record<string, string | null>>({})
+    const [queries, setQueries] = useState<QueryEntry[]>([])
     const [expandedTables, setExpandedTables] = useState<string[]>([])
 
     const baseUrl = `/project/${projectName}/query`
     const viewType = selectedTableName ? 'table' : 'query'
     const selectedTableQuery = selectedTableName ? `SELECT * FROM ${selectedTableName}` : null
 
+    const activeQuery = queries.find((qq) => qq.id === activeQueryId)
+
     function handleEditorDidMount(editor: editor.IStandaloneCodeEditor) {
         editorRef.current = editor
 
-        // Allow running query one cmd/ctrl + enter
+        // Allow running query on cmd/ctrl + enter
         editor.addAction({
             id: 'executeCurrentAndAdvance',
             label: 'Execute Block and Advance',
@@ -82,6 +98,7 @@ export default function ProjectDatabaseQuery() {
         })
     }
 
+    // Handle showing/hiding the column names for tables
     function handleSidebarSectionToggle(name: string) {
         if (expandedTables.includes(name)) {
             setExpandedTables(expandedTables.filter((n) => n !== name))
@@ -90,103 +107,95 @@ export default function ProjectDatabaseQuery() {
         }
     }
 
-    function handleQueryChange(queryName: string | null, queryValue: string | null) {
-        const newQueries = { ...queries }
-        let resolvedQueryName = queryName
-        if (!resolvedQueryName) {
-            resolvedQueryName = handleNewQuery()
-        }
+    // handle any changes to a query, this updates both the local state, as well as
+    // local storage and the URL.
+    const handleQueryChange = useCallback(
+        function handleQueryChange(queryId: string, queryValue: string | null) {
+            let newQueries = queries
 
-        // Allow deleting a query
-        if (queryValue === null && resolvedQueryName in newQueries) {
-            delete newQueries[resolvedQueryName]
-            if (activeQuery === resolvedQueryName) {
-                setActiveQuery(null)
-            }
-        } else {
-            newQueries[resolvedQueryName] = queryValue
-        }
-
-        setQueries(newQueries)
-        // Do this in a setTimeout so that it doesn't affect the render time
-        setTimeout(() => {
-            localStorage.setItem(localStorageQueriesKey, JSON.stringify(newQueries))
-            if (resolvedQueryName === activeQuery) {
-                persistQueryToUrl(queryValue || '')
-            }
-        }, 0)
-    }
-
-    function handleNewQuery(queryValue: string = '') {
-        navigate(baseUrl, { replace: true })
-        // Find the highest number of untitled queries so that name
-        // won't collide with other names
-        const untitledNumbers = Object.keys(queries).map((q) => {
-            const match = q.match(/Untitled (\d+)$/)
-            return match ? parseInt(match[1]) : 0
-        })
-        const maxUntitled = untitledNumbers.length > 0 ? Math.max(...untitledNumbers) : 0
-        const queryName = `Untitled ${maxUntitled + 1}`
-
-        setActiveQuery(queryName)
-        handleQueryChange(queryName, queryValue)
-        return queryName
-    }
-
-    function handleDeleteQuery(name: string) {
-        handleQueryChange(name, null)
-    }
-
-    function handleRenameQuery(oldName: string, newName: string) {
-        if (oldName === newName) return
-
-        let resolvedNewName = newName
-        const newQueries = { ...queries }
-        const oldQuery = newQueries[oldName]
-        const conflictingQuery = newQueries[newName]
-
-        // Handle case where query has been renamed to an existing query, in this case add an
-        // incrementing number to the end of the query name
-        if (conflictingQuery && conflictingQuery !== oldQuery) {
-            const otherNumbers = Object.keys(newQueries)
-                .filter((q) => q.startsWith(newName))
-                .map((q) => {
-                    const suffix = q.slice(newName.length + 1)
-                    const match = suffix.match(/^(\d+)$/)
-                    return match ? parseInt(match[1]) : 0
+            // Allow deleting a query
+            if (queryValue === null) {
+                newQueries = newQueries.filter((qq) => qq.id !== queryId)
+                if (activeQueryId === queryId) {
+                    setActiveQueryId(null)
+                }
+            } else {
+                newQueries = newQueries.map((qq) => {
+                    if (qq.id === queryId) return { ...qq, query: queryValue }
+                    return qq
                 })
-            const maxNumber = otherNumbers.length > 0 ? Math.max(...otherNumbers) : 0
-            resolvedNewName = `${newName} ${maxNumber + 1}`
-        }
-        // if this was the active query, be sure to update the active query name
-        if (oldName === activeQuery) setActiveQuery(resolvedNewName)
-        // Remove the old query and add the new one
-        delete newQueries[oldName]
-        newQueries[resolvedNewName] = oldQuery
+            }
+
+            setQueries(newQueries)
+            // Do this in a setTimeout so that it doesn't affect the render time
+            setTimeout(() => {
+                localStorage.setItem(localStorageQueriesKey, JSON.stringify(newQueries))
+                if (queryId === activeQueryId) {
+                    persistQueryToUrl(queryValue || '')
+                }
+            }, 0)
+        },
+        [queries, activeQueryId, localStorageQueriesKey]
+    )
+
+    // Handle creation of a new query
+    const handleNewQuery = useCallback(
+        function handleNewQuery(queryValue: string = '') {
+            // Navigate to query url with query string removed
+            navigate(baseUrl, { replace: true })
+
+            const queryId = getQueryId()
+            const queryName = 'Untitled'
+            const newQueries = [...queries, { id: queryId, query: queryValue, name: queryName }]
+
+            setQueries(newQueries)
+            setActiveQueryId(queryId)
+
+            // Do this in a setTimeout so that it doesn't affect the render time
+            setTimeout(() => {
+                localStorage.setItem(localStorageQueriesKey, JSON.stringify(newQueries))
+                persistQueryToUrl(queryValue || '')
+            }, 0)
+        },
+        [baseUrl, queries, navigate, localStorageQueriesKey]
+    )
+
+    function handleDeleteQuery(queryId: string) {
+        handleQueryChange(queryId, null)
+    }
+
+    function handleRenameQuery(queryId: string, newName: string) {
+        const newQueries = queries.map((qq) => {
+            if (qq.id === queryId) return { ...qq, name: newName }
+            return qq
+        })
+
         // Update the state and local storage
         setQueries(newQueries)
         localStorage.setItem(localStorageQueriesKey, JSON.stringify(newQueries))
     }
 
+    // Show results for the active query in the table
     function handleSubmitActiveQuery() {
-        const currentQuery = activeQuery ? queries[activeQuery] : null
-        if (currentQuery) {
-            setTableQueryValue(currentQuery)
+        if (activeQuery) {
+            setTableQueryValue(activeQuery.query)
         }
     }
 
-    function handleSelectQuery(name: string) {
-        setActiveQuery(name)
-        navigate(baseUrl, { replace: true })
-        const queryValue = queries[name] || ''
-        persistQueryToUrl(queryValue)
-    }
+    const handleSelectQuery = useCallback(
+        function handleSelectQuery(id: string) {
+            setActiveQueryId(id)
+            navigate(baseUrl, { replace: true })
+            persistQueryToUrl(activeQuery?.query || '')
+        },
+        [baseUrl, activeQuery, navigate]
+    )
 
     const dbSetupStatus = useProjectDbSetup(projectName)
 
     // Only run this on load, we don't want to have the url be the source of truth for the query
     useEffect(() => {
-        setqueryStringFromUrl(getQueryFromUrl())
+        setQueryStringFromUrl(getQueryFromUrl())
     }, [])
 
     // Load queries out of local storage
@@ -195,41 +204,36 @@ export default function ProjectDatabaseQuery() {
 
         if (storedQueries) {
             const parsed = JSON.parse(storedQueries)
-            if (!parsed || typeof parsed !== 'object' || Array.isArray(parsed)) return
-            const keys = Object.keys(parsed)
-            if (keys.length === 0) return
+            if (!parsed || typeof parsed !== 'object' || !Array.isArray(parsed)) return
             setQueries(parsed)
         }
     }, [localStorageQueriesKey])
 
-    // Set the active query if it isn't already set
+    // Set the active query if it isn't already set, either from the URL or to the first query
     useEffect(() => {
         // Don't do anything if it is already set, or viewing a table
-        if (activeQuery || viewType === 'table') return
+        if (activeQueryId || viewType === 'table') return
 
         // This is only meant to hydrate the query from the url on load, so no need to run if the
         // query has changed
         const queryChangedSinceLoad = queryStringFromUrl !== getQueryFromUrl()
         if (queryChangedSinceLoad) return
 
-        const queryKeys = Object.keys(queries)
-        const queryMatchingUrl = Object.entries(queries).find(
-            ([_, query]) => query === queryStringFromUrl
-        )?.[0]
+        const queryMatchingUrl = queries.find(({ query }) => query === queryStringFromUrl)
 
         // If there is a query in the url and it matches a saved query, set that one to active
         if (queryStringFromUrl && queryMatchingUrl) {
-            setActiveQuery(queryMatchingUrl)
+            setActiveQueryId(queryMatchingUrl.id)
         }
         // If there is a query in the url but it doesn't match a saved query, create new query
         else if (queryStringFromUrl) {
             handleNewQuery(queryStringFromUrl)
         }
         // Otherwise, set the first query as active
-        else if (queryKeys.length > 0) {
-            handleSelectQuery(queryKeys[0])
+        else if (queries.length > 0) {
+            handleSelectQuery(queries[0].id)
         }
-    })
+    }, [activeQueryId, viewType, queryStringFromUrl, queries, handleSelectQuery, handleNewQuery])
 
     const sidebar = dbSetupStatus && dbSetupStatus.status === 'success' && (
         <List dense={true} disablePadding>
@@ -239,14 +243,14 @@ export default function ProjectDatabaseQuery() {
                 </Typography>
             </ListSubheader>
 
-            {Object.keys(queries).map((name) => (
-                <ListItem key={name} disablePadding>
+            {queries.map((query) => (
+                <ListItem key={query.id} disablePadding>
                     <ListItemButton
-                        selected={activeQuery === name}
-                        onClick={() => handleSelectQuery(name)}
+                        selected={activeQueryId === query.id}
+                        onClick={() => handleSelectQuery(query.id)}
                     >
                         <ListItemText>
-                            <Typography component={'span'}>{name}</Typography>
+                            <Typography component={'span'}>{query.name}</Typography>
                         </ListItemText>
 
                         <IconButton
@@ -255,7 +259,7 @@ export default function ProjectDatabaseQuery() {
                             onClick={(e) => {
                                 e.stopPropagation()
                                 const newName = prompt('Rename query')
-                                if (newName) handleRenameQuery(name, newName)
+                                if (newName) handleRenameQuery(query.id, newName)
                             }}
                         >
                             <EditIcon />
@@ -269,7 +273,7 @@ export default function ProjectDatabaseQuery() {
                                 const amSure = confirm(
                                     'Are you sure you want to delete this query?'
                                 )
-                                if (amSure) handleDeleteQuery(name)
+                                if (amSure) handleDeleteQuery(query.id)
                             }}
                         >
                             <DeleteIcon />
@@ -281,7 +285,7 @@ export default function ProjectDatabaseQuery() {
             <ListItem disablePadding>
                 <ListItemButton onClick={() => handleNewQuery()}>
                     <ListItemText>
-                        <Typography component={'span'}>New Query</Typography>
+                        <Button variant="outlined">New Query</Button>
                     </ListItemText>
                 </ListItemButton>
             </ListItem>
@@ -388,8 +392,14 @@ export default function ProjectDatabaseQuery() {
                             theme={theme.theme === 'dark-mode' ? 'vs-dark' : 'vs-light'}
                             language="sql"
                             onMount={handleEditorDidMount}
-                            onChange={(value) => handleQueryChange(activeQuery, value || '')}
-                            value={activeQuery ? queries[activeQuery] || '' : ''}
+                            onChange={(value) => {
+                                if (activeQueryId) {
+                                    handleQueryChange(activeQueryId, value || '')
+                                } else {
+                                    handleNewQuery(value || '')
+                                }
+                            }}
+                            value={activeQuery?.query || ''}
                             options={{
                                 padding: { top: 10 },
                                 minimap: { enabled: false },
