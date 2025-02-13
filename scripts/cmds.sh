b11489cff06b50405eae971b258bac624cccb3ab
EddieLF <eddieformaini@gmail.com>
2025-02-13 16:47:15 +1100

    e

--- scripts/cmds.sh
+++ scripts/cmds.sh
@@ -1,25 +1,28 @@
 SM_ENVIRONMENT=
-PROJECT="rdp-autoimmune"
+PROJECT="genomic-autopsy"
 SEARCH_PATH="gs://cpg-${PROJECT}-main-upload/"
-SUBDIR="2024-12-20"
+# SEARCH_PATH="gs://cpg-${PROJECT}-upload/"
+SUBDIR="2023-03-01"
 # BATCH="R_241021_ALEBUT_DNA_P002"
 SEARCH_PATH+=$SUBDIR
 ROOT_PATH="/Users/edwfor/Code/metamist/ingestion_metadata/"
-DATA_DATE="2024-11-21"
+DATA_DATE="2025-02-03"
 FOLDER="${PROJECT}_${DATA_DATE}/"
 ROOT_PATH+="${FOLDER}"
 # SAMPLE_MAPPING="${ROOT_PATH}sample_file_mapping_${BATCH}.csv"
 # MAPPING_FILE="${ROOT_PATH}mapping_file_${BATCH}.csv"
 SAMPLE_MAPPING="${ROOT_PATH}sample_file_mapping.csv"
-MAPPING_FILE="${ROOT_PATH}mapping_file.csv"
+MAPPING_FILE="${ROOT_PATH}mapping_file_wes_hg38.csv"
 PED_FILE="${ROOT_PATH}ped_file.csv"
 FAMILY_FILE="${ROOT_PATH}families_metadata.csv"
 INDIVIDUAL_METADATA="${ROOT_PATH}individuals_metadata.csv"
-CRAM_REF="gs://cpg-common-main/references/hg38/v0/dragen_reference/Homo_sapiens_assembly38_masked.fasta"
+# CRAM_REF="gs://cpg-common-main/references/hg38/v0/dragen_reference/Homo_sapiens_assembly38_masked.fasta"
+CRAM_REF="gs://cpg-common-main/references/hg38/v0/Homo_sapiens_assembly38.fasta"
+# CRAM_REF="gs://cpg-genomic-autopsy-main-upload/2023-03-01/human_g1k_v37_decoy.fasta"
 
 
 python generate_sample_file_map.py -i $SAMPLE_MAPPING -p $SEARCH_PATH > $MAPPING_FILE
 
-python parse_sample_file_map.py --project $PROJECT --search-path $SEARCH_PATH --allow-extra-files-in-search_path  --confirm $MAPPING_FILE  
+python parse_sample_file_map.py --project $PROJECT --search-path $SEARCH_PATH --allow-extra-files-in-search_path --ref $CRAM_REF  --confirm $MAPPING_FILE  
  
 python parse_rd_metadata.py --project $PROJECT --ped-file $PED_FILE --individual-metadata $INDIVIDUAL_METADATA --family-metadata $FAMILY_FILE
