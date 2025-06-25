7d37bdcedd6c79bf93ccd4ed31f37391be61241f
Dan Coates <dan.coates@populationgenomics.org.au>
2025-06-25 16:46:29 +1000

    update data quality report

--- web/src/pages/report/reports/ourdna/DataQuality.tsx
+++ web/src/pages/report/reports/ourdna/DataQuality.tsx
@@ -6,12 +6,19 @@ const ROW_HEIGHT = 220
 const PROJECT = 'ourdna'
 
 const AGE_QUERY = `
-    select
-        participant_id,
-        "meta_birth-year" as birth_year,
-        date_part('year', current_date()) - try_cast("meta_birth-year" as int) as age
-    from participant
-    where date_part('year', current_date()) - try_cast("meta_birth-year" as int) is not null
+    select distinct
+        p.participant_id,
+        try_cast(p.meta_birth_year as int) as birth_year,
+        date_part('year', try_strptime(
+            s.meta_collection_datetime,
+            '%Y-%m-%dT%H:%M:%S'
+        )) as collection_year,
+        collection_year - birth_year as age
+    from participant p
+    join sample s
+    on s.participant_id = p.participant_id
+    where age is not null
+    and s.type = 'blood'
 `
 
 export default function DataQuality() {
@@ -41,17 +48,17 @@ export default function DataQuality() {
                     height={ROW_HEIGHT}
                     flexBasis={300}
                     flexGrow={1}
-                    title="Samples without a parent blood sample"
-                    description="Samples that aren't correctly nested under a blood sample"
+                    title="Participants without a parent blood sample"
+                    description="Participants whose samples aren't correctly nested under a blood sample"
                     query={`
                         select
-                            count(distinct sample_id) as count
+                            count(distinct participant_id) as count
                         from sample s
                         where not exists (
                             select 1 from sample ss
                             where ss.sample_id = s.sample_root_id
                             and ss.type = 'blood'
-                        )
+                        ) and type != 'blood'
                     `}
                 />
                 <ReportItemMetric
@@ -66,7 +73,7 @@ export default function DataQuality() {
                             count(distinct participant_id) as count
                         from sample
                         where type = 'blood'
-                        and "meta_collection-time" is null
+                        and meta_collection_datetime is null
                     `}
                 />
                 <ReportItemMetric
@@ -81,8 +88,8 @@ export default function DataQuality() {
                             count(distinct participant_id) as count
                         from sample
                         where type = 'blood'
-                        and "meta_collection-time" is not null
-                        and try_strptime(nullif("meta_collection-time", ' '), '%Y-%m-%d %H:%M:%S') is null
+                        and meta_collection_datetime is not null
+                        and try_strptime(meta_collection_datetime, '%Y-%m-%dT%H:%M:%S') is null
                     `}
                 />
             </ReportRow>
@@ -98,8 +105,8 @@ export default function DataQuality() {
                         select
                             count(distinct participant_id) as count
                         from sample
-                        where type = 'blood'
-                        and "meta_process-end-time" is null
+                        where type != 'blood'
+                        and meta_processing_end_datetime is null
                     `}
                 />
                 <ReportItemMetric
@@ -114,8 +121,8 @@ export default function DataQuality() {
                             count(distinct participant_id) as count
                         from sample
                         where type = 'blood'
-                        and "meta_process-end-time" is not null
-                        and try_strptime(nullif("meta_process-end-time", ' '), '%Y-%m-%d %H:%M:%S') is null
+                        and meta_processing_end_datetime is not null
+                        and try_strptime(meta_processing_end_datetime, '%Y-%m-%dT%H:%M:%S') is null
                     `}
                 />
                 <ReportItemMetric
@@ -161,6 +168,39 @@ export default function DataQuality() {
                             `,
                         },
                     ]}
+                />
+            </ReportRow>
+            <ReportRow>
+                <ReportItemTable
+                    height={600}
+                    flexGrow={1}
+                    flexBasis={300}
+                    title="Mismatched or invalid Sonic Ids"
+                    description={`Sonic ids that are either not numeric, or don't match the sonic id on the participant`}
+                    project={PROJECT}
+                    showToolbar={true}
+                    query={`
+                        select
+                            coalesce(p.participant_id, s.participant_id) as participant_id,
+                            s.sample_id,
+                            p.meta_event_recorded_sonic_id as event_portal_sonic_id,
+                            s.external_id_sonic as processing_site_sonic_id
+                        from participant p
+                        full outer join sample s on p.participant_id = s.participant_id
+                        where (
+                            p.meta_event_recorded_sonic_id is not null
+                            and s.external_id_sonic is not null
+                            and p.meta_event_recorded_sonic_id != s.external_id_sonic
+                        )
+                        or (
+                            s.external_id_sonic is not null
+                            and not regexp_full_match(s.external_id_sonic, '\d+')
+                        )
+                        or (
+                            p.meta_event_recorded_sonic_id is not null
+                            and not regexp_full_match(p.meta_event_recorded_sonic_id, '\d+')
+                        )    
+                    `}
                 />
             </ReportRow>
         </Report>
