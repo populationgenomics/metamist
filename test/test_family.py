03765c8408fd44cee83b63a309f94aab6109e797
hrrsheen <harrison.sheen@populationgenomics.org.au>
2025-09-23 14:09:18 +1000

    Linting

--- test/test_family.py
+++ test/test_family.py
@@ -4,21 +4,35 @@ from fastapi import UploadFile
 from api.routes import family
 from tempfile import TemporaryFile
 
+
 class TestPedigree(DbIsolatedTest):
     """Family testing methods"""
 
     @run_as_sync
     async def test_import_families(self):
-        with TemporaryFile(prefix="test", suffix=".tsv") as f:
+        with TemporaryFile(prefix='test', suffix='.tsv') as f:
             emptyTestFile = UploadFile(f)
 
             # Test has_header = true
-            response = await family.import_families(emptyTestFile, has_header=True, delimiter='\t', connection=self.connection)
+            response = await family.import_families(
+                emptyTestFile,
+                has_header=True,
+                delimiter='\t',
+                connection=self.connection,
+            )
             assert response == (
-                {'success': False, 'message': 'A header was expected but file is empty.'},
+                {
+                    'success': False,
+                    'message': 'A header was expected but file is empty.',
+                },
                 400,
             )
 
             # Test no op when has_header = false
-            response = await family.import_families(emptyTestFile, has_header=False, delimiter='\t', connection=self.connection)
+            response = await family.import_families(
+                emptyTestFile,
+                has_header=False,
+                delimiter='\t',
+                connection=self.connection,
+            )
             assert response == ({}, 204)
