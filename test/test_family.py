8de01a494cd842f9931fea5f1ce8be42d2429635
hrrsheen <harrison.sheen@populationgenomics.org.au>
2025-09-23 14:07:15 +1000

    Changed to using FastAPI's UploadFile

--- test/test_family.py
+++ test/test_family.py
@@ -1,5 +1,6 @@
 from test.testbase import DbIsolatedTest, run_as_sync
 
+from fastapi import UploadFile
 from api.routes import family
 from tempfile import TemporaryFile
 
@@ -8,10 +9,6 @@ class TestPedigree(DbIsolatedTest):
 
     @run_as_sync
     async def test_import_families(self):
-        class UploadFile:
-            def __init__(self, file):
-                self.file = file
-        
         with TemporaryFile(prefix="test", suffix=".tsv") as f:
             emptyTestFile = UploadFile(f)
 
@@ -25,4 +22,3 @@ class TestPedigree(DbIsolatedTest):
             # Test no op when has_header = false
             response = await family.import_families(emptyTestFile, has_header=False, delimiter='\t', connection=self.connection)
             assert response == ({}, 204)
-            
