76f1783ab3a2b9eb1529032afb821b5d9307fd60
piyumiamarasinghe <rameshkafox@gmail.com>
2025-10-07 11:13:26 +1100

    [SET-233] ruff format

--- test/conftest.py
+++ test/conftest.py
@@ -6,11 +6,13 @@ This set-up improve test runtime by reusing the test database container whose cr
 
 Adapted from - https://stackoverflow.com/questions/17801300/how-to-run-a-method-before-all-tests-in-all-classes
 """
+
 import pytest
 
 from test.testdb_container import TestDatabaseContainer
 
-@pytest.fixture(scope="session", autouse=True)
+
+@pytest.fixture(scope='session', autouse=True)
 def setup_before_all_tests():
     db_container = TestDatabaseContainer()
     db_container.start()
