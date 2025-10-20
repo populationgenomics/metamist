eb2c169025b6aa633d8edeba905a341f3e49285b
Milo Hyben <milo.hyben@gmail.com>
2025-10-20 11:27:03 +1100

    Refactoring static dunctions.

--- db/python/tables/sample.py
+++ db/python/tables/sample.py
@@ -756,3 +756,42 @@ class SampleTable(DbBase):
                 current_month = (current_month + relativedelta(months=1)).replace(day=1)
 
         return result
+
+    async def _get_sample_count_per_project_by_seq_group(
+        self, sequencing_group_ids: list[int]
+    ) -> dict[int, int]:
+        """Get the count of samples per project based on provided seq groups"""
+        _query = """
+            SELECT s.project, COUNT(DISTINCT s.id) AS sample_count
+            FROM sequencing_group sg
+            INNER JOIN sample s ON s.id = sg.sample_id
+            WHERE sg.id in :sequencing_group_ids
+            GROUP BY s.project
+        """
+        rows = await self.connection.fetch_all(
+            _query, {'sequencing_group_ids': sequencing_group_ids}
+        )
+        return {r['project']: r['sample_count'] for r in rows}
+
+    async def match_seq_grp_to_project(
+        self, sg_compute_per_month: dict[str, list[int]] | None
+    ) -> dict[int, dict[date, int]]:
+        """
+        Match passed month->list of sequencing groups to projects
+        Return a map of {project_id: {month: count of samples used in compute}}
+        """
+        # Map to hold the results
+        result: dict[int, dict[date, int]] = defaultdict(lambda: defaultdict(int))
+
+        if not sg_compute_per_month:
+            return result
+
+        for month, seq_groups in sg_compute_per_month.items():
+            for seq_group in seq_groups:
+                project_sample_count = (
+                    await self._get_sample_count_per_project_by_seq_group(seq_group)
+                )
+                for project_id, sample_count in project_sample_count.items():
+                    result[project_id][month] += sample_count
+
+        return result
