eaec6d8ee94251821f45983c5d430d994cc30563
EddieLF <eddieformaini@gmail.com>
2025-11-25 12:23:42 +1100

    Cast paths to strings where required

--- scripts/duplicate_sequencing_group.py
+++ scripts/duplicate_sequencing_group.py
@@ -431,7 +431,7 @@ async def reheader_analysis_files_in_batch(
             continue
         job = batch.new_job(f'Replace SG ID in {new_path!s}')
         if file_reheaderable(str(old_path)):
-            job.storage(file_size(old_path) + 2 * 1024**3)  # Allow 2 GiB extra
+            job.storage(file_size(str(old_path)) + 2 * 1024**3)  # Allow 2 GiB extra
             main_file_commands(
                 batch,
                 job,
@@ -440,7 +440,7 @@ async def reheader_analysis_files_in_batch(
                 (source_sequencing_group_id, new_sequencing_group_id),
             )
         elif str(old_path).endswith('.gz'):
-            job.storage(file_size(old_path) + 512 * 1024**2)  # Allow 512 MiB extra
+            job.storage(file_size(str(old_path)) + 512 * 1024**2)  # Allow 512 MiB extra
             gzipped_file_commands(
                 batch,
                 job,
