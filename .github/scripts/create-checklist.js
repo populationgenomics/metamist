a15a0a48d2bdd0a690ca658cec1100564f3233b3
hrrsheen <harrison.sheen@populationgenomics.org.au>
2026-01-12 15:41:01 +1000

    Updated script for better comment parsing

--- .github/scripts/create-checklist.js
+++ .github/scripts/create-checklist.js
@@ -1,29 +1,71 @@
-const checklist = `
-## PR Checklist ✅
+const checklistItems = [
+    'Does not contain the names of any sensitive CPG projects',
+    'Version number is bumped appropriately if API is changed',
+    'Tests cover any newly added code'
+]
+const commentTitle = '## PR Checklist ✅'
+
+const itemRegex = /- \[(x| )\] ([\w ]+)/g
+
+/**
+ * Builds a new checklist as an array of strings based on an existing comment and an
+ * array of checklist items. Any items that are already represented in the body
+ * will maintain their checked status, and any new items will be added.
+ * 
+ * @param {string[]} items 
+ * @param {string} checklistStr
+ * @returns {string[]}
+ */
+const coverItemsFromComment = (items, checklistStr) => {
+    const matches = [...checklistStr.matchAll(itemRegex)].reduce((listContents, [_, checked, itemString]) => {
+        listContents.set(itemString, checked)
+        return listContents
+    }, new Map())
+    console.log(matches)
+
+    const newListBody = items.map((item) => {
+        const checked = matches.get(item) || ' '
+        return `- [${checked}] ${item}`
+    })
+
+    return newListBody
+}
+
+/**
+ * Constructs a new comment from title and checklist body strings.
+ * @param {string} title 
+ * @param {string} checklist 
+ * @returns {string}
+ */
+const buildCommentFromChecklist = (title, checklist) => {
+    return `
+${title}
 Have you checked the following?
-- [ ] Does not contain the names of any sensitive CPG projects
-- [ ] Version number is bumped appropriately if API is changed
-- [ ] Tests cover any newly added code
+${checklist}
 
-*Check all boxes before merging.*`;
-
+**Check all boxes before merging.**
+`
+}
 
 module.exports = async ({ github, context, core }) => {
     try {
-        const { owner, repo } = context.repo;
-        const prNumber = context.issue.number;
+        const { owner, repo } = context.repo
+        const prNumber = context.issue.number
 
         // Look for existing checklist comment.
         const comments = await github.rest.issues.listComments({
             owner,
             repo,
             issue_number: prNumber
-        });
+        })
 
         const botComment = comments.data.find(comment => 
-        comment.body.includes('## PR Checklist') && 
-        comment.user.login === 'github-actions[bot]'
-        );
+            comment.body.includes(commentTitle) && 
+            comment.user.login === 'github-actions[bot]'
+        ) || ''
+        
+        const checklistBody = coverItemsFromComment(checklistItems, botComment).join('\n')
+        const checklist = buildCommentFromChecklist(checklistTitle, checklistBody)
 
         if (botComment) {
         // Update existing comment.
