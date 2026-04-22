97dd0424fd0ca914a032e16a1a26dfcecf69407e
John Marshall <john.marshall@populationgenomics.org.au>
2026-04-22 15:07:28 +1200

    [CODE CHANGE] gql 4.0 refactored DocumentNode to GraphQLRequest

--- packages/metamist/src/metamist/graphql/__init__.py
+++ packages/metamist/src/metamist/graphql/__init__.py
@@ -9,16 +9,13 @@ from json.decoder import JSONDecodeError
 from typing import Any, Dict  # noqa: UP035
 
 import backoff
-from gql import Client
+from gql import Client, GraphQLRequest
 from gql import gql as gql_constructor
 from gql.transport.aiohttp import AIOHTTPTransport
 from gql.transport.aiohttp import log as aiohttp_logger
 from gql.transport.exceptions import TransportServerError
 from gql.transport.requests import RequestsHTTPTransport
 from gql.transport.requests import log as requests_logger
-
-# this does not import itself, it imports the module
-from graphql import DocumentNode  # type: ignore
 from requests.exceptions import HTTPError
 
 import metamist.configuration
@@ -115,7 +112,7 @@ async def get_async_client():
 
 def gql(
     request_string: str, should_validate=False, use_local_schema=False
-) -> DocumentNode:
+) -> GraphQLRequest:
     """
     Given a String containing a GraphQL request, parse it into a Document.
     Optionally validate by fetching schema from metamist at SM_URL / SM_ENVIRONMENT
@@ -126,7 +123,7 @@ def gql(
     return doc
 
 
-def validate(doc: DocumentNode, client=None, use_local_schema=False):
+def validate(doc: GraphQLRequest, client=None, use_local_schema=False):
     """
     Validate a GraphQL document against the schema.
     Fetch schema from SM_URL / SM_ENVIRONMENT
@@ -150,7 +147,7 @@ def validate(doc: DocumentNode, client=None, use_local_schema=False):
     max_time=60,
 )
 def query(
-    _query: str | DocumentNode,
+    _query: str | GraphQLRequest,
     variables: dict | None = None,
     client: Client | None = None,
     log_response: bool = False,
@@ -165,7 +162,7 @@ def query(
         requests_logger.setLevel('WARNING')
 
     response = (client or configure_sync_client()).execute_sync(
-        _query if isinstance(_query, DocumentNode) else gql(_query),
+        _query if isinstance(_query, GraphQLRequest) else gql(_query),
         variable_values=variables,
     )
 
@@ -183,7 +180,7 @@ def query(
     max_time=60,
 )
 async def query_async(
-    _query: str | DocumentNode,
+    _query: str | GraphQLRequest,
     variables: dict | None = None,
     client: Client | None = None,
     log_response: bool = False,
@@ -201,7 +198,7 @@ async def query_async(
         client = await configure_async_client()
 
     response = await client.execute_async(
-        _query if isinstance(_query, DocumentNode) else gql(_query),
+        _query if isinstance(_query, GraphQLRequest) else gql(_query),
         variable_values=variables,
     )
 
