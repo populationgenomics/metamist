cc7504ac0d71d585c25c5845d0f811753bf82238
John Marshall <john.marshall@populationgenomics.org.au>
2026-06-30 17:21:36 +1200

    Simplify importlib.metadata.entry_points() code [minor]

--- etl/load/main.py
+++ etl/load/main.py
@@ -404,9 +404,7 @@ def prepare_parser_map() -> dict[str, type[GenericParser]]:
     """
     parser_map = {}
 
-    for entry_point in importlib.metadata.entry_points().select(
-        group='metamist_parser'
-    ):
+    for entry_point in importlib.metadata.entry_points(group='metamist_parser'):
         parser_cls = entry_point.load()
         parser_map[entry_point.name] = parser_cls
 
